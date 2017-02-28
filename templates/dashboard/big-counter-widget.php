affb11bba08c9774e98e8e6bd6e62ff68f4e825b
rafsuntaskin <rafsun.aiub@gmail.com>
2017-02-28 17:46:59 +0600

    added new seller earning calculation method and updated big counter widget of dashboard

--- templates/dashboard/big-counter-widget.php
+++ templates/dashboard/big-counter-widget.php
@@ -22,7 +22,7 @@
         </li>
         <li>
             <div class="title"><?php _e( 'Earning', 'dokan' ); ?></div>
-            <div class="count"><?php echo $seller_balance; ?></div>
+            <div class="count"><?php echo wc_price( dokan_get_seller_earnings( get_current_user_id() ) ) ?></div>
         </li>
         <li>
             <div class="title"><?php _e( 'Pageview', 'dokan' ); ?></div>
