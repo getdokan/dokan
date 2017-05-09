30ee3fd18b2b1c6f9fc538354a5d366cd0205c0a
Sabbir Ahmed <sabbir.081070@gmail.com>
2017-05-09 13:04:32 +0600

    Fix updating issue

--- includes/functions.php
+++ includes/functions.php
@@ -425,22 +425,21 @@ function dokan_get_new_post_status() {
 function dokan_get_client_ip() {
     $ipaddress = '';
 
-    if ( getenv( 'HTTP_CLIENT_IP' ) )
-        $ipaddress = getenv( 'HTTP_CLIENT_IP' );
-    else if ( getenv( 'HTTP_X_FORWARDED_FOR' ) )
-        $ipaddress = getenv( 'HTTP_X_FORWARDED_FOR' & quot );
-    else if ( getenv( 'HTTP_X_FORWARDED' ) )
-        $ipaddress = getenv( 'HTTP_X_FORWARDED' );
-    else if ( getenv( 'HTTP_FORWARDED_FOR' ) )
-        $ipaddress = getenv( 'HTTP_FORWARDED_FOR' );
-    else if ( getenv( 'HTTP_X_CLUSTER_CLIENT_IP' ) )
-        $ipaddress = getenv( 'HTTP_FORWARDED_FOR' );
-    else if ( getenv( 'HTTP_FORWARDED' ) )
-        $ipaddress = getenv( 'HTTP_FORWARDED' );
-    else if ( getenv( 'REMOTE_ADDR' ) )
-        $ipaddress = getenv( 'REMOTE_ADDR' );
-    else
+    if ( isset($_SERVER['HTTP_CLIENT_IP'] ) ) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if ( isset( $_SERVER['HTTP_X_FORWARDED'] ) ) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if ( isset( $_SERVER['HTTP_FORWARDED_FOR'] ) ) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if ( isset( $_SERVER['HTTP_FORWARDED'] ) ) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
         $ipaddress = 'UNKNOWN';
+    }
 
     return $ipaddress;
 }
