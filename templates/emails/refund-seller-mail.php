1811e0b051df2979b272273acb14d082bb4943a5
rafsuntaskin <rafsun.aiub@gmail.com>
2017-07-20 14:31:37 +0600

    text domain updated on refund email template

--- templates/emails/refund-seller-mail.php
+++ templates/emails/refund-seller-mail.php
@@ -1,14 +1,14 @@
-<?php _e( 'Hi,', 'dokan' ); ?>
+<?php _e( 'Hi,', 'dokan-lite' ); ?>
 
-<?php _e( 'Your refund request has been', 'dokan' ); ?> %status%.
+<?php _e( 'Your refund request has been', 'dokan-lite' ); ?> %status%.
 
-<?php _e( 'Order ID:', 'dokan' ); ?> %order_id%
-<?php _e( 'Refund Amount:', 'dokan' ); ?> %amount%
-<?php _e( 'Refund Reason:', 'dokan' ); ?> %reason%
+<?php _e( 'Order ID:', 'dokan-lite' ); ?> %order_id%
+<?php _e( 'Refund Amount:', 'dokan-lite' ); ?> %amount%
+<?php _e( 'Refund Reason:', 'dokan-lite' ); ?> %reason%
 
-<?php _e( 'You can view the order details by going here:', 'dokan' ); ?>
+<?php _e( 'You can view the order details by going here:', 'dokan-lite' ); ?>
 %order_page%
 
 ---
-<?php _e( 'Sent from', 'dokan' ); ?> %site_name%
+<?php _e( 'Sent from', 'dokan-lite' ); ?> %site_name%
 %site_url%
