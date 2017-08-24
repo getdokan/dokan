6ebe98c131e85840e02b890ee8e1f0d72e6eff70
rafsuntaskin <rafsun.aiub@gmail.com>
2017-08-24 11:46:32 +0600

    new seller registered plain email updated

--- templates/emails/plain/new-seller-registered.php
+++ templates/emails/plain/new-seller-registered.php
@@ -1,15 +1,32 @@
-<?php _e( 'Hello there,', 'dokan-lite' ); ?>
+<?php
+/**
+ * New Seller Email ( plain text )
+ *
+ * An email sent to the admin when a new vendor is registered.
+ *
+ * @class       Dokan_Email_New_Seller
+ * @version     2.6.8
+ * 
+ */
 
-<?php _e( 'A new vendor has been registered to your site ', 'dokan-lite' ); ?>(%site_url%).
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+echo "= " . $email_heading . " =\n\n";
+?>
 
-<?php _e( 'Vendor Details:', 'dokan-lite' ); ?>
-------------------------
+<?php _e( 'Hello there,', 'dokan-lite' ); echo " \n";?>
 
-<?php _e( 'Vendor:', 'dokan-lite' ); ?> %seller_name%
-<?php _e( 'Vendor Store:', 'dokan-lite' ); ?> %store_url%
+<?php _e( 'A new vendor has registered in your marketplace  ', 'dokan-lite' );  echo " \n";?>
+<?php _e( 'Vendor Details:', 'dokan-lite' ); echo " \n"; ?>
 
-<?php _e( 'To edit vendor access and details visit', 'dokan-lite' ); ?> - %seller_edit%
+<?php echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n"; ?>
 
----
-%site_name%
-%site_url%
+<?php _e( 'Vendor: '. $data['seller_name'], 'dokan-lite' ); echo " \n"; ?> 
+<?php _e( 'Vendor Store: '. $data['store_name'], 'dokan-lite' ); echo " \n"; ?>
+
+<?php _e( 'To edit vendor access and details visit : '.$data['seller_edit'], 'dokan-lite' );  ?>
+<?php
+echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
