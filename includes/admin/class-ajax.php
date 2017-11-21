da0f99600b1cfac38080078dc02c93f8ed3cba3a
rafsuntaskin <rafsun.aiub@gmail.com>
2017-11-21 13:02:47 +0600

    formatting fix

--- includes/admin/class-ajax.php
+++ includes/admin/class-ajax.php
@@ -1,23 +1,23 @@
 <?php
 
 /**
-*  Ajax handling for Dokan in Admin area
-*
-*  @since 2.2
-*
-*  @author weDevs <info@wedevs.com>
-*/
+ *  Ajax handling for Dokan in Admin area
+ *
+ *  @since 2.2
+ *
+ *  @author weDevs <info@wedevs.com>
+ */
 class Dokan_Admin_Ajax {
 
-	/**
-	 *  Load autometically all actions
-	 */
-	function __construct() {
+    /**
+     *  Load autometically all actions
+     */
+    function __construct() {
         add_action( 'wp_ajax_dokan_withdraw_form_action', array( $this, 'handle_withdraw_action' ) );
         add_action( 'wp_ajax_dokan-dismiss-promotional-offer-notice', array( $this, 'dismiss_promotional_offer' ) );
-	}
+    }
 
-	/**
+    /**
      * Initializes the Dokan_Template_Withdraw class
      *
      * Checks for an existing Dokan_Template_Withdraw instance
@@ -26,23 +26,23 @@ class Dokan_Admin_Ajax {
     public static function init() {
         static $instance = false;
 
-        if ( ! $instance ) {
+        if ( !$instance ) {
             $instance = new Dokan_Admin_Ajax();
         }
 
         return $instance;
     }
 
-	/**
-	 *  Handle withdraw action via ajax
-	 *
-	 *  @return json success|error|data
-	 */
-	function handle_withdraw_action() {
+    /**
+     *  Handle withdraw action via ajax
+     *
+     *  @return json success|error|data
+     */
+    function handle_withdraw_action() {
 
         parse_str( $_POST['formData'], $postdata );
 
-        if( !wp_verify_nonce( $postdata['dokan_withdraw_admin_bulk_action_nonce'], 'dokan_withdraw_admin_bulk_action' ) ) {
+        if ( !wp_verify_nonce( $postdata['dokan_withdraw_admin_bulk_action_nonce'], 'dokan_withdraw_admin_bulk_action' ) ) {
             wp_send_json_error();
         }
 
@@ -59,7 +59,7 @@ class Dokan_Admin_Ajax {
                 $withdraw->delete_withdraw( $withdraw_id );
 
                 $url = admin_url( 'admin.php?page=dokan-withdraw&message=trashed&status=' . $status );
-                wp_send_json_success( array( 'url'=> $url ) );
+                wp_send_json_success( array( 'url' => $url ) );
 
                 break;
 
@@ -74,7 +74,7 @@ class Dokan_Admin_Ajax {
                 $withdraw->update_status( $withdraw_id, $user_id, 2 );
 
                 $url = admin_url( 'admin.php?page=dokan-withdraw&message=cancelled&status=' . $status );
-                wp_send_json_success( array( 'url'=> $url ) );
+                wp_send_json_success( array( 'url' => $url ) );
 
                 break;
 
@@ -89,7 +89,7 @@ class Dokan_Admin_Ajax {
                 do_action( 'dokan_withdraw_request_approved', $user_id, $amount, $method );
 
                 $url = admin_url( 'admin.php?page=dokan-withdraw&message=approved&status=' . $status );
-                wp_send_json_success( array( 'url'=> $url ) );
+                wp_send_json_success( array( 'url' => $url ) );
 
                 break;
 
@@ -98,7 +98,7 @@ class Dokan_Admin_Ajax {
                 $withdraw->update_status( $withdraw_id, $postdata['user_id'][$withdraw_id], 0 );
 
                 $url = admin_url( 'admin.php?page=dokan-withdraw&message=pending&status=' . $status );
-                wp_send_json_success( array( 'url'=> $url ) );
+                wp_send_json_success( array( 'url' => $url ) );
 
                 break;
         }
@@ -113,7 +113,7 @@ class Dokan_Admin_Ajax {
      */
     public function dismiss_promotional_offer() {
 
-        if ( ! empty( $_POST['dokan_promotion_dismissed'] ) ) {
+        if ( !empty( $_POST['dokan_promotion_dismissed'] ) ) {
             $offer_key = 'dokan_4th_yr_aniv_44_perc_discount';
             update_option( $offer_key . '_tracking_notice', 'hide' );
         }
