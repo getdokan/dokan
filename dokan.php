e8283260e3ef595fa4d2fddfba57a8097110771d
Sabbir Ahmed <sabbir.081070@gmail.com>
2017-12-18 14:16:24 +0600

    Version bump, changelog udpated, make pot file

--- dokan.php
+++ dokan.php
@@ -3,7 +3,7 @@
 Plugin Name: Dokan
 Plugin URI: https://wordpress.org/plugins/dokan-lite/
 Description: An e-commerce marketplace plugin for WordPress. Powered by WooCommerce and weDevs.
-Version: 2.7.1
+Version: 2.7.2
 Author: weDevs
 Author URI: https://wedevs.com/
 Text Domain: dokan-lite
@@ -76,7 +76,7 @@ final class WeDevs_Dokan {
      *
      * @var string
      */
-    public $version = '2.7.1';
+    public $version = '2.7.2';
 
     /**
      * Holds various class instances
