671c23f0edd4e9b4eb50212e46dc589b37c05a7a
Mohammed Saimon <saimon2721@gmail.com>
2018-02-03 12:37:05 +0600

    update seller capablilites

--- includes/upgrades/dokan-upgrade-2.7.6.php
+++ includes/upgrades/dokan-upgrade-2.7.6.php
@@ -11,4 +11,33 @@ function dokan_update_table_structure_276(){
     );
 }
 
+/**
+ * Update seller capabilities
+ *
+ * @since 2.7.6
+ *
+ * @return void
+ */
+function dokan_update_user_capabilities_276() {
+    global $wp_roles;
+
+    if ( class_exists( 'WP_Roles' ) && ! isset( $wp_roles ) ) {
+        $wp_roles = new WP_Roles();
+    }
+
+    $capabilities = array();
+    $all_cap      = dokan_get_all_caps();
+
+    foreach( $all_cap as $key=>$cap ) {
+        $capabilities = array_merge( $capabilities, $cap );
+    }
+
+    foreach ( $capabilities as $key => $capability ) {
+        $wp_roles->add_cap( 'seller', $capability );
+        $wp_roles->add_cap( 'administrator', $capability );
+        $wp_roles->add_cap( 'shop_manager', $capability );
+    }
+}
+
 dokan_update_table_structure_276();
+dokan_update_user_capabilities_276();
