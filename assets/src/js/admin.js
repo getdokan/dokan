bef2c53f207c7258cdbcf347129a97b3b8aeb2c4
Sabbir Ahmed <sabbir.081070@gmail.com>
2018-02-18 14:49:32 +0600

    Added vendor field as a product quick edit

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -61,5 +61,19 @@ jQuery(function($) {
 
         btn.parents('.dokan-settings-radio-image').addClass('active').removeClass('not-active');
     });
+
+    $(function(){
+        $('#the-list').on('click', '.editinline', function(){
+            var post_id = $(this).closest('tr').attr('id');
+
+            post_id = post_id.replace("post-", "");
+
+            var $vendor_id_inline_data = $('#dokan_vendor_id_inline_' + post_id).find('#dokan_vendor_id').text(),
+                $wc_inline_data = $('#woocommerce_inline_' + post_id );
+
+            $( 'select[name="dokan_product_author_override"] option:selected', '.inline-edit-row' ).attr( 'selected', false ).change();
+            $( 'select[name="dokan_product_author_override"] option[value="' + $vendor_id_inline_data + '"]' ).attr( 'selected', 'selected' ).change();
+        });
+    });
 });
 
