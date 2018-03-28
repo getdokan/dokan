21fa920225f605ce1a2af7b236c13731a4a7d813
Mohammed Saimon <saimon2721@gmail.com>
2018-03-28 13:57:05 +0600

    checked if commission is neumeric

--- includes/functions.php
+++ includes/functions.php
@@ -422,19 +422,22 @@ if ( !function_exists( 'dokan_get_seller_percentage' ) ) :
 function dokan_get_seller_percentage( $seller_id = 0, $product_id = 0 ) {
 
     $global_percentage = dokan_get_option( 'admin_percentage', 'dokan_selling' , 0 );
-    $commission_val    = (float) ( 100 - $global_percentage );
+
+    if ( is_numeric( $global_percentage ) && $global_percentage >= 0 && $global_percentage <= 100 ) {
+        $commission_val = (float) ( 100 - $global_percentage );
+    }
 
     //product wise percentage
     if ( $product_id ) {
         $category_commission = dokan_get_category_wise_seller_commission( $product_id );
 
-        if ( !empty( $category_commission ) ) {
+        if ( is_numeric( $category_commission ) && $category_commission >= 0 && $category_commission <= 100 ) {
             $commission_val = (float) $category_commission;
         }
 
         $_per_product_commission = get_post_meta( $product_id, '_per_product_admin_commission', true );
 
-        if ( $_per_product_commission != '' ) {
+        if ( is_numeric( $_per_product_commission ) && $_per_product_commission >= 0 && $_per_product_commission <= 100 ) {
             $commission_val = (float) ( 100 - $_per_product_commission );
         }
     }
@@ -442,7 +445,7 @@ function dokan_get_seller_percentage( $seller_id = 0, $product_id = 0 ) {
     //seller wise percentage
     if ( $seller_id ) {
         $admin_commission = get_user_meta( $seller_id, 'dokan_admin_percentage', true );
-        if ( $admin_commission != '' ) {
+        if ( is_numeric( $admin_commission ) && $admin_commission >= 0 && $admin_commission <= 100 ) {
             $commission_val = (float) ( 100 - $admin_commission );
         }
     }
