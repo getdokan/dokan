2dfc29a40a5421c0920f237131c85c4f97d748d3
Mohammed Saimon <saimon2721@gmail.com>
2018-09-04 09:49:36 +0600

    fix: following wp coding standards

--- classes/withdraw.php
+++ classes/withdraw.php
@@ -50,7 +50,9 @@ class Dokan_Withdraw {
             SET status = %d WHERE user_id=%d AND id = %d",
             $status, $user_id, $row_id
         ) );
-        do_action('dokan_withdraw_status_updated', $status, $user_id, $row_id);
+
+        do_action( 'dokan_withdraw_status_updated', $status, $user_id, $row_id );
+
         $cache_key     = 'dokan_seller_balance_' . $user_id;
         wp_cache_delete( $cache_key );
     }
