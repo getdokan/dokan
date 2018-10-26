16b2909f037ff5c5840cff74eb18bf696dc8db0e
Saimon Sajjad <saimon2721@gmail.com>
2018-10-26 11:02:48 +0600

    fix: add currency component to show translated currency symbol (#417)

---
+++ src/admin/components/Currency.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-html="formattedPrice( amount )"></div>
+</template>
+
+<script>
+export default {
+    props : ['amount'],
+
+    methods: {
+        formattedPrice(value) {
+            return accounting.formatMoney( value, dokan.currency );
+        }
+    }
+};
+</script>
