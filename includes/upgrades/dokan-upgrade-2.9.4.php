d84eb6ff06635ab829f8880463c7c8823cf0f4e0
saimonh3 <saimon2721@gmail.com>
2018-11-14 18:13:12 +0600

    refactor: increase dokan_refund table column length to avoid error if there are too many items in a single order

--- includes/upgrades/dokan-upgrade-2.9.4.php
+++ includes/upgrades/dokan-upgrade-2.9.4.php
@@ -23,3 +23,29 @@ function dokan_update_shop_order_post_author() {
 }
 
 dokan_update_shop_order_post_author();
+
+/**
+ * Update refund table structure
+ *
+ * @return void
+ */
+function dokan_update_refund_table_2_9_4() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'dokan_refund';
+
+    if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) !== $table_name ) {
+        return;
+    }
+
+    $columns = array( 'item_qtys', 'item_totals', 'item_tax_totals' );
+
+    foreach ( $columns as $column ) {
+        $wpdb->query(
+            "ALTER TABLE `{$wpdb->prefix}dokan_refund`
+            MODIFY COLUMN {$column} varchar(100)"
+        );
+    }
+}
+
+dokan_update_refund_table_2_9_4();
