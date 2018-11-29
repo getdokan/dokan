f2d2ee08cf9e25b2c7831735eaff7fb408139529
saimonh3 <saimon2721@gmail.com>
2018-11-29 14:22:55 +0600

    fix: change author to vendor_id to retrive all the vendor orders

--- includes/admin-functions.php
+++ includes/admin-functions.php
@@ -114,7 +114,7 @@ function dokan_shop_order_custom_columns( $col ) {
 
             if ( $has_sub != '1' ) {
                 $seller = get_user_by( 'id', dokan_get_seller_id_by_order( $post->ID ) );
-                printf( '<a href="%s">%s</a>', admin_url( 'edit.php?post_type=shop_order&author=' . $seller->ID ), $seller->display_name );
+                printf( '<a href="%s">%s</a>', admin_url( 'edit.php?post_type=shop_order&vendor_id=' . $seller->ID ), $seller->display_name );
             }
 
             break;
