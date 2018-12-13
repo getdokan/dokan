9b11cd8f15e56cdd2b6cdd8d32c8c43d9cd31669
Saimon Sajjad <saimon2721@gmail.com>
2018-12-14 01:06:35 +0600

    refactor: code format (#467)

--- templates/withdraw/status-listing.php
+++ templates/withdraw/status-listing.php
@@ -9,12 +9,12 @@
 ?>
 <ul class="list-inline subsubsub">
     <li<?php echo $current == 'pending' ? ' class="active"' : ''; ?>>
-        <a href="<?php echo dokan_get_navigation_url( 'withdraw' ); ?>"><?php _e( 'Withdraw Request', 'dokan-lite' ); ?></a>
+        <a href="<?php echo esc_url( dokan_get_navigation_url( 'withdraw' ) ); ?>"><?php esc_html_e( 'Withdraw Request', 'dokan-lite' ); ?></a>
     </li>
     <li<?php echo $current == 'approved' ? ' class="active"' : ''; ?>>
-        <a href="<?php echo add_query_arg( array( 'type' => 'approved' ), dokan_get_navigation_url( 'withdraw' ) ); ?>"><?php _e( 'Approved Requests', 'dokan-lite' ); ?></a>
+        <a href="<?php echo esc_url( add_query_arg( array( 'type' => 'approved' ), dokan_get_navigation_url( 'withdraw' ) ) ); ?>"><?php esc_html_e( 'Approved Requests', 'dokan-lite' ); ?></a>
     </li>
     <li<?php echo $current == 'cancelled' ? ' class="active"' : ''; ?>>
-        <a href="<?php echo add_query_arg( array( 'type' => 'cancelled' ), dokan_get_navigation_url( 'withdraw' ) ); ?>"><?php _e( 'Cancelled Requests', 'dokan-lite' ); ?></a>
+        <a href="<?php echo esc_url( add_query_arg( array( 'type' => 'cancelled' ), dokan_get_navigation_url( 'withdraw' ) ) ); ?>"><?php esc_html_e( 'Cancelled Requests', 'dokan-lite' ); ?></a>
     </li>
 </ul>
