3d22a9a31297b8b82dd6a4e83c793371ed8db3ab
Edi Amin <to.ediamin@gmail.com>
2018-12-14 18:39:44 +0600

    Fix vendor dashboard title with some spacific html tags

--- templates/settings/header.php
+++ templates/settings/header.php
@@ -6,10 +6,24 @@
  *
  * @package dokan
  */
+
+$heading_allowed_tags = array(
+    'a' => array(
+        'href' => array(),
+        'class' => array()
+    ),
+    'span' => array(
+        'class' => array(),
+        'style' => array(),
+    ),
+    'i' => array(
+        'class' => array(),
+    ),
+);
 ?>
 <header class="dokan-dashboard-header">
     <h1 class="entry-title">
-        <?php echo esc_html( $heading ); ?>
+        <?php echo wp_kses( $heading, $heading_allowed_tags ); ?>
         <small>&rarr; <a href="<?php echo esc_url( dokan_get_store_url( dokan_get_current_user_id() ) ); ?>"><?php esc_html_e( 'Visit Store', 'dokan-lite' ); ?></a></small>
     </h1>
 </header><!-- .dokan-dashboard-header -->
