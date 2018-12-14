7ca078cca570f8a13eae95fe08f6cceff91825c2
Sabbir Ahmed <sabbir.081070@gmail.com>
2018-12-14 19:57:20 +0600

    fix: escape rendering settings heading

--- templates/settings/header.php
+++ templates/settings/header.php
@@ -6,24 +6,10 @@
  *
  * @package dokan
  */
-
-$heading_allowed_tags = array(
-    'a' => array(
-        'href' => array(),
-        'class' => array()
-    ),
-    'span' => array(
-        'class' => array(),
-        'style' => array(),
-    ),
-    'i' => array(
-        'class' => array(),
-    ),
-);
 ?>
 <header class="dokan-dashboard-header">
     <h1 class="entry-title">
-        <?php echo wp_kses( $heading, $heading_allowed_tags ); ?>
+        <?php echo wp_kses_post( $heading ); ?>
         <small>&rarr; <a href="<?php echo esc_url( dokan_get_store_url( dokan_get_current_user_id() ) ); ?>"><?php esc_html_e( 'Visit Store', 'dokan-lite' ); ?></a></small>
     </h1>
 </header><!-- .dokan-dashboard-header -->
