36ad5b25a645fd264d10a7a35ac794184f3060ba
Saimon Sajjad <saimon2721@gmail.com>
2018-12-14 10:04:09 +0600

    refactor: use printf correctly to show the content (#478)
    * refactor: use printf correctly to show the content
    
    * refactor: check sanitization
    
    * refactor: check data sanitization

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -261,7 +261,7 @@ class Dokan_Admin {
         }
         ?>
         <div id="message" class="updated">
-            <p><?php esc_html_e( '<strong>Dokan Data Update Required</strong> &#8211; We need to update your install to the latest version', 'dokan-lite' ); ?></p>
+            <p><?php printf( '<strong>%s &#8211; %s</strong>', esc_attr__( 'Dokan Data Update Required', 'dokan-lite' ), esc_attr__( 'We need to update your install to the latest version', 'dokan-lite' ) ); ?></p>
             <p class="submit"><a href="<?php echo esc_url( add_query_arg( 'dokan_do_update', 'true', admin_url( 'admin.php?page=dokan' ) ) ); ?>" class="dokan-update-btn button-primary"><?php esc_html_e( 'Run the updater', 'dokan-lite' ); ?></a></p>
         </div>
 
