1cb574f73b9cd1a4dcc71d00e01a237619946234
Softound Solutions <softound@gmail.com>
2019-01-10 10:19:03 +0530

    Update order-functions.php
    Add "dokan_order_total" filter hook to modify the order total. Thanks

--- includes/order-functions.php
+++ includes/order-functions.php
@@ -308,6 +308,8 @@ function dokan_sync_insert_order( $order_id ) {
     $order              = wc_get_order( $order_id );
     $seller_id          = dokan_get_seller_id_by_order_id( $order_id );
     $order_total        = $order->get_total();
+    //New filter hook to modify the order total.
+	$order_total 		= apply_filters( 'dokan_order_total', $order_total, $order );
     $order_status       = dokan_get_prop( $order, 'status' );
     $admin_commission   = dokan_get_admin_commission_by( $order, $seller_id );
     $net_amount         = $order_total - $admin_commission;
