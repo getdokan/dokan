86b6626931cd7e3c4113b814e7f53cac6044dd82
Saimon Sajjad <saimon2721@gmail.com>
2019-02-25 10:32:50 +0600

    fix: order tracking date format is not matching with WordPress is fixed #553 (#554)

--- includes/class-assets.php
+++ includes/class-assets.php
@@ -637,7 +637,8 @@ class Dokan_Assets {
                 'i18n_selection_too_long_1'           => __( 'You can only select 1 item', 'dokan-lite' ),
                 'i18n_selection_too_long_n'           => __( 'You can only select %qty% items', 'dokan-lite' ),
                 'i18n_load_more'                      => __( 'Loading more results&hellip;', 'dokan-lite' ),
-                'i18n_searching'                      => __( 'Searching&hellip;', 'dokan-lite' )
+                'i18n_searching'                      => __( 'Searching&hellip;', 'dokan-lite' ),
+                'i18n_date_format'                    => get_option( 'date_format' )
             );
 
             $default_args = array_merge( $default_args, $custom_args );
