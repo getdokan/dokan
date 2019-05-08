2291887fdd364be48dc204b9b19be7d6a7e0b613
Edi Amin <to.ediamin@gmail.com>
2019-05-08 17:35:36 +0600

    Add updater file for admin settings rearrangment

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="vue-backend-app">
+        <h1>@todo: Add updater file <code>upgrades/dokan-upgrade-next.php</code> before release</h1>
         <router-view />
         <notifications position="bottom right"/>
     </div>
