028cb336993226f09ffe1993df901119f30438c3
FaraziF <farazi333@gmail.com>
2019-07-20 16:23:34 +0600

    Write refund request tese case and modify withdraw confirm request

--- tests/acceptance/Scenario/02WithdrawRequestAndUpdateCest.php
+++ tests/acceptance/Scenario/02WithdrawRequestAndUpdateCest.php
@@ -10,30 +10,25 @@ class WithdrawRequestAndUpdateCest
     {
     }
 
-    // tests
-    public function tryToTest(\Step\Acceptance\Vendor $I,
+    /*Scenario 3: Vendor submit Refund request 
+                and Admin will change refund request status*/
+    public function withdrawRequestAndUpdate(\Step\Acceptance\Vendor $I,
     							\Step\Acceptance\Admin $A)
     {
-    	// $I->loginAsVendor();
-     //    //$I->wait(10);
-     //    $I->click('Withdraw');
-     //    $I->fillField('#withdraw-amount','80');
-     //    $I->selectOption('#withdraw-method','Bank Transfer');
-     //    $I->click('Submit Request');
-     //    $I->wait('5');
-     //    $I->click('Log out');
+    	$I->loginAsVendor();
+        //$I->wait(10);
+        $I->click('Withdraw');
+        $I->fillField('#withdraw-amount','80');
+        $I->selectOption('#withdraw-method','Bank Transfer');
+        $I->click('Submit Request');
+        $I->wait('5');
+        $I->click('Log out');
 	        $A->loginAsAdmin();
 	        $I->click('Dokan');
 	        $I->click('#toplevel_page_dokan');
 	        $I->click('Withdraw');
-	        // $I->click('item[]','4');
-
-
+	        $I->wait(5);
 			$I->checkOption(Locator::firstElement('//input[@name="item[]"]'));
-			// $I->fillField(Locator::combine('form input[type=text]','//form/textarea[2]'), 'qwerty');
-
-
-	       // $I->click('vendor-one');
 	        $I->selectOption('#bulk-action-selector-top','Approve');
 	        $I->click('Apply');
     }
