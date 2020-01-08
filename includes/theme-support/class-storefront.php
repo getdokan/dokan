0cb9bfc1553bbfe3b364c8698af7306709b58a81
saimonh3 <saimon2721@gmail.com>
2020-01-08 15:39:50 +0600

    fix: add storefront theme support for store listing

--- includes/theme-support/class-storefront.php
+++ includes/theme-support/class-storefront.php
@@ -15,6 +15,7 @@ class Dokan_Theme_Support_Storefront {
     function __construct() {
         add_action( 'storefront_page_after', [ $this, 'remove_sidebar'], 5 );
         add_filter( 'body_class', [ $this, 'full_width_page'] );
+        add_action( 'wp_enqueue_scripts', [ $this, 'reset_style' ] );
     }
 
     /**
@@ -45,6 +46,29 @@ class Dokan_Theme_Support_Storefront {
         }
 
         return $classes;
+    }
+
+    /**
+     * Reset style
+     *
+     * @since  DOKAN_LITE_SINCE
+     *
+     * @return void
+     */
+    public function reset_style() {
+        if ( ! dokan_is_store_listing() ) {
+            return;
+        }
+
+        $style = '#dokan-seller-listing-wrap .store-content .store-data-container .store-data h2 a {text-decoration: none}';
+        $style .= '#dokan-seller-listing-wrap .store-content .store-data-container .store-data h2 {font-size: 24px; margin: 20px 0 10px 0}';
+        $style .= '#dokan-store-listing-filter-form-wrap:before {top: -74px; height: 0}';
+        $style .= 'form.sort-by.item {margin-bottom: 0}';
+        $style .= '#dokan-store-listing-filter-wrap .left .store-count {margin-bottom: 0}';
+        $style .= '#dokan-seller-listing-wrap.list-view .dokan-seller-wrap .dokan-single-seller .store-wrapper .store-content .store-data-container .store-data .dokan-seller-rating[class] {font-size: 14px}';
+
+        wp_add_inline_style( 'dokan-style', $style );
+        wp_enqueue_style( 'woocommerce-general', WC()->plugin_url() . '/assets/css/woocommerce.css', array( '' ), false, 'all' );
     }
 }
 
