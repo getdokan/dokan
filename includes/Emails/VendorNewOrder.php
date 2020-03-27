64ca8da5cc19ca3069208d68b8a16c3a025f5d87
Alamgir Hossain <alamgircse.bd@gmail.com>
2020-03-27 20:29:33 +0600

    fix - Vendor is not receiving order email issue fixed #799 (#800)

--- includes/Emails/VendorNewOrder.php
+++ includes/Emails/VendorNewOrder.php
@@ -97,7 +97,7 @@ class VendorNewOrder extends WC_Email {
         }
         
         // check has sub order 
-        if ( ! dokan_is_sub_order( $order_id ) ) {
+        if ( get_post_meta( $order_id, 'has_sub_order', true ) ) {
         	foreach ($sellers as $seller) {
         		$seller_info      = get_userdata( $seller );
 		        $seller_email 	  = $seller_info->user_email;
