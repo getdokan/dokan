ac8ce69ca59247b1f532ffb3736a9ecd447b571e
Alamgir Hossain <alamgircse.bd@gmail.com>
2020-04-22 15:50:25 +0600

    fix: Vendor order email does not have the TAX details issue fixed #809 (#816)
    * Fix - Vendor order email does not have the TAX details issue fixed #809
    
    * Fix - product addon fields details show on order email

--- templates/emails/vendor-new-order.php
+++ templates/emails/vendor-new-order.php
@@ -39,7 +39,6 @@ $text_align = is_rtl() ? 'right' : 'left';
     ?>
 </h2>
 
-
 <div style="margin-bottom: 40px;">
     <table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
         <thead>
@@ -49,90 +48,57 @@ $text_align = is_rtl() ? 'right' : 'left';
                 <th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Price', 'dokan-lite' ); ?></th>
             </tr>
         </thead>
+        <tbody>
+            <?php
+            echo wc_get_email_order_items( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                $order,
+                array(
+                    'show_sku'      => $sent_to_admin,
+                    'show_image'    => false,
+                    'image_size'    => array( 32, 32 ),
+                    'plain_text'    => $plain_text,
+                    'sent_to_admin' => $sent_to_admin,
+                )
+            );
+            ?>
+        </tbody>
+        <tfoot>
+            <?php
+            $item_totals = $order->get_order_item_totals();
 
-    <tbody>
-
-        <?php $total_price = array(); ?>
-        <?php foreach ( $order_info as $value ) : ?>
-            <tr class="order_item">
-            <?php foreach ( $value as $key => $info ) :?>
-                <?php
-                    if ( $key == 'total' ) {
-                        array_push( $total_price, $info );
-                    }
+            if ( $item_totals ) {
+                $i = 0;
+                foreach ( $item_totals as $total ) {
+                    $i++;
+                    ?>
+                    <tr>
+                        <th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
+                        <td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
+                    </tr>
+                    <?php
+                }
+            }
+            if ( $order->get_customer_note() ) {
                 ?>
-
-                <td class="td" style="text-align: left;vertical-align: middle;font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;color: #636363;border: 1px solid #e5e5e5;padding: 12px"><?php echo $info; ?></td>
-            <?php endforeach; ?>
-            </tr>
-        <?php endforeach; ?>
-
-        <tr>
-            <th class="td" scope="row" colspan="2" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px"><?php esc_attr_e( 'Subtotal', 'dokan-lite' ); ?>:
-            </th>
-
-            <td class="td" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px">
-                <?php echo $order->get_subtotal_to_display(); ?>
-            </td>
-        </tr>
-
-        <?php if ( $order->get_discount_total() ) : ?>
-        <tr>
-            <th class="td" scope="row" colspan="2" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px"><?php esc_attr_e( 'Discount', 'dokan-lite' ); ?>:
-            </th>
-
-            <td class="td" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px">
-                <?php echo '-' . $order->get_discount_to_display(); ?>
-            </td>
-        </tr>
-        <?php endif; ?>
-
-        <tr>
-            <th class="td" scope="row" colspan="2" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px"><?php esc_attr_e( 'Shipping', 'dokan-lite' ); ?>:
-            </th>
-
-            <td class="td" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px">
-                <?php echo $order->get_shipping_to_display(); ?>
-            </td>
-        </tr>
-
-        <?php if ( $order->get_total_tax() ) : ?>
-        <tr>
-            <th class="td" scope="row" colspan="2" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px"><?php esc_attr_e( 'Tax', 'dokan-lite' ); ?>:
-            </th>
-
-            <td class="td" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px">
-                <?php echo wc_price( $order->get_total_tax() ); ?>
-            </td>
-        </tr>
-        <?php endif; ?>
-
-        <tr>
-            <th class="td" scope="row" colspan="2" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px"><?php esc_attr_e( 'Payment Method', 'dokan-lite' ); ?>:
-            </th>
-
-            <td class="td" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px">
-                <?php echo esc_attr( $order->get_payment_method_title() ); ?>
-            </td>
-        </tr>
-
-        <tr>
-            <th class="td" scope="row" colspan="2" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px"><?php esc_attr_e( 'Total', 'dokan-lite' ); ?>:
-            </th>
-
-            <td class="td" style="text-align: left;color: #636363;border: 1px solid #e5e5e5;vertical-align: middle;padding: 12px">
-                <?php echo $order->get_formatted_order_total(); ?>
-            </td>
-        </tr>
-
-    </tbody>
-
+                <tr>
+                    <th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Note:', 'dokan-lite' ); ?></th>
+                    <td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </tfoot>
     </table>
-
-
 </div>
 
-<?php
+<?php 
+ do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email );
+
+ /*
+  * @hooked WC_Emails::order_meta() Shows order meta data.
+  */
+ do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+
  /**
   * @hooked WC_Emails::customer_details() Shows customer details
   * @hooked WC_Emails::email_address() Shows email address
