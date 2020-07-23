ed4cafc6609911edc6be36ae2aef1861e18031a3
Sabbir Ahmed <sabbir.081070@gmail.com>
2020-07-23 08:41:47 +0600

    chore: version bump

--- dokan.php
+++ dokan.php
@@ -3,7 +3,7 @@
 Plugin Name: Dokan
 Plugin URI: https://wordpress.org/plugins/dokan-lite/
 Description: An e-commerce marketplace plugin for WordPress. Powered by WooCommerce and weDevs.
-Version: 3.0.6
+Version: 3.0.7
 Author: weDevs
 Author URI: https://wedevs.com/
 Text Domain: dokan-lite
@@ -54,7 +54,7 @@ final class WeDevs_Dokan {
      *
      * @var string
      */
-    public $version = '3.0.6';
+    public $version = '3.0.7';
 
     /**
      * Instance of self
