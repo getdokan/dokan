220c19b5fbd1d918747ac0271dbb488104f60302
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2020-11-05 17:33:18 +0600

    Refactor/upgrade to pro (#942)
    * fix: settings banner showing when dokan pro is active
    
    * refactor: dokan upgrade-to-pro link
    
    * add: open in new tab for upgrade-to-pro links
    
    * add: upgrade-to-pro banner on Help, Settings, Vendors, Withdraw page

--- src/admin/components/UpgradeBanner.vue
+++ src/admin/components/UpgradeBanner.vue
@@ -2,7 +2,7 @@
     <div id="dokan-upgrade-wrapper">
         <div id="dokan-upgrade-popper">
             <p>{{ __("You're using", 'dokan-lite' ) }} <span>{{ __( 'Dokan Lite', 'dokan-lite' ) }}</span>. {{ __( 'To unlock more features, consider', 'dokan-lite' ) }}</p>
-            <a :href="upgradeURL" class="button">{{ __( 'Upgrade to Pro', 'dokan-lite' ) }}</a>
+            <a target="_blank" rel="noopener noreferrer" :href="upgradeURL" class="button">{{ __( 'Upgrade to Pro', 'dokan-lite' ) }}</a>
         </div>
     </div>
 </template>
@@ -12,7 +12,7 @@ export default {
     name: "UpgradeBanner",
     data() {
         return {
-            upgradeURL: dokan.dokan_upgrade_to_pro_url
+            upgradeURL: dokan.urls.upgradeToPro
         }
     },
 }
--- src/admin/pages/Settings.vue
+++ src/admin/pages/Settings.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <UpgradeBanner v-if="! hasPro"></UpgradeBanner>
+
         <div class="dokan-settings">
             <h2 style="margin-bottom: 15px;">{{ __( 'Settings', 'dokan-lite' ) }}</h2>
 
@@ -64,7 +66,7 @@
             </div>
         </div>
 
-        <SettingsBanner if="! hasPro"></SettingsBanner>
+        <SettingsBanner v-if="! hasPro"></SettingsBanner>
     </div>
 
 </template>
@@ -73,6 +75,7 @@
     let Loading = dokan_get_lib('Loading');
     import Fields from "admin/components/Fields.vue"
     import SettingsBanner from "admin/components/SettingsBanner.vue";
+    import UpgradeBanner from "admin/components/UpgradeBanner.vue";
 
     export default {
 
@@ -82,6 +85,7 @@
             Fields,
             Loading,
             SettingsBanner,
+            UpgradeBanner,
         },
 
         data () {
