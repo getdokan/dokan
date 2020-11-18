726183b551a291bf79c0afa7c617d3392a72f854
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2020-11-18 09:51:23 +0600

    feat: custom deactivation reason for appsero tracker

--- includes/Tracker.php
+++ includes/Tracker.php
@@ -51,6 +51,8 @@ class Tracker {
           );
 
         $this->insights->init_plugin();
+
+        add_filter( 'appsero_custom_deactivation_reasons', [ $this, 'get_custom_deactivation_reasons' ] );
     }
 
     /**
@@ -62,5 +64,51 @@ class Tracker {
         global $wpdb;
 
         return (int) $wpdb->get_var( "SELECT count(ID) FROM $wpdb->posts WHERE post_type = 'shop_order' and post_status IN ('wc-completed', 'wc-processing', 'wc-on-hold', 'wc-refunded');" );
+    }
+
+    /**
+     * Gets custom deactivation reasons
+     *
+     * @since 3.0.14
+     *
+     * @param $reasons
+     *
+     * @return \array
+     */
+    public function get_custom_deactivation_reasons( $reasons ) {
+        $reasons = [
+            [
+                'id'          => 'temporary-deactivation',
+                'text'        => 'I’m Testing/Temporarily deactivating/Debugging',
+                'placeholder' => '',
+                'icon'        => '',
+            ],
+            [
+                'id'          => 'upgrading-to-dokan-pro',
+                'text'        => 'I’m Upgrading to Dokan Pro',
+                'placeholder' => '',
+                'icon'        => '',
+            ],
+            [
+                'id'          => 'encountered-issue-with-plugin',
+                'text'        => 'I encountered an issue with the plugin',
+                'placeholder' => '',
+                'icon'        => '',
+            ],
+            [
+                'id'          => 'encountered-error-with-plugin',
+                'text'        => 'I encountered an error with the plugin',
+                'placeholder' => '',
+                'icon'        => '',
+            ],
+            [
+                'id'          => 'conflicting-with-something',
+                'text'        => 'The plugin is conflicting with something',
+                'placeholder' => '',
+                'icon'        => '',
+            ],
+
+        ];
+        return $reasons;
     }
 }
