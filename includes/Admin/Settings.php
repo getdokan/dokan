b2012f0e6922ed35482ece19299d6ecef16e93ce
weDevs-kapil <kapilpaul007@gmail.com>
2020-12-22 16:45:35 +0600

    perf: dokan admin settings fast load and optimized

--- includes/Admin/Settings.php
+++ includes/Admin/Settings.php
@@ -162,8 +162,16 @@ class Settings {
             return false;
         }
 
+        //settings fields are called every time. so we kept it in cache for a small amount of time. to avoid error and better performance.
+        $settings_fields = get_transient( 'get_settings_fields' );
+
+        if ( ! $settings_fields ) {
+            $settings_fields = $this->get_settings_fields();
+            set_transient( 'get_settings_fields', $settings_fields, 90 );
+        }
+
         // Iterate over registered fields and see if we can find proper callback
-        foreach ( $this->get_settings_fields() as $section => $options ) {
+        foreach ( $settings_fields as $section => $options ) {
             foreach ( $options as $option ) {
                 if ( $option['name'] != $slug ) {
                     continue;
