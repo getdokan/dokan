7f1f265b3b226222424d47646ba370e7d828855a
Nurul Umbhiya <zikubd@gmail.com>
2021-01-10 20:51:52 +0600

    new: added current_datetime() dokan compatibility for wp version < 5.3

--- includes/functions.php
+++ includes/functions.php
@@ -3899,3 +3899,63 @@ function dokan_is_vendor_info_hidden( $option = null ) {
 
     return ! empty( $options[ $option ] );
 }
+
+/**
+ * Function current_datetime() compatibility for wp version < 5.3
+ *
+ * @since 3.1.1
+ * @throws Exception
+ * @return DateTimeImmutable
+ */
+function dokan_current_datetime() {
+    if ( function_exists( 'current_datetime' ) ) {
+        return current_datetime();
+    }
+
+    return new DateTimeImmutable( 'now', dokan_wp_timezone() );
+}
+
+/**
+ * Function dokan_wp_timezone() compatibility for wp version < 5.3
+ *
+ * @since 3.1.1
+ *
+ * @return DateTimeZone
+ */
+function dokan_wp_timezone() {
+    if ( function_exists( 'wp_timezone' ) ) {
+        return wp_timezone();
+    }
+
+    return new DateTimeZone( dokan_wp_timezone_string() );
+}
+
+/**
+ * Function dokan_wp_timezone_string() compatibility for wp version < 5.3
+ *
+ * @since 3.1.1
+ *
+ * @return string
+ */
+function dokan_wp_timezone_string() {
+    if ( function_exists( 'wp_timezone_string' ) ) {
+        return wp_timezone_string();
+    }
+
+    $timezone_string = get_option( 'timezone_string' );
+
+    if ( $timezone_string ) {
+        return $timezone_string;
+    }
+
+    $offset  = (float) get_option( 'gmt_offset' );
+    $hours   = (int) $offset;
+    $minutes = ( $offset - $hours );
+
+    $sign      = ( $offset < 0 ) ? '-' : '+';
+    $abs_hour  = abs( $hours );
+    $abs_mins  = abs( $minutes * 60 );
+    $tz_offset = sprintf( '%s%02d:%02d', $sign, $abs_hour, $abs_mins );
+
+    return $tz_offset;
+}
