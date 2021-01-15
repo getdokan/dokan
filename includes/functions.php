24b224408c5d9725e1062134853a6f159638e759
Nurul Umbhiya <zikubd@gmail.com>
2021-01-15 17:28:20 +0600

    fix: handle warnings in case of invalid date provided

--- includes/functions.php
+++ includes/functions.php
@@ -3982,12 +3982,8 @@ function dokan_format_date( $date = '', $format = false ) {
     }
 
     // if date is not timestamp, convert it to timestamp
-    try {
-        if ( ! is_numeric( $date ) ) {
-            $date = dokan_current_datetime()->modify( $date )->getTimestamp();
-        }
-    } catch( Exception $exception ) {
-        $date = null;
+    if ( ! is_numeric( $date ) && strtotime( $date ) ) {
+        $date = dokan_current_datetime()->modify( $date )->getTimestamp();
     }
 
     if ( function_exists( 'wp_date' ) ) {
