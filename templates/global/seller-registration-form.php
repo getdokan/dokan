a77c83d98168402d03fc12831f7afd4af4f78143
Shams Sadek <shamssadek@gmail.com>
2021-01-20 09:55:57 +0600

    added whitespace after negation sign

--- templates/global/seller-registration-form.php
+++ templates/global/seller-registration-form.php
@@ -11,14 +11,14 @@
     <div class="split-row form-row-wide">
         <p class="form-row form-group">
             <label for="first-name"><?php esc_html_e( 'First Name', 'dokan-lite' ); ?> <span class="required">*</span></label>
-            <input type="text" class="input-text form-control" name="fname" id="first-name" value="<?php if ( !empty( $postdata['fname'] ) ) {
+            <input type="text" class="input-text form-control" name="fname" id="first-name" value="<?php if ( ! empty( $postdata['fname'] ) ) {
     echo esc_attr( $postdata['fname'] );
 } ?>" required="required" />
         </p>
 
         <p class="form-row form-group">
             <label for="last-name"><?php esc_html_e( 'Last Name', 'dokan-lite' ); ?> <span class="required">*</span></label>
-            <input type="text" class="input-text form-control" name="lname" id="last-name" value="<?php if ( !empty( $postdata['lname'] ) ) {
+            <input type="text" class="input-text form-control" name="lname" id="last-name" value="<?php if ( ! empty( $postdata['lname'] ) ) {
     echo esc_attr( $postdata['lname'] );
 } ?>" required="required" />
         </p>
@@ -26,7 +26,7 @@
 
     <p class="form-row form-group form-row-wide">
         <label for="company-name"><?php esc_html_e( 'Shop Name', 'dokan-lite' ); ?> <span class="required">*</span></label>
-        <input type="text" class="input-text form-control" name="shopname" id="company-name" value="<?php if ( !empty( $postdata['shopname'] ) ) {
+        <input type="text" class="input-text form-control" name="shopname" id="company-name" value="<?php if ( ! empty( $postdata['shopname'] ) ) {
     echo esc_attr( $postdata['shopname'] );
 } ?>" required="required" />
     </p>
@@ -34,7 +34,7 @@
     <p class="form-row form-group form-row-wide">
         <label for="seller-url" class="pull-left"><?php esc_html_e( 'Shop URL', 'dokan-lite' ); ?> <span class="required">*</span></label>
         <strong id="url-alart-mgs" class="pull-right"></strong>
-        <input type="text" class="input-text form-control" name="shopurl" id="seller-url" value="<?php if ( !empty( $postdata['shopurl'] ) ) {
+        <input type="text" class="input-text form-control" name="shopurl" id="seller-url" value="<?php if ( ! empty( $postdata['shopurl'] ) ) {
     echo esc_attr( $postdata['shopurl'] );
 } ?>" required="required" />
         <small><?php echo esc_url( home_url() . '/' . dokan_get_option( 'custom_store_url', 'dokan_general', 'store' ) ); ?>/<strong id="url-alart"></strong></small>
@@ -42,7 +42,7 @@
 
     <p class="form-row form-group form-row-wide">
         <label for="shop-phone"><?php esc_html_e( 'Phone Number', 'dokan-lite' ); ?><span class="required">*</span></label>
-        <input type="text" class="input-text form-control" name="phone" id="shop-phone" value="<?php if ( !empty( $postdata['phone'] ) ) {
+        <input type="text" class="input-text form-control" name="phone" id="shop-phone" value="<?php if ( ! empty( $postdata['phone'] ) ) {
     echo esc_attr( $postdata['phone'] );
 } ?>" required="required" />
     </p>
