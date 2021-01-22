0b9e54cd424539fc6eb87a515feeeb01b5ab07b3
Alamgir Hossain <alamgircse.bd@gmail.com>
2021-01-22 17:23:10 +0600

    new: Suggestion related to Update Settings button #93 (#1053)
    * new: Suggestion related to Update Settings button #93
    
    * - After upload profile image show click to update button message
    
    * - Added codes more optimized
    
    * - Show alert when vendor setting page leave without update the form
    
    * new: Store setting from update button added top header area
    
    * - Improved some codes

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -123,6 +123,10 @@ jQuery(function($) {
       $('a.dokan-gravatar-drag').on('click', this.simpleImageUpload);
       $('a.dokan-remove-gravatar-image').on('click', this.removeGravatar);
 
+      $('.dokan-update-setting-top-button').click(function(){
+          $("input[name='dokan_update_store_settings']").click();
+      });
+
       this.validateForm(self);
 
       return false;
@@ -216,6 +220,7 @@ jQuery(function($) {
     },
 
     setImageFromURL: function(url, attachmentId, width, height) {
+      var banner_profile_upload_status = false;
       if ($(this.uploadBtn).hasClass('dokan-banner-drag')) {
         var wrap = $(this.uploadBtn).closest('.dokan-banner');
 
@@ -230,11 +235,16 @@ jQuery(function($) {
         $(this.uploadBtn)
           .parent('.button-area')
           .addClass('dokan-hide');
+
+        banner_profile_upload_status = true;
+
       } else if ($(this.uploadBtn).hasClass('dokan-pro-gravatar-drag')) {
         var wrap = $(this.uploadBtn).closest('.dokan-gravatar');
 
         wrap.find('input.dokan-file-field').val(attachmentId);
         wrap.find('img.dokan-gravatar-img').attr('src', url);
+
+        banner_profile_upload_status = true;
 
         $(this.uploadBtn)
           .parent()
@@ -244,6 +254,19 @@ jQuery(function($) {
         $(this.uploadBtn)
           .parent('.gravatar-button-area')
           .addClass('dokan-hide');
+      }
+
+      if ( banner_profile_upload_status === true ) {
+        $(window).on("beforeunload", function() {
+          return dokan.dokan_banner_added_alert_msg;
+        });
+        
+        $(document).ready(function() {
+          $("#store-form").on("submit", function(e) {
+            $(window).off("beforeunload");
+            return true;
+          });
+        });
       }
     },
 
@@ -447,8 +470,10 @@ jQuery(function($) {
           self.serialize() + '&action=dokan_settings&form_id=' + form_id;
 
       self.find('.ajax_prev').append('<span class="dokan-loading"> </span>');
+      $('.dokan-update-setting-top-button').append('<span class="dokan-loading"> </span>');
       $.post(dokan.ajaxurl, form_data, function(resp) {
         self.find('span.dokan-loading').remove();
+        $('.dokan-update-setting-top-button span.dokan-loading').remove();
         $('html,body').animate({ scrollTop: 100 });
 
         if (resp.success) {
