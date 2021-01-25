84dd89d8b44ba47a0aedb6587b7e38663418a633
Shams Sadek <shamssadek@gmail.com>
2021-01-25 14:46:43 +0600

    Fix:Site registration as vendor ghost touch fixes #998 (#1052)
    * Fix:Site registration as vendor ghost touch fixes #998
    
    * fix: added css in login-form-popup.less
    
    * fixed php-cs error
    
    * added whitespace after negation sign
    
    * fix: vendor registration form css issue fixed
    
    * style: deleted comments out of unnecessary lines

--- templates/global/seller-registration-form.php
+++ templates/global/seller-registration-form.php
@@ -3,8 +3,6 @@
  * Dokan Seller registration form
  *
  * @since 2.4
- *
- * @package dokan
  */
 ?>
 
@@ -13,30 +11,30 @@
     <div class="split-row form-row-wide">
         <p class="form-row form-group">
             <label for="first-name"><?php esc_html_e( 'First Name', 'dokan-lite' ); ?> <span class="required">*</span></label>
-            <input type="text" class="input-text form-control" name="fname" id="first-name" value="<?php if ( ! empty( $postdata['fname'] ) ) echo esc_attr($postdata['fname']); ?>" required="required" />
+            <input type="text" class="input-text form-control" name="fname" id="first-name" value="<?php if ( ! empty( $postdata['fname'] ) ) { echo esc_attr( $postdata['fname'] ); } ?>" required="required" />
         </p>
 
         <p class="form-row form-group">
             <label for="last-name"><?php esc_html_e( 'Last Name', 'dokan-lite' ); ?> <span class="required">*</span></label>
-            <input type="text" class="input-text form-control" name="lname" id="last-name" value="<?php if ( ! empty( $postdata['lname'] ) ) echo esc_attr($postdata['lname']); ?>" required="required" />
+            <input type="text" class="input-text form-control" name="lname" id="last-name" value="<?php if ( ! empty( $postdata['lname'] ) ) { echo esc_attr( $postdata['lname'] ); } ?>" required="required" />
         </p>
     </div>
 
     <p class="form-row form-group form-row-wide">
         <label for="company-name"><?php esc_html_e( 'Shop Name', 'dokan-lite' ); ?> <span class="required">*</span></label>
-        <input type="text" class="input-text form-control" name="shopname" id="company-name" value="<?php if ( ! empty( $postdata['shopname'] ) ) echo esc_attr($postdata['shopname']); ?>" required="required" />
+        <input type="text" class="input-text form-control" name="shopname" id="company-name" value="<?php if ( ! empty( $postdata['shopname'] ) ) { echo esc_attr( $postdata['shopname'] ); } ?>" required="required" />
     </p>
 
     <p class="form-row form-group form-row-wide">
         <label for="seller-url" class="pull-left"><?php esc_html_e( 'Shop URL', 'dokan-lite' ); ?> <span class="required">*</span></label>
         <strong id="url-alart-mgs" class="pull-right"></strong>
-        <input type="text" class="input-text form-control" name="shopurl" id="seller-url" value="<?php if ( ! empty( $postdata['shopurl'] ) ) echo esc_attr($postdata['shopurl']); ?>" required="required" />
+        <input type="text" class="input-text form-control" name="shopurl" id="seller-url" value="<?php if ( ! empty( $postdata['shopurl'] ) ) { echo esc_attr( $postdata['shopurl'] ); } ?>" required="required" />
         <small><?php echo esc_url( home_url() . '/' . dokan_get_option( 'custom_store_url', 'dokan_general', 'store' ) ); ?>/<strong id="url-alart"></strong></small>
     </p>
 
     <p class="form-row form-group form-row-wide">
         <label for="shop-phone"><?php esc_html_e( 'Phone Number', 'dokan-lite' ); ?><span class="required">*</span></label>
-        <input type="text" class="input-text form-control" name="phone" id="shop-phone" value="<?php if ( ! empty( $postdata['phone'] ) ) echo esc_attr($postdata['phone']); ?>" required="required" />
+        <input type="text" class="input-text form-control" name="phone" id="shop-phone" value="<?php if ( ! empty( $postdata['phone'] ) ) { echo esc_attr( $postdata['phone'] ); } ?>" required="required" />
     </p>
 
     <?php
@@ -56,15 +54,17 @@
 
 <?php do_action( 'dokan_reg_form_field' ); ?>
 
-<p class="form-row form-group user-role">
+<p class="form-row form-group user-role vendor-customer-registration">
+    
     <label class="radio">
         <input type="radio" name="role" value="customer"<?php checked( $role, 'customer' ); ?>>
         <?php esc_html_e( 'I am a customer', 'dokan-lite' ); ?>
     </label>
-
+    <br/>
     <label class="radio">
         <input type="radio" name="role" value="seller"<?php checked( $role, 'seller' ); ?>>
         <?php esc_html_e( 'I am a vendor', 'dokan-lite' ); ?>
     </label>
     <?php do_action( 'dokan_registration_form_role', $role ); ?>
+
 </p>
