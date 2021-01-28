4df6e501be70c3bbdea16a53a493edb6b65e5d60
Alamgir Hossain <alamgircse.bd@gmail.com>
2021-01-28 23:32:01 +0600

    - PHPCS error fixed

--- includes/Shortcodes/BestSellingProduct.php
+++ includes/Shortcodes/BestSellingProduct.php
@@ -15,7 +15,7 @@ class BestSellingProduct extends DokanShortcode {
      *
      * @return string
      */
-    function render_shortcode( $atts ) {
+    public function render_shortcode( $atts ) {
         /**
         * Filter return the number of best selling product per page.
         *
