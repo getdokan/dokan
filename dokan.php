6455137f52094ddb91231aa5e38ca1e62600e9bc
Nurul Umbhiya <zikubd@gmail.com>
2021-02-12 18:20:32 +0600

    chore: Bump version to 3.2.1

--- dokan.php
+++ dokan.php
@@ -3,12 +3,12 @@
  * Plugin Name: Dokan
  * Plugin URI: https://wordpress.org/plugins/dokan-lite/
  * Description: An e-commerce marketplace plugin for WordPress. Powered by WooCommerce and weDevs.
- * Version: 3.2.0
+ * Version: 3.2.1
  * Author: weDevs
  * Author URI: https://wedevs.com/
  * Text Domain: dokan-lite
  * WC requires at least: 3.0
- * WC tested up to: 4.9.2
+ * WC tested up to: 5.0.0
  * Domain Path: /languages/
  * License: GPL2
  */
@@ -56,7 +56,7 @@ final class WeDevs_Dokan {
      *
      * @var string
      */
-    public $version = '3.2.0';
+    public $version = '3.2.1';
 
     /**
      * Instance of self
