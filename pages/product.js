1c7d67eaacec0592a9e9fccf139df6b252ad0d07
Shanjidah Afroz <wedevs@weDevss-Air-3.localdomain>
2021-02-12 15:03:56 +0600

    Variable product exploratory testing

--- pages/product.js
+++ pages/product.js
@@ -19,6 +19,7 @@ module.exports = {
  {
          // I.click(this.fields.viewProduct);
          // I.dontSee(this.fields.alertMassage);
+         I.wait(10);
          I.click(this.fields.adddProduct);
          I.fillField(this.fields.addTitle,title);
          I.fillField(this.fields.productPrice, price);
@@ -30,11 +31,40 @@ module.exports = {
  
  
  },
- deleteproduct()
+ createvariation()
  {
+  I.click({css:'.variation-topbar-heading > .dokan-clearfix'});
+  I.selectOption({css:'.variation-topbar-heading > select:nth-child(2)'},'green');
+  I.click({css:'.variation-topbar-heading > select:nth-child(2)'});
+  I.fillField('variable_sku[0]','10');
+  I.fillField('variable_regular_price[0]','400');
+  I.fillField('//input[@name="variable_weight[0]"]',faker.random.number());
+  I.fillField('//input[@id="product_length[0]"]',faker.random.number());
+  I.fillField({css:'//input[@name="variable_width[0]"]'},faker.random.number());
+  I.fillField('//input[@name="variable_height[0]"]',faker.random.number());
+  I.selectOption('//select[@id="variable_shipping_class[0]"]','Flat rate');
+  I.fillField('//textarea[@name="variable_description[0]"]',faker.random.words());
+  I.click('Save Variations');
+  
    
  },
- 
+ clearvariationproduct()
+ {
+  I.click({css:'.variation-topbar-heading > .dokan-clearfix'});
+  I.selectOption({css:'.variation-topbar-heading > select:nth-child(2)'},'green');
+  I.click({css:'.variation-topbar-heading > select:nth-child(2)'});
+  I.click({css:'.variation-topbar-heading > select:nth-child(2)'});
+  I.wait(5);
+  I.clearField('variable_sku[0]');
+  I.clearField('variable_regular_price[0]');
+  I.clearField('//input[@name="variable_weight[0]"]');
+  I.clearField('//input[@id="product_length[0]"]');
+  I.clearField({css:'//input[@name="variable_width[0]"]'});
+  I.clearField('//input[@name="variable_height[0]"]');
+  I.clearField('//textarea[@name="variable_description[0]"]');
+  I.click('Save Variations');
+
+ }
 
   // insert your locators and methods here
 }
