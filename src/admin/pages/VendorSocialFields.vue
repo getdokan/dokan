560206cfaa9a589a504c1b126b9e54229742dfaa
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2021-02-25 10:01:02 +0600

    fix: gplus cleaning

--- src/admin/pages/VendorSocialFields.vue
+++ src/admin/pages/VendorSocialFields.vue
@@ -8,43 +8,38 @@
 
             <div class="dokan-form-group">
                 <div class="column">
-                    <label for="">{{ __( 'Facebook', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.fb" :placeholder="__( 'https://example.com' )">
+                    <label for="facebook">{{ __( 'Facebook', 'dokan-lite' ) }}</label>
+                    <input id="facebook" type="text" class="dokan-form-input" v-model="vendorInfo.social.fb" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <div class="column">
-                    <label for="">{{ __( 'Flickr', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.flickr" :placeholder="__( 'https://example.com' )">
+                    <label for="flickr">{{ __( 'Flickr', 'dokan-lite' ) }}</label>
+                    <input id="flickr" type="text" class="dokan-form-input" v-model="vendorInfo.social.flickr" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <div class="column">
-                    <label for="">{{ __( 'Google Plus', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.gplus" :placeholder="__( 'https://example.com' )">
+                    <label for="twitter">{{ __( 'Twitter', 'dokan-lite' ) }}</label>
+                    <input id="twitter" type="text" class="dokan-form-input" v-model="vendorInfo.social.twitter" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <div class="column">
-                    <label for="">{{ __( 'Twitter', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.twitter" :placeholder="__( 'https://example.com' )">
+                    <label for="youtube">{{ __( 'Youtube', 'dokan-lite' ) }}</label>
+                    <input id="youtube" type="text" class="dokan-form-input" v-model="vendorInfo.social.youtube" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <div class="column">
-                    <label for="">{{ __( 'Youtube', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.youtube" :placeholder="__( 'https://example.com' )">
+                    <label for="linkedin">{{ __( 'Linkedin', 'dokan-lite' ) }}</label>
+                    <input id="linkedin" type="text" class="dokan-form-input" v-model="vendorInfo.social.linkedin" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <div class="column">
-                    <label for="">{{ __( 'Linkedin', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.linkedin" :placeholder="__( 'https://example.com' )">
-                </div>
-
-                <div class="column">
-                    <label for="">{{ __( 'Pinterest', 'dokan-lite' ) }}</label>
+                    <label id="pinterest" for="pinterest">{{ __( 'Pinterest', 'dokan-lite' ) }}</label>
                     <input type="text" class="dokan-form-input" v-model="vendorInfo.social.pinterest" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <div class="column">
-                    <label for="">{{ __( 'Instagram', 'dokan-lite' ) }}</label>
-                    <input type="text" class="dokan-form-input" v-model="vendorInfo.social.instagram" :placeholder="__( 'https://example.com' )">
+                    <label for="instagram">{{ __( 'Instagram', 'dokan-lite' ) }}</label>
+                    <input id="instagram" type="text" class="dokan-form-input" v-model="vendorInfo.social.instagram" :placeholder="__( 'https://example.com' )">
                 </div>
 
                 <!-- Add other social fields here -->
