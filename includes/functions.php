4835737cff69d83d069cc8ce31e132afe1541fcd
Shazahanul Islam Shohag <shohag121@gmail.com>
2021-03-03 11:37:32 +0600

    fix: different edit url for different product status

--- includes/functions.php
+++ includes/functions.php
@@ -983,17 +983,15 @@ function dokan_edit_product_url( $product ) {
         return false;
     }
 
-    if ( 'publish' === $product->get_status() ) {
-        $url = trailingslashit( get_permalink( $product->get_id() ) ) . 'edit/';
-    } else {
-        $url = add_query_arg(
-            [
-                'product_id' => $product->get_id(),
-                'action'     => 'edit',
-            ],
-            dokan_get_navigation_url( 'products' )
-        );
-    }
+
+    $url = add_query_arg(
+        [
+            'product_id' => $product->get_id(),
+            'action'     => 'edit',
+        ],
+        dokan_get_navigation_url( 'products' )
+    );
+
 
     return apply_filters( 'dokan_get_edit_product_url', $url, $product );
 }
