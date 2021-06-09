69f69a8603973c3faf8502037e4f7703ddae69a6
Alamgir Hossain <alamgircse.bd@gmail.com>
2021-06-09 17:25:37 +0600

    - Some design changed

--- includes/Product/Hooks.php
+++ includes/Product/Hooks.php
@@ -66,14 +66,14 @@ class Hooks {
         ?>
         <div class="dokan-store-products-filter-area dokan-clearfix">
             <form class="dokan-store-products-ordeby" method="get">
-                <input type="text" name="product_name" class="product-name-search" placeholder="<?php esc_attr_e( 'Enter product name', 'dokan-lite' ); ?>">
-                <select name="product_orderby" class="orderby orderby-search" aria-label="<?php esc_attr_e( 'Shop order', 'dokan-lite' ); ?>">
+                <select name="product_orderby" class="orderby orderby-search" aria-label="<?php esc_attr_e( 'Shop order', 'dokan-lite' ); ?>" onchange='if(this.value != 0) { this.form.submit(); }'>
                     <?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
                         <option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
                     <?php endforeach; ?>
                 </select>
-                <input type="hidden" name="paged" value="1" />
                 <input type="submit" name="search_store_products" class="search-store-products dokan-btn-theme" value="<?php esc_attr_e( 'Search', 'dokan-lite' ); ?>">
+                <input type="text" name="product_name" class="product-name-search" placeholder="<?php esc_attr_e( 'Enter product name', 'dokan-lite' ); ?>">
+                <input type="hidden" name="paged" value="1" />
             </form>
         </div>
         <?php
