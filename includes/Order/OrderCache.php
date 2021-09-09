3973a75e01c3efe304cef41f29d2595c882232c8
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-09 09:33:54 +0600

    Fixed Order Caching Issues

--- includes/Order/OrderCache.php
+++ includes/Order/OrderCache.php
@@ -14,9 +14,9 @@ use WeDevs\Dokan\Cache\CacheHelper;
 class OrderCache extends CacheHelper {
 
     public function __construct() {
-        add_action( 'dokan_checkout_update_order_meta', [ $this, 'dokan_cache_reset_seller_order_data' ], 10, 2 );
-        add_action( 'woocommerce_order_status_changed', [ $this, 'dokan_cache_reset_order_data_on_status' ], 10, 4 );
-        add_action( 'woocommerce_update_order', [ $this, 'dokan_cache_reset_on_update_order' ] );
+        add_action( 'dokan_checkout_update_order_meta', [ $this, 'reset_seller_order_data' ], 10, 2 );
+        add_action( 'woocommerce_order_status_changed', [ $this, 'reset_seller_order_data' ], 10, 4 );
+        add_action( 'woocommerce_update_order', [ $this, 'reset_cache_on_update_order' ] );
     }
 
     /**
@@ -29,29 +29,12 @@ class OrderCache extends CacheHelper {
      *
      * @return void
      */
-    public static function dokan_cache_reset_seller_order_data( $order_id, $seller_id ) {
-        self::invalidate_cache_group( 'dokan_seller_data_' . $seller_id );
+    public static function reset_seller_order_data( $order_id, $seller_id ) {
+        self::dokan_cache_clear_group( 'dokan_seller_data_' . $seller_id );
     }
 
     /**
-     * Reset order data on change status
-     *
-     * @since DOKAN_LITE_SINCE
-     *
-     * @param int $order_id
-     * @param string $from_status
-     * @param string $to_status
-     * @param WC_Order $order
-     *
-     * @return void
-     */
-    public static function dokan_cache_reset_order_data_on_status( $order_id, $from_status, $to_status, $order ) {
-        $seller_id = dokan_get_seller_id_by_order( $order_id );
-        self::invalidate_cache_group( 'dokan_seller_data_' . $seller_id );
-    }
-
-    /**
-     * Reset order data on update order
+     * Reset cache data on update woocomerce order
      *
      * @since DOKAN_LITE_SINCE
      *
@@ -59,8 +42,9 @@ class OrderCache extends CacheHelper {
      *
      * @return void
      */
-    public static function dokan_cache_reset_on_update_order( $order_id ) {
+    public static function reset_cache_on_update_order( $order_id ) {
         $seller_id = dokan_get_seller_id_by_order( $order_id );
-        wp_cache_delete('dokan-count-orders-' . $seller_id, 'dokan_seller_data_' . $seller_id);
+
+        self::reset_seller_order_data( $order_id, $seller_id );
     }
 }
