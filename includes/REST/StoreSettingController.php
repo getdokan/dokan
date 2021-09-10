f35eccc7ca46ea32254c00a1ddf53c155fe92463
Alamgir Hossain <alamgircse.bd@gmail.com>
2021-09-10 11:03:37 +0600

    new: Vendor settings update REST api #1192 (#1319)
    * new: Vendor settings update REST api #1192
    
    * - Some warnings fixed
    
    * - Requested changes fixed
    
    * - Requested changes fixed
    
    * - Fixed some phpcs errors

--- includes/REST/StoreSettingController.php
+++ includes/REST/StoreSettingController.php
@@ -41,8 +41,41 @@ class StoreSettingController extends WP_REST_Controller {
 					'callback'            => [ $this, 'get_settings' ],
 					'permission_callback' => [ $this, 'get_settings_permission_callback' ],
 				],
+                [
+                    'methods'             => WP_REST_Server::EDITABLE,
+                    'callback'            => [ $this, 'update_settings' ],
+                    'permission_callback' => [ $this, 'get_settings_permission_callback' ],
+                ],
 			]
         );
+    }
+
+    /**
+     * Update Store
+     *
+     * @param \WP_REST_Request $request
+     *
+     * @since DOKAN_LITE_SINCE
+     *
+     * @return WP_Error|\WP_REST_Response
+     */
+    public function update_settings( $request ) {
+        $vendor   = $this->get_vendor();
+        $params   = $request->get_params();
+        $store_id = dokan()->vendor->update( $vendor->get_id(), $params );
+
+        if ( is_wp_error( $store_id ) ) {
+            return new WP_Error( $store_id->get_error_code(), $store_id->get_error_message() );
+        }
+
+        $store = dokan()->vendor->get( $store_id );
+
+        do_action( 'dokan_rest_store_settings_after_update', $store, $request );
+
+        $stores_data = $this->prepare_item_for_response( $store, $request );
+        $response    = rest_ensure_response( $stores_data );
+
+        return $response;
     }
 
     /**
@@ -64,8 +97,13 @@ class StoreSettingController extends WP_REST_Controller {
      */
     public function get_settings_permission_callback() {
         $vendor = $this->get_vendor();
+
         if ( is_wp_error( $vendor ) ) {
             return $vendor;
+        }
+
+        if ( empty( $vendor->get_id() ) ) {
+            return new WP_Error( 'no_store_found', __( 'No vendor found', 'dokan-lite' ), [ 'status' => 404 ] );
         }
 
         return true;
@@ -83,10 +121,48 @@ class StoreSettingController extends WP_REST_Controller {
         }
 
         if ( ! $current_user ) {
-            return new WP_Error( 'Unauthorized', __( 'You are not logged in' ), [ 'code' => 401 ] );
+            return new WP_Error( 'Unauthorized', __( 'You are not logged in', 'dokan-lite' ), [ 'code' => 401 ] );
         }
 
         return $vendor;
     }
 
+    /**
+     * Prepare links for the request.
+     *
+     * @param \WC_Data $object Object data.
+     * @param \WP_REST_Request $request Request object.
+     *
+     * @return array Links for the given post.
+     */
+    protected function prepare_links( $object, $request ) {
+        $links = [
+            'self' => [
+                'href' => rest_url( sprintf( '/%s/%s/%d', $this->namespace, $this->rest_base, $object['id'] ) ),
+            ],
+            'collection' => [
+                'href' => rest_url( sprintf( '/%s/%s', $this->namespace, $this->rest_base ) ),
+            ],
+        ];
+
+        return $links;
+    }
+
+    /**
+     * Prepare a single item output for response
+     *
+     * @param $store
+     * @param \WP_REST_Request $request Request object.
+     * @param array $additional_fields (optional)
+     *
+     * @return \WP_REST_Response $response Response data.
+     */
+    public function prepare_item_for_response( $store, $request, $additional_fields = [] ) {
+        $data     = $store->to_array();
+        $data     = array_merge( $data, apply_filters( 'dokan_rest_store_settings_additional_fields', $additional_fields, $store, $request ) );
+        $response = rest_ensure_response( $data );
+        $response->add_links( $this->prepare_links( $data, $request ) );
+
+        return apply_filters( 'dokan_rest_prepare_store_settings_item_for_response', $response );
+    }
 }
