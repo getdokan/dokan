7e94c7716385ce56d2a0269e3ffa734419424ca7
Md. Sadikur Rahaman <sadik7070@gmail.com>
2021-09-10 06:42:37 +0600

    fixed: dokan_store_name meta missing (#1313)
    * fixed: dokan_store_name meta missing
    
    * DokanUpgrader version renamed
    
    * phpcs fixed

---
+++ includes/Upgrade/Upgrades/V_3_2_12.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WeDevs\Dokan\Upgrade\Upgrades;
+
+use WeDevs\Dokan\Abstracts\DokanUpgrader;
+use WeDevs\Dokan\Upgrade\Upgrades\BackgroundProcesses\V_2_9_23_StoreName;
+
+class V_3_2_12 extends DokanUpgrader {
+
+    /**
+     * Update store name meta data
+     *
+     * @since 3.2.12
+     *
+     * @return void
+     */
+    public static function dokan_update_store_name() {
+        $processor = new V_2_9_23_StoreName();
+
+        $args = [
+            'updating' => 'store_name',
+            'paged'    => 0,
+        ];
+
+        $processor->push_to_queue( $args )->dispatch_process();
+    }
+
+}
--- includes/Vendor/Manager.php
+++ includes/Vendor/Manager.php
@@ -328,6 +328,7 @@ class Manager {
         // update vendor store data
         if ( ! empty( $data['store_name'] ) ) {
             $vendor->set_store_name( $data['store_name'] );
+            $vendor->update_meta( 'dokan_store_name', $data['store_name'] );
         }
 
         if ( ! empty( $data['phone'] ) ) {
