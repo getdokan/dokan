970ecb32c5fa0bd31f8cae2a00c488b7fc7e4674
abdulhadicse <abdulhadi.aust@gmail.com>
2021-09-22 17:26:25 +0600

    code modified

--- templates/global/product-tab.php
+++ templates/global/product-tab.php
@@ -13,7 +13,7 @@
 <ul class="list-unstyled">
     <?php do_action( 'dokan_product_seller_tab_start', $author, $store_info ); ?>
 
-    <?php if ( !empty( $store_info['store_name'] ) ) { ?>
+    <?php if ( !empty( $store_info['store_name'] ) ) : ?>
         <li class="store-name">
             <span><?php esc_html_e( 'Store Name:', 'dokan-lite' ); ?></span>
             <span class="details">
@@ -26,10 +26,10 @@
             </span>
 
             <span class="details">
-                <?php printf( '<a href="%s">%s</a>', esc_url( dokan_get_store_url( $author->ID ) ), esc_attr( $store_info['store_name'] ) ); ?>
+                <?php printf( '<a href="%1$s">%2$s</a>', esc_url( dokan_get_store_url( $author->ID ) ), esc_html( $store_info['store_name'] ) ); ?>
             </span>
         </li>
-    <?php } ?>
+    <?php endif; ?>
 
     <?php if ( ! dokan_is_vendor_info_hidden( 'address' ) && ! empty( $store_info['address'] ) ) { ?>
         <li class="store-address">
