13e6605626dd42099195fedc9ded4bc6dbb66c76
ratulhasan <ratuljh@gmail.com>
2021-10-08 09:04:02 +0600

    spelling corrected.

--- includes/PageViews.php
+++ includes/PageViews.php
@@ -26,17 +26,20 @@ class PageViews {
 				if(localStorage){
                     var date = new Date();
                     var new_date = date.getDate();
-                    var dokan_pageview_conut = JSON.parse(localStorage.getItem("dokan_pageview_conut"));
+                    var dokan_pageview_count = JSON.parse(localStorage.getItem("dokan_pageview_count"));
 					var storeData = [];
 					var post_id = [];
 
-					if ( dokan_pageview_conut && dokan_pageview_conut[0].post_id && dokan_pageview_conut[0].today === new_date ){
-						post_id = dokan_pageview_conut[0].post_id;
+					if ( dokan_pageview_count ){
+					    if (dokan_pageview_count[0].post_id && dokan_pageview_count[0].today === new_date){
+						    post_id = dokan_pageview_count[0].post_id;
+						}
+						if ( dokan_pageview_count[0].today !== new_date ){
+                            post_id = [];
+                        }
 					}
-					if ( dokan_pageview_conut[0].today !== new_date ){
-						post_id = [];
-					}
-                    if ( dokan_pageview_conut == null || dokan_pageview_conut[0].today !== new_date || ! dokan_pageview_conut[0].post_id.includes( ' . get_the_ID() . ' )  ) {
+
+                    if ( dokan_pageview_count == null || dokan_pageview_count[0].today !== new_date || ! dokan_pageview_count[0].post_id.includes( ' . get_the_ID() . ' )  ) {
 						var data = {
                             action: "dokan_pageview",
                             _ajax_nonce: "' . esc_html( $nonce ) . '",
@@ -45,7 +48,7 @@ class PageViews {
                         $.post( "' . esc_url( admin_url( 'admin-ajax.php' ) ) . '", data );
 						post_id.push(' . get_the_ID() . ');
                         storeData.push({"today": new_date, "post_id" : post_id});
-                        localStorage.setItem("dokan_pageview_conut", JSON.stringify(storeData));
+                        localStorage.setItem("dokan_pageview_count", JSON.stringify(storeData));
                     }
 				}
             } );
