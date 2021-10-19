6b307bd4d56d8dfcebbc2617c38cb67e4a760ddf
Asad Nur <asaduzzamannur.dev@gmail.com>
2021-10-19 23:52:33 +0600

    new: add postpond and dismiss interaciton with ajax to the ask for review admin dashboard notice

--- includes/Admin/ReviewNotice.php
+++ includes/Admin/ReviewNotice.php
@@ -18,6 +18,7 @@ class ReviewNotice {
      */
     public function __construct() {
         add_action( 'admin_notices', [ $this, 'show_ask_for_review_notice' ], 0, 1 );
+        add_action( 'wp_ajax_ask_for_review_admin_notice', [ $this, 'review_notice_action_handler' ] );
     }
 
     /**
@@ -45,7 +46,7 @@ class ReviewNotice {
         $current_time  = strtotime( 'now' );
         $install_time  = get_option( 'dokan_installed_time' );
         $eligible_time = strtotime( '10 days', $install_time );
-        $postpond_days = 7;
+        $postpond_days = 15;
         $postpond_time = get_option( 'dokan_review_notice_postpond_time', 0 );
 
         if ( ! empty( get_option( 'dokan_review_notice_postpond' ) ) ) {
@@ -113,6 +114,86 @@ class ReviewNotice {
                 cursor: pointer;
             }
         </style>
+
+        <script type="text/javascript">
+            ;(function($) {
+
+                /**
+                * Dokan ask for reveiw admin notice
+                */
+                let Dokan_ask_for_reveiw_notice = {
+                    init : function () {
+                        $( '.dokan-review-notice .dokan-notice-action' ).on( 'click', function( e ) {
+                            // Prevent default action
+                            if ( 'a' !== e.target.tagName.toLowerCase() ) {
+                                e.preventDefault();
+                            }
+
+                            let self = $( this );
+                            let key  = self.data( 'key' );
+
+                            wp.ajax.send( 'ask_for_review_admin_notice', {
+                                data: {
+                                    dokan_ask_for_review_notice_action: true,
+                                    key: key,
+                                    nonce: '<?php echo esc_attr( wp_create_nonce( 'dokan_admin' ) ); ?>',
+                                },
+                                complete: function ( response ) {
+                                    self.closest( '.dokan-notice' ).fadeOut( 200 );
+                                }
+                            } );
+
+                        });
+                    }
+                }
+
+                Dokan_ask_for_reveiw_notice.init();
+
+            })(jQuery);
+        </script>
         <?php
+    }
+
+    /**
+     * Reveiw notice action ajax handler
+     *
+     * @return void
+     */
+    public function review_notice_action_handler() {
+        if ( ! wp_verify_nonce( $_POST['nonce'], 'dokan_admin' ) ) {
+            wp_send_json_error( __( 'Invalid nonce', 'dokan' ) );
+        }
+
+        if ( ! current_user_can( 'manage_woocommerce' ) ) {
+            wp_send_json_error( __( 'You have no permission to do that', 'dokan' ) );
+        }
+
+        if ( empty( $_POST['dokan_ask_for_review_notice_action'] ) || empty( $_POST['key'] ) ) {
+            wp_send_json_error( __( 'Invalid request', 'dokan' ) );
+        }
+
+        $status = false;
+        if ( 'dokan-notice-dismiss' === $_POST['key'] ) {
+            // $updated[] = update_option( 'dokan_review_notice_enable', 0 );
+            $result = update_option( 'dokan_review_notice_enable', 0 );
+            $status = $result ? true : false;
+        }
+
+        if ( 'dokan-notice-postpond' === $_POST['key'] && $status === false ) {
+            $result = update_option( 'dokan_review_notice_enable', 1 );
+            $status = $result && $status !== false ? true : false;
+
+            $result = update_option( 'dokan_review_notice_postpond', 1 );
+            $status = $result && $status !== false ? true : false;
+
+            $result = update_option( 'dokan_review_notice_postpond_time', strtotime( 'now' ) );
+            $status = $result && $status !== false ? true : false;
+        }
+
+        if ( empty( $status ) ) {
+            wp_send_json_error( __( 'Request failed', 'dokan' ) );
+        }
+
+        wp_send_json_success();
     }
 }
