139aae84e53016b3d42d33f069454a59214a575f
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-11-10 17:01:23 +0600

    reformat code

--- templates/settings/payment.php
+++ templates/settings/payment.php
@@ -7,7 +7,6 @@
  * @package dokan
  */
 $has_methods = false;
-$has_methods_dropdown = false;
 
 do_action( 'dokan_payment_settings_before_form', $current_user, $profile_info ); ?>
 
@@ -20,19 +19,20 @@ do_action( 'dokan_payment_settings_before_form', $current_user, $profile_info );
             <div id="vendor-payment-method-drop-down-wrapper">
                 <div id="vendor-payment-method-drop-down">
                     <?php
-                    $icon_mismatch_map = [
+                    $has_methods_in_dropdown = false;
+                    $icon_mismatch_map    = [
                         'dokan-stripe-connect'     => 'stripe',
                         'dokan-paypal-marketplace' => 'paypal'
                     ];
-                    $mis_match_methods = [ 'stripe', 'moip' ];
-                    $unused_methods    = get_unused_payment_methods( $methods, $profile_info['payment'], $mis_match_methods );
+                    $mis_match_methods    = [ 'stripe', 'moip' ];
+                    $unused_methods       = get_unused_payment_methods( $methods, $profile_info['payment'], $mis_match_methods );
                     ?>
                     <ul>
                     <?php foreach ( $unused_methods as $method_key ) :
                         $method = dokan_withdraw_get_method( $method_key );
 
                         if ( ! empty( $method ) ) {
-                            $has_methods_dropdown = true;
+                            $has_methods_in_dropdown = true;
                         } else {
                             continue;
                         }
@@ -41,7 +41,8 @@ do_action( 'dokan_payment_settings_before_form', $current_user, $profile_info );
                             <a href="<?php echo esc_url( home_url( "dashboard/settings/payment/manage-" . $method_key ) ); ?>">
                                 <div>
                                     <img src="<?php echo esc_url( dokan_withdraw_get_method_icon( isset( $icon_mismatch_map[ $method_key ] ) ? $icon_mismatch_map[ $method_key ] : $method_key ) ); ?>" alt="<?php echo esc_attr( $method_key ); ?>" />
-                                    <span> <?php
+                                    <span>
+                                        <?php
                                         //translators: %s: payment method title
                                         echo sprintf( __( 'Direct to %s', 'dokan-lite' ), esc_html__( $method['title'], 'dokan-lite' ) );
                                         ?>
@@ -52,9 +53,9 @@ do_action( 'dokan_payment_settings_before_form', $current_user, $profile_info );
                     <?php endforeach; ?>
                     </ul>
 
-                    <?php if ( ! $has_methods_dropdown ) : ?>
+                    <?php if ( ! $has_methods_in_dropdown ) : ?>
                         <div>
-                            <?php esc_html_e( 'All payment methods are used', 'dokan-lite' ); ?>
+                            <?php esc_html_e( 'All payment methods are used.', 'dokan-lite' ); ?>
                         </div>
                     <?php endif; ?>
                 </div>
@@ -99,13 +100,14 @@ do_action( 'dokan_payment_settings_before_form', $current_user, $profile_info );
                     </span>
                 </div>
                 <div>
-                    <?php if ( $default_method === $method_key ) {?>
+                    <?php if ( $default_method === $method_key ) : ?>
                         <button disabled tabindex="-1" class="dokan-btn-sm"><?php esc_html_e( 'Default', 'dokan-lite' ); ?></button>
-                    <?php } else { ?>
+                    <?php else : ?>
                         <button class="dokan-btn-sm dokan-btn-success" data-dokan-payment-method="<?php echo esc_attr( $method_key ) ?>">
                             <?php esc_html_e( 'Make Default', 'dokan-lite' ); ?>
                         </button>
-                    <?php } ?>
+                    <?php endif; ?>
+
                     <a href="<?php echo esc_url(  home_url( "dashboard/settings/payment/manage-" . $method_key . "/edit" ) ); ?>">
                         <button class="dokan-btn-theme dokan-btn-sm"><?php esc_html_e( 'Manage', 'dokan-lite' ); ?></button>
                     </a>
