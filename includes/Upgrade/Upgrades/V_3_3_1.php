57a595c128093156be7589b22f5d9233f20010fc
Rajan Karmaker <rajankarmakar@yahoo.com>
2021-11-11 15:58:01 +0600

    Fix/update refund and withdraw database table column (#1391)
    * fix/update withdraw and refund table
    
    * fix/update refund and withdraw database table column
    
    * fix/refund withdraw table column
    
    * fix/update minor issue
    
    * fix: necessary changes are made
    
    * fix: phpcs updated
    
    * fix/Update code

---
+++ includes/Upgrade/Upgrades/V_3_3_1.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace WeDevs\Dokan\Upgrade\Upgrades;
+
+use WeDevs\Dokan\Abstracts\DokanUpgrader;
+
+class V_3_3_1 extends DokanUpgrader {
+
+    /**
+     * Updates withdraw database table column
+     *
+     * @since 3.3.1
+     *
+     * @return void
+     */
+    public static function update_withdraw_table_column() {
+        global $wpdb;
+
+        include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+        $existing_columns = $wpdb->get_col( "DESC `{$wpdb->prefix}dokan_withdraw`", 0 );
+
+        if ( in_array( 'details', $existing_columns, true ) ) {
+            return;
+        }
+
+        $wpdb->query(
+            "ALTER TABLE `{$wpdb->prefix}dokan_withdraw` ADD COLUMN `details` longtext AFTER `note`" // phpcs:ignore
+        );
+    }
+
+    /**
+     * Updates refund database table column
+     *
+     * @since 3.3.1
+     *
+     * @return void
+     */
+    public static function update_refund_table_column() {
+        global $wpdb;
+
+        include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+        $existing_columns = $wpdb->get_results( "DESC `{$wpdb->prefix}dokan_refund`" );
+
+        foreach ( (array) $existing_columns as $existing_column ) {
+            if ( in_array( $existing_column->Field, [ 'item_totals', 'item_tax_totals' ], true ) && 'text' !== $existing_column->Type ) { // phpcs:ignore
+                $wpdb->query(
+                    "ALTER TABLE `{$wpdb->prefix}dokan_refund` MODIFY COLUMN {$existing_column->Field} text" // phpcs:ignore
+                );
+            }
+        }
+    }
+}
