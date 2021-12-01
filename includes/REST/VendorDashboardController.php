8ce8416ceba9a785729eee7fbda3dbbcd7dc97df
Shazahanul Islam Shohag <shohag121@gmail.com>
2021-12-01 17:51:50 +0600

    update: vendor dashboard api endpoint and data

--- includes/REST/VendorDashboardController.php
+++ includes/REST/VendorDashboardController.php
@@ -2,7 +2,10 @@
 
 namespace WeDevs\Dokan\REST;
 
-use WeDevs\Dokan\Vendor\Vendor;
+use WP_Error;
+use WP_HTTP_Response;
+use WP_REST_Request;
+use WP_REST_Response;
 use WP_REST_Server;
 
 class VendorDashboardController extends \WP_REST_Controller {
@@ -22,7 +25,7 @@ class VendorDashboardController extends \WP_REST_Controller {
     protected $base = 'vendor-dashboard';
 
     /**
-     * Vendor dashboard controller constructor
+     * Vendor dashboard controller constructor.
      *
      * @since 3.3.3
      *
@@ -57,11 +60,13 @@ class VendorDashboardController extends \WP_REST_Controller {
                     'args'                => [
                         'from' => [
                             'type'        => 'string',
+                            'format'      => 'date-time',
                             'description' => __( 'From Date', 'dokan-lite' ),
                             'required'    => true,
                         ],
-                        'to' => [
+                        'to'   => [
                             'type'        => 'string',
+                            'format'      => 'date-time',
                             'description' => __( 'To Date', 'dokan-lite' ),
                             'required'    => true,
                         ],
@@ -76,7 +81,7 @@ class VendorDashboardController extends \WP_REST_Controller {
                     'methods'             => WP_REST_Server::READABLE,
                     'callback'            => [ $this, 'get_products_summary' ],
                     'args'                => [],
-                    'permission_callback' => '__return_true',
+                    'permission_callback' => 'is_user_logged_in',
                 ],
             ]
         );
@@ -92,7 +97,107 @@ class VendorDashboardController extends \WP_REST_Controller {
         );
     }
 
+    /**
+     * Get dashboard statistics.
+     *
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
     public function get_dashboard_statistics() {
+        $vendor = dokan()->vendor->get( dokan_get_current_user_id() );
 
+        return rest_ensure_response(
+            [
+                'total_sales'    => $vendor->get_sales(),
+                'total_orders'   => $vendor->get_orders_count(),
+                'total_products' => $vendor->get_products_count(),
+                'total_earnings' => $vendor->get_earnings(),
+                'total_views'    => $vendor->get_pageview(),
+            ]
+        );
+    }
+
+    /**
+     * Get Vendor profile completeness.
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_profile_completeness() {
+        $vendor = dokan()->vendor->get( get_current_user_id() );
+
+        return rest_ensure_response(
+            $vendor->get_profile_completeness()
+        );
+    }
+
+    /**
+     * Get Vendor Sales Report.
+     *
+     * @since 3.3.3
+     *
+     * @param WP_REST_Request $request
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_sales_reports( $request ) {
+        return rest_ensure_response(
+            dokan_get_order_report_data(
+                array(
+                    'data' => array(
+                        '_order_total' => array(
+                            'type'     => 'meta',
+                            'function' => 'SUM',
+                            'name'     => 'total_sales',
+                        ),
+                        'ID' => array(
+                            'type'     => 'post_data',
+                            'function' => 'COUNT',
+                            'name'     => 'total_orders',
+                            'distinct' => true,
+                        ),
+                        'post_date' => array(
+                            'type'     => 'post_data',
+                            'function' => '',
+                            'name'     => 'post_date',
+                        ),
+                    ),
+                    'group_by'     => 'YEAR(post_date), MONTH(post_date), DAY(post_date)',
+                    'order_by'     => 'post_date ASC',
+                    'query_type'   => 'get_results',
+                    'filter_range' => true,
+                    'debug' => false,
+                ),
+                dokan_current_datetime()->modify( $request->get_param( 'from' ) )->format( 'Y-m-d' ),
+                dokan_current_datetime()->modify( $request->get_param( 'to' ) )->format( 'Y-m-d' )
+            )
+        );
+    }
+
+    /**
+     * Get Vendor products reports summary.
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_products_summary() {
+        return rest_ensure_response( dokan_count_posts( 'product', dokan_get_current_user_id() ) );
+    }
+
+    /**
+     * Get Vendor Order reports summary.
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_orders_summary() {
+        return rest_ensure_response(
+            dokan_count_orders( dokan_get_current_user_id() )
+        );
     }
 }
