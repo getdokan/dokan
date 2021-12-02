a778b96347b7b6879abacf529265dfe9ed40b352
Shazahanul Islam Shohag <shohag121@gmail.com>
2021-12-02 13:41:51 +0600

    enhance: Dashboard API (#1438)
    * new: vendor dashboard api endpoint
    
    * update: vendor dashboard api endpoint and data
    
    * update: profile endpoint and statistics
    
    * update: vendor dashboard stat
    
    * update: formatted balance added

---
+++ includes/REST/VendorDashboardController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace WeDevs\Dokan\REST;
+
+use WP_Error;
+use WP_HTTP_Response;
+use WP_REST_Request;
+use WP_REST_Response;
+use WP_REST_Server;
+
+class VendorDashboardController extends \WP_REST_Controller {
+
+    /**
+     * Endpoint namespace
+     *
+     * @var string
+     */
+    protected $namespace = 'dokan/v1';
+
+    /**
+     * Route name
+     *
+     * @var string
+     */
+    protected $base = 'vendor-dashboard';
+
+    /**
+     * Vendor dashboard controller constructor.
+     *
+     * @since 3.3.3
+     *
+     * @return void
+     */
+    public function register_routes() {
+        register_rest_route(
+            $this->namespace, '/' . $this->base, [
+                [
+                    'methods'             => WP_REST_Server::READABLE,
+                    'callback'            => [ $this, 'get_dashboard_statistics' ],
+                    'args'                => [],
+                    'permission_callback' => 'is_user_logged_in',
+                ],
+            ]
+        );
+        register_rest_route(
+            $this->namespace, '/' . $this->base . '/profile', [
+                [
+                    'methods'             => WP_REST_Server::READABLE,
+                    'callback'            => [ $this, 'get_profile_completeness' ],
+                    'args'                => [],
+                    'permission_callback' => 'is_user_logged_in',
+                ],
+            ]
+        );
+        register_rest_route(
+            $this->namespace, '/' . $this->base . '/sales', [
+                [
+                    'methods'             => WP_REST_Server::READABLE,
+                    'callback'            => [ $this, 'get_sales_reports' ],
+                    'args'                => [
+                        'from' => [
+                            'type'        => 'string',
+                            'format'      => 'date-time',
+                            'description' => __( 'From Date', 'dokan-lite' ),
+                            'required'    => true,
+                        ],
+                        'to'   => [
+                            'type'        => 'string',
+                            'format'      => 'date-time',
+                            'description' => __( 'To Date', 'dokan-lite' ),
+                            'required'    => true,
+                        ],
+                    ],
+                    'permission_callback' => 'is_user_logged_in',
+                ],
+            ]
+        );
+        register_rest_route(
+            $this->namespace, '/' . $this->base . '/products', [
+                [
+                    'methods'             => WP_REST_Server::READABLE,
+                    'callback'            => [ $this, 'get_products_summary' ],
+                    'args'                => [],
+                    'permission_callback' => 'is_user_logged_in',
+                ],
+            ]
+        );
+        register_rest_route(
+            $this->namespace, '/' . $this->base . '/orders', [
+                [
+                    'methods'             => WP_REST_Server::READABLE,
+                    'callback'            => [ $this, 'get_orders_summary' ],
+                    'args'                => [],
+                    'permission_callback' => 'is_user_logged_in',
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Get dashboard statistics.
+     *
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_dashboard_statistics() {
+        return rest_ensure_response(
+            [
+                'balance'  => dokan_get_seller_balance( dokan_get_current_user_id() ),
+                'orders'   => dokan_count_orders( dokan_get_current_user_id() ),
+                'products' => dokan_count_posts( 'product', dokan_get_current_user_id() ),
+                'earnings' => wc_price( dokan_author_total_sales( dokan_get_current_user_id() ) ),
+                'views'    => dokan_author_pageviews( dokan_get_current_user_id() ),
+            ]
+        );
+    }
+
+    /**
+     * Get Vendor profile Information.
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_profile_completeness() {
+        return rest_ensure_response(
+            dokan_get_store_info( dokan_get_current_user_id() )
+        );
+    }
+
+    /**
+     * Get Vendor Sales Report.
+     *
+     * @since 3.3.3
+     *
+     * @param WP_REST_Request $request
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_sales_reports( $request ) {
+        return rest_ensure_response(
+            dokan_get_order_report_data(
+                array(
+                    'data' => array(
+                        '_order_total' => array(
+                            'type'     => 'meta',
+                            'function' => 'SUM',
+                            'name'     => 'total_sales',
+                        ),
+                        'ID' => array(
+                            'type'     => 'post_data',
+                            'function' => 'COUNT',
+                            'name'     => 'total_orders',
+                            'distinct' => true,
+                        ),
+                        'post_date' => array(
+                            'type'     => 'post_data',
+                            'function' => '',
+                            'name'     => 'post_date',
+                        ),
+                    ),
+                    'group_by'     => 'YEAR(post_date), MONTH(post_date), DAY(post_date)',
+                    'order_by'     => 'post_date ASC',
+                    'query_type'   => 'get_results',
+                    'filter_range' => true,
+                    'debug' => false,
+                ),
+                dokan_current_datetime()->modify( $request->get_param( 'from' ) )->format( 'Y-m-d' ),
+                dokan_current_datetime()->modify( $request->get_param( 'to' ) )->format( 'Y-m-d' )
+            )
+        );
+    }
+
+    /**
+     * Get Vendor products reports summary.
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_products_summary() {
+        return rest_ensure_response( dokan_count_posts( 'product', dokan_get_current_user_id() ) );
+    }
+
+    /**
+     * Get Vendor Order reports summary.
+     *
+     * @since 3.3.3
+     *
+     * @return WP_Error|WP_HTTP_Response|WP_REST_Response
+     */
+    public function get_orders_summary() {
+        return rest_ensure_response(
+            dokan_count_orders( dokan_get_current_user_id() )
+        );
+    }
+}
