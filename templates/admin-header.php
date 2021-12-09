1fc42334fa0a41d33682b5f9ec75f167d28522eb
Shahadat Hossaïn <shahadat015@yahoo.com>
2021-12-09 13:30:58 +0600

    Comment out unused code.

--- templates/admin-header.php
+++ templates/admin-header.php
@@ -65,12 +65,12 @@
                 </div>
             </div>
         </div>
-<!--        <div class="menu-item">-->
-<!--            <div class="item">-->
-<!--                <div class="dokan-icon icon-notification-bell">-->
-<!--                    <span class="notification-count">4</span>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
+        <!--<div class="menu-item">
+            <div class="item">
+                <div class="dokan-icon icon-notification-bell">
+                    <span class="notification-count">4</span>
+                </div>
+            </div>
+        </div>-->
     </div>
 </div>
