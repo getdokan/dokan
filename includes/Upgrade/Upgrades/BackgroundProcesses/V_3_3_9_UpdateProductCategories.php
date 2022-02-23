41fda87b4222c242fa52cba5cb592fa0dee49a01
Aunshon <aunshonhasan@gmail.com>
2022-02-23 11:22:10 +0600

    Upgrader fix

--- includes/Upgrade/Upgrades/BackgroundProcesses/V_3_3_9_UpdateProductCategories.php
+++ includes/Upgrade/Upgrades/BackgroundProcesses/V_3_3_9_UpdateProductCategories.php
@@ -13,9 +13,6 @@ class V_3_3_9_UpdateProductCategories extends DokanBackgroundProcesses {
 
     /**
      * Save all ancestors ids based on child id.
-     * For every products we are getting it's categories then finding it's ancestors and saving it.
-     * We are also saving a chosen category, we are finding the same parents child and saving
-     * the most youngest child as chosen category.
      *
      * @param array $products
      *
@@ -36,19 +33,19 @@ class V_3_3_9_UpdateProductCategories extends DokanBackgroundProcesses {
 
             foreach ( $terms as $index => $term ) {
                 $category        = $term->term_id;
-                $all_ancestors       = get_ancestors( $category, 'product_cat' );
+                $all_ancestors   = get_ancestors( $category, 'product_cat' );
                 $most_old_parent = end( $all_ancestors );
                 $all_categories  = $category !== 0 ? $all_ancestors : [];
-                $all_categories  = wp_parse_args( $all_categories, $category );
+                $all_categories  = wp_parse_args( $all_categories, [ $category ] );
 
                 empty( $most_old_parent ) ? $most_old_parent = $category : '';
 
                 // If the child already in the array and the current child is more younger than the previous child we are
                 // updating as the chosen category.
                 if( ! array_key_exists( $most_old_parent, $all_parents ) || $all_parents[ $most_old_parent ]['size'] < sizeof( $all_ancestors ) ) {
-                    $all_parents[ $most_old_parent ]['size'] = sizeof( $all_ancestors );
+                    $all_parents[ $most_old_parent ]['size']   = sizeof( $all_ancestors );
                     $all_parents[ $most_old_parent ]['parent'] = $most_old_parent;
-                    $all_parents[ $most_old_parent ]['child'] = $category;
+                    $all_parents[ $most_old_parent ]['child']  = $category;
                 }
                 $all_cats_with_parents = array_unique( array_merge( $all_cats_with_parents, $all_categories ) );
             }
@@ -56,6 +53,7 @@ class V_3_3_9_UpdateProductCategories extends DokanBackgroundProcesses {
             $chosen_cat = array_map( function( $value ) {
                 return $value['child'];
             }, $all_parents );
+            $chosen_cat = array_values( $chosen_cat );
 
             update_post_meta( $product->ID, 'chosen_product_cat', $chosen_cat );
             wp_set_object_terms( $product->ID, $all_cats_with_parents, 'product_cat' );
