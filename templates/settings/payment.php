7d34360c7b3901e53a75d4140735193357179e54
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2022-05-27 14:40:26 +0600

    Enhance/payment page dorpdown and back button (#1608)
    * added back button
    
    * update dropdown hover area
    
    * add dropdown arrow
    
    * add hover background
    
    * change hover effect
    
    * fix reviewer comments
    
    * remove unnecessary string concatenation
    
    * fix design break on firefox
    
    * change dropdown indicator
    
    * fix ui
    
    * hide buttons in setup page
    
    * fix bank style in vendor setup page
    
    * fix bank style in vendor setup page
    
    * fix indentation
    
    * fix indentation
    
    * fix ui
    
    * change bank account number place holder
    
    * set wodth of dorpdown no content message
    
    * center text
    
    * fix height of wrapper unordered list
    
    * fix height of wrapper unordered list
    
    * fix hover not cover entire rectangle
    
    * highlight list item on hover

--- templates/settings/payment.php
+++ templates/settings/payment.php
@@ -14,33 +14,35 @@ do_action( 'dokan_payment_settings_before_form', $current_user, $profile_info );
     <h2 id="vendor-dashboard-payment-settings-error"></h2>
     <div class="payment-methods-listing-header">
         <h2> <?php esc_html_e( 'Payment Methods', 'dokan-lite' ); ?></h2>
-        <div id="vendor-dashboard-payment-settings-toggle-dropdown">
-            <button id="toggle-vendor-payment-method-drop-down" class="dokan-btn dokan-btn-success"> <?php esc_html_e( 'Add Payment Method', 'dokan-lite' ); ?></button>
-            <div id="vendor-payment-method-drop-down-wrapper">
-                <div id="vendor-payment-method-drop-down">
-                    <?php if ( is_array( $unused_methods ) && ! empty( $unused_methods ) ) : ?>
-                        <ul>
-                        <?php foreach ( $unused_methods as $method_key => $method ) :?>
-                            <li>
-                                <a href="<?php echo esc_url( dokan_get_page_url( 'dashboard', 'dokan', 'settings/payment/manage-' . $method_key ) ); ?>">
-                                    <div>
-                                        <img src="<?php echo esc_url( dokan_withdraw_get_method_icon(  $method_key ) ); ?>" alt="<?php echo esc_attr( $method_key ); ?>" />
-                                        <span>
-                                            <?php
-                                            //translators: %s: payment method title
-                                            printf( esc_html__( 'Direct to %s', 'dokan-lite' ), apply_filters( 'dokan_payment_method_title', $method['title'], $method ) );
-                                            ?>
-                                        </span>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
-                        </ul>
-                    <?php else : ?>
-                        <div class="no-content">
-                            <?php esc_html_e( 'There is no payment method to add.', 'dokan-lite' ); ?>
-                        </div>
-                    <?php endif; ?>
+        <div>
+            <div id="vendor-dashboard-payment-settings-toggle-dropdown">
+                <a id="toggle-vendor-payment-method-drop-down"> <?php esc_html_e( 'Add Payment Method', 'dokan-lite' ); ?></a>
+                <div id="vendor-payment-method-drop-down-wrapper">
+                    <div id="vendor-payment-method-drop-down">
+                        <?php if ( is_array( $unused_methods ) && ! empty( $unused_methods ) ) : ?>
+                            <ul>
+                            <?php foreach ( $unused_methods as $method_key => $method ) :?>
+                                <li>
+                                    <a href="<?php echo esc_url( dokan_get_page_url( 'dashboard', 'dokan', 'settings/payment/manage-' . $method_key ) ); ?>">
+                                        <div>
+                                            <img src="<?php echo esc_url( dokan_withdraw_get_method_icon(  $method_key ) ); ?>" alt="<?php echo esc_attr( $method_key ); ?>" />
+                                            <span>
+                                                <?php
+                                                //translators: %s: payment method title
+                                                printf( esc_html__( 'Direct to %s', 'dokan-lite' ), apply_filters( 'dokan_payment_method_title', $method['title'], $method ) );
+                                                ?>
+                                            </span>
+                                        </div>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                            </ul>
+                        <?php else : ?>
+                            <div class="no-content">
+                                <?php esc_html_e( 'There is no payment method to add.', 'dokan-lite' ); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
