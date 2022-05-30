2f0dcad9c62d5cae3816dfc8d07327b9aa42a1c5
Nurul Umbhiya <zikubd@gmail.com>
2022-05-30 14:42:18 +0600

    fix: fixed some phpcs issue

--- templates/emails/plain/reverse-withdrawal-invoice.php
+++ templates/emails/plain/reverse-withdrawal-invoice.php
@@ -24,19 +24,19 @@ echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
 echo esc_html( wp_strip_all_tags( $email_heading ) );
 echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
-printf( esc_attr__( 'Hi %s,\n\n', 'dokan-lite' ), $seller_info->get_shop_name() );
+printf( esc_html__( 'Hi %s,\n\n', 'dokan-lite' ), $seller_info->get_shop_name() );
 
-printf( esc_attr__( 'Your %1$s %2$s invoice is now available for store: %3$s.\n\n', 'dokan-lite' ), $data['{month}'], $data['{year}'], $seller_info->get_shop_name() );
+printf( esc_html__( 'Your %1$s %2$s invoice is now available for store: %3$s.\n\n', 'dokan-lite' ), $data['{month}'], $data['{year}'], $seller_info->get_shop_name() );
 
-printf(esc_attr__('Summary for %1$s : \n\n', 'dokan-lite'), $seller_info->get_shop_name());
+printf(esc_html__('Summary for %1$s : \n\n', 'dokan-lite'), $seller_info->get_shop_name());
 
-printf( esc_attr__( 'Reverse withdrawal charges for %1$s %2$s: %3$s \n\n', 'dokan-lite' ), $data['{month}'], $data['{year}'], wc_price( $due_status['balance']['payable_amount'] ) );
+printf( esc_html__( 'Reverse withdrawal charges for %1$s %2$s: %3$s \n\n', 'dokan-lite' ), $data['{month}'], $data['{year}'], wc_price( $due_status['balance']['payable_amount'] ) );
 
-printf( esc_attr__( 'Due Date %1$s : \n\n', 'dokan-lite' ), 'immediate' === $due_status['due_date'] ? ucfirst( $due_status['due_date'] ) : dokan_format_date( $due_status['due_date'] ) );
+printf( esc_html__( 'Due Date %1$s : \n\n', 'dokan-lite' ), 'immediate' === $due_status['due_date'] ? ucfirst( $due_status['due_date'] ) : dokan_format_date( $due_status['due_date'] ) );
 
 printf(
     wp_kses(
-        '<a href="' . $data['{reverse_withdrawal_url}'] . '">' . esc_attr__( 'Pay Now', 'dokan-lite' ) . '</a>',
+        '<a href="' . $data['{reverse_withdrawal_url}'] . '">' . esc_html__( 'Pay Now', 'dokan-lite' ) . '</a>',
         array(
             'a' => array(
                 'href' => array(),
