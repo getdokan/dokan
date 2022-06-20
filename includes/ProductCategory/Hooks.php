724627422829c1187c3db019b0ce19b87dcc21f6
Aunshon <aunshonhasan@gmail.com>
2022-06-20 15:59:31 +0600

    delete refrence

--- includes/ProductCategory/Hooks.php
+++ includes/ProductCategory/Hooks.php
@@ -19,6 +19,7 @@ class Hooks {
     public function __construct() {
         add_action( 'edit_product_cat', [ $this, 'clear_multistep_category_cache' ], 10, 1 );
         add_action( 'pre_delete_term', [ $this, 'clear_multistep_category_cache' ], 10, 1 );
+        add_action( 'create_product_cat', [ $this, 'clear_multistep_category_cache' ], 10);
 
         add_action( 'pre_delete_term', [ $this, 'delete_references_for_chosen_products_cats' ], 10, 1 );
     }
@@ -45,6 +46,28 @@ class Hooks {
      * @return void
      */
     public function delete_references_for_chosen_products_cats( $category_id ) {
+        global $wpdb;
+        $deletable_term = get_term( $category_id );
+        $results = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}postmeta where meta_key='chosen_product_cat' AND meta_value LIKE '%%%d%'", $category_id ), ARRAY_A );
 
+        foreach ( $results as $result ) {
+            $datas = maybe_unserialize( $result['meta_value'] );
+            $searched = array_search( $category_id, $datas );
+
+            if ( false !== $searched ) {
+                $datas[ $searched ] = $deletable_term->parent;
+                $ids = maybe_serialize( $datas );
+
+                $wpdb->update(
+                    $wpdb->prefix . 'postmeta',
+                    [
+                        'meta_value' => $ids,
+                    ],
+                    [
+                        'meta_id' => $result['meta_id'],
+                    ]
+                );
+            }
+        }
     }
 }
