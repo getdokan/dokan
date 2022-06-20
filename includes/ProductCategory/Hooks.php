f7adaa7b5b13df9c07417ffcb24ebff228981adf
Aunshon <aunshonhasan@gmail.com>
2022-06-20 16:01:28 +0600

    rename funiction

--- includes/ProductCategory/Hooks.php
+++ includes/ProductCategory/Hooks.php
@@ -21,7 +21,7 @@ class Hooks {
         add_action( 'pre_delete_term', [ $this, 'clear_multistep_category_cache' ], 10, 1 );
         add_action( 'create_product_cat', [ $this, 'clear_multistep_category_cache' ], 10);
 
-        add_action( 'pre_delete_term', [ $this, 'delete_references_for_chosen_products_cats' ], 10, 1 );
+        add_action( 'pre_delete_term', [ $this, '$datas[$searched]' ], 10, 1 );
     }
 
     /**
@@ -45,7 +45,7 @@ class Hooks {
      *
      * @return void
      */
-    public function delete_references_for_chosen_products_cats( $category_id ) {
+    public function delete_reference_category_from_chosen_cat( $category_id ) {
         global $wpdb;
         $deletable_term = get_term( $category_id );
         $results = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}postmeta where meta_key='chosen_product_cat' AND meta_value LIKE '%%%d%'", $category_id ), ARRAY_A );
