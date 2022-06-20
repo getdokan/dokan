9014b73201c3896bf28503d8738a498ce66c1db3
Aunshon <aunshonhasan@gmail.com>
2022-06-20 13:07:03 +0600

    version conflict fix

--- includes/Upgrade/Upgrades.php
+++ includes/Upgrade/Upgrades.php
@@ -38,7 +38,7 @@ class Upgrades {
         '3.3.7'  => Upgrades\V_3_3_7::class,
         '3.3.8'  => Upgrades\V_3_3_8::class,
         '3.5.1'  => Upgrades\V_3_5_1::class,
-        '3.6.2'  => Upgrades\V_3_6_1::class,
+        '3.6.2'  => Upgrades\V_3_6_2::class,
     ];
 
     /**
