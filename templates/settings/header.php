ad008f9465b279b5877c463649292937474d436c
Nurul Umbhiya <zikubd@gmail.com>
2022-09-15 21:33:32 +0600

    fix: phpcs fix

--- templates/settings/header.php
+++ templates/settings/header.php
@@ -2,22 +2,22 @@
 /**
  * Dokan Settings Header Template
  *
- * @since 2.4
+ * @since   2.4
  *
  * @package dokan
  */
 ?>
 <header class="dokan-dashboard-header">
-	<div class="dokan-store-settign-header-wrap">
-	    <h1 class="entry-title">
-	        <?php echo wp_kses_post( $heading ); ?>
-	        <small>&rarr; <a href="<?php echo esc_url( dokan_get_store_url( dokan_get_current_user_id() ) ); ?>"><?php esc_html_e( 'Visit Store', 'dokan-lite' ); ?></a></small>
-	    </h1>
-	</div>
+    <div class="dokan-store-settign-header-wrap">
+        <h1 class="entry-title">
+            <?php echo wp_kses_post( $heading ); ?>
+            <small>&rarr; <a href="<?php echo esc_url( dokan_get_store_url( dokan_get_current_user_id() ) ); ?>"><?php esc_html_e( 'Visit Store', 'dokan-lite' ); ?></a></small>
+        </h1>
+    </div>
 
-	<?php if ( isset( $is_store_setting ) && $is_store_setting ) : ?>
-	    <span class="dokan-update-setting-top">
+    <?php if ( isset( $is_store_setting ) && $is_store_setting ) : ?>
+        <span class="dokan-update-setting-top">
 	        <button class="dokan-update-setting-top-button dokan-btn dokan-btn-theme dokan-right"><?php esc_html_e( 'Update Settings', 'dokan-lite' ); ?></button>
 	    </span>
-	<?php endif ?>
+    <?php endif ?>
 </header><!-- .dokan-dashboard-header -->
