d6972e2d434fcb520ea8fdc8c894b4e0123667f2
Nurul Umbhiya <zikubd@gmail.com>
2022-11-01 09:21:59 +0600

    update: Sort product categories alphabetically, closes #1676

--- assets/src/js/product-category-ui.js
+++ assets/src/js/product-category-ui.js
@@ -192,6 +192,9 @@
                 }
             }
 
+            // sort categories by name
+            returnableCategories.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+
             return {
                 categories: returnableCategories,
                 level: level,
