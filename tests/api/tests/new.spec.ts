6cb0057142ba7074ffef1ae157cdfac7946449fe
shashwata <shashwatahalder01@gmail.com>
2022-12-23 16:51:09 +0600

    updated suite

--- tests/api/tests/new.spec.ts
+++ tests/api/tests/new.spec.ts
@@ -27,7 +27,7 @@ test.beforeAll(async ({ request }) => {
 // test.afterEach(async ({ request }) => { });
 
 
-test.describe('new v1 api test', () => {
+test.describe.skip('new v1 api test', () => {
 
     test('get product block details', async ({ request }) => {
         let response = await request.get(endPoints.getProductBlockDetails(productId))
