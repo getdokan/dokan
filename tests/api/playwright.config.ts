b5fed62e2cc00e05109b7142ffe4ffca0cec58d4
shashwata <shashwatahalder01@gmail.com>
2023-01-19 09:33:45 +0600

    updated yml file

--- tests/api/playwright.config.ts
+++ tests/api/playwright.config.ts
@@ -1,7 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 // import { devices } from '@playwright/test';
 require('dotenv').config();
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
