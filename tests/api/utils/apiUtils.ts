2b7dbcee77f450609ded0352322c97739d725e0c
shashwata <shashwatahalder01@gmail.com>
2023-01-19 11:31:34 +0600

    updated yml file

--- tests/api/utils/apiUtils.ts
+++ tests/api/utils/apiUtils.ts
@@ -28,11 +28,12 @@ export class ApiUtils {
         let responseBody: any
         try {
             responseBody = await response.json()
-            console.log('Status Code: ', response.status())
-            console.log('ResponseBody: ', responseBody)
+            // console.log('Status Code: ', response.status())
+            // console.log('ResponseBody: ', responseBody)
         } catch (err) {
+            console.log('Status Code: ', response.status())
             console.log('Error: ', err.message)
-
+            // console.log('Response text: ', await response.text())
         }
         return responseBody
     }
