ac2be5de3a04fd5c26387cee13c50a1c380fb146
shashwata <shashwatahalder01@gmail.com>
2023-02-20 10:18:53 +0600

    fix:rank-math-api

--- tests/api/global-setup.ts
+++ tests/api/global-setup.ts
@@ -1,20 +1,22 @@
-require( 'dotenv' ).config();
+require('dotenv').config();
 import { chromium, FullConfig, request } from '@playwright/test';
 
-async function globalSetup( config: FullConfig ) {
+async function globalSetup(config: FullConfig) {
 	console.log('Global Setup running....');
 
 	// get site url structure
-	let serverUrl = process.env.BASE_URL ? process.env.BASE_URL + '/wp-json' : 'http://localhost:8888' + '/wp-json';
+	let serverUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:8888';
 	let query = '?';
-	const context = await request.newContext( { ignoreHTTPSErrors: true } );
-	const head = await context.head( serverUrl );
+	const context = await request.newContext({ ignoreHTTPSErrors: true });
+	const head = await context.head(serverUrl);
 	const headers = head.headers();
 	const link = headers.link;
-	if ( link.includes( 'rest_route' ) ) {
+	if (link.includes('rest_route')) {
 		serverUrl = serverUrl + '?rest_route=';
 		query = '&';
-	} 
+	} else {
+		serverUrl = serverUrl + '/wp-json';
+	}
 	process.env.SERVER_URL = serverUrl;
 	process.env.QUERY = query;
 	console.log('Global Setup Finished!');
