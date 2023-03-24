bb6fc5513db168da0bba9af4888cde546223f236
Shazahanul Islam Shohag <shohag121@gmail.com>
2023-03-24 10:53:58 +0600

    update: added shipping tax fee recipient admin settings `shipping_tax_fee_recipient`

--- includes/Admin/Settings.php
+++ includes/Admin/Settings.php
@@ -490,7 +490,7 @@ class Settings {
                 'shipping_fee_recipient' => [
                     'name'    => 'shipping_fee_recipient',
                     'label'   => __( 'Shipping Fee Recipient', 'dokan-lite' ),
-                    'desc'    => __( 'Who will be receiving the shipping fees', 'dokan-lite' ),
+                    'desc'    => __( 'Who will be receiving the shipping fees (without tax fees)', 'dokan-lite' ),
                     'type'    => 'radio',
                     'options' => [
                         'seller' => __( 'Vendor', 'dokan-lite' ),
@@ -500,8 +500,20 @@ class Settings {
                 ],
                 'tax_fee_recipient'      => [
                     'name'    => 'tax_fee_recipient',
-                    'label'   => __( 'Tax Fee Recipient', 'dokan-lite' ),
-                    'desc'    => __( 'Who will be receiving the tax fees', 'dokan-lite' ),
+                    'label'   => __( 'Product Tax Fee Recipient', 'dokan-lite' ),
+                    'desc'    => __( 'Who will be receiving the tax fees for products (without shipping tax fees)', 'dokan-lite' ),
+                    'type'    => 'radio',
+                    'options' => [
+                        'seller' => __( 'Vendor', 'dokan-lite' ),
+                        'admin'  => __( 'Admin', 'dokan-lite' ),
+                    ],
+                    'default' => 'seller',
+                ],
+
+                'shipping_tax_fee_recipient'      => [
+                    'name'    => 'shipping_tax_fee_recipient',
+                    'label'   => __( 'Shipping Tax Fee Recipient', 'dokan-lite' ),
+                    'desc'    => __( 'Who will be receiving the tax fees for shipping', 'dokan-lite' ),
                     'type'    => 'radio',
                     'options' => [
                         'seller' => __( 'Vendor', 'dokan-lite' ),
