0333255657316e5830f7b4e9b00d16c0f5eb1cdf
Nurul Umbhiya <zikubd@gmail.com>
2023-03-28 11:41:26 +0600

    refactor: moved some methods to a separate file

--- includes/Order/Admin/Hooks.php
+++ includes/Order/Admin/Hooks.php
@@ -47,11 +47,6 @@ class Hooks {
         add_action( 'delete_post', [ $this, 'admin_on_delete_order' ] );
         add_filter( 'woocommerce_reports_get_order_report_query', [ $this, 'admin_order_reports_remove_parents' ] );
         add_filter( 'post_class', [ $this, 'admin_shop_order_row_classes' ], 10, 3 );
-
-        add_filter( 'posts_clauses', [ $this, 'filter_orders_for_current_vendor' ], 12, 2 );
-        add_action( 'load-post.php', [ $this, 'revoke_change_order_status' ] );
-        add_filter( 'manage_edit-shop_order_columns', [ $this, 'remove_action_column' ], 15 );
-        add_filter( 'woocommerce_admin_order_preview_actions', [ $this, 'remove_action_button' ], 15 );
     }
 
     /**
@@ -440,117 +435,5 @@ class Hooks {
         if ( $typenow === 'shop_order' ) {
             echo '<button class="toggle-sub-orders button show">' . esc_html__( 'Toggle Sub-orders', 'dokan-lite' ) . '</button>';
         }
-    }
-
-    /**
-     * Filter orders of current user
-     *
-     * @since 2.9.4
-     * @since DOKAN_SINCE Moved this method from includes/functions.php
-     * @since DOKAN_SINCE Added HPOS Support
-     *
-     * @param array  $args
-     * @param object $query
-     *
-     * @return array
-     */
-    public function filter_orders_for_current_vendor( $args, $query ) {
-        global $wpdb;
-
-        if ( ! is_admin() || ! $query->is_main_query() ) {
-            return $args;
-        }
-
-        if ( ! isset( $query->query_vars['post_type'] ) ) {
-            return $args;
-        }
-
-        if ( ! in_array( $query->query_vars['post_type'], [ 'shop_order', 'wc_booking' ], true ) ) {
-            return $args;
-        }
-
-        $vendor_id = isset( $_GET['vendor_id'] ) ? absint( wp_unslash( $_GET['vendor_id'] ) ) : 0; // phpcs:ignore;
-        if ( ! current_user_can( 'manage_woocommerce' ) ) {
-            $vendor_id = dokan_get_current_user_id();
-        }
-
-        if ( ! $vendor_id ) {
-            return $args;
-        }
-
-        $args['join']  .= " LEFT JOIN {$wpdb->prefix}dokan_orders as do ON $wpdb->posts.ID=do.order_id";
-        $args['where'] .= " AND do.seller_id=$vendor_id";
-
-        return $args;
-    }
-
-    /**
-     * Revoke vendor access of changing order status in the backend if permission is not given
-     *
-     * @since 2.8.0
-     * @since DOKAN_SINCE Moved this method from includes/functions.php file
-     *
-     * @return void
-     */
-    public function revoke_change_order_status() {
-        if ( current_user_can( 'manage_woocommerce' ) ) {
-            return;
-        }
-
-        if ( is_admin() && get_current_screen()->id === 'shop_order' ) {
-            if ( dokan_get_option( 'order_status_change', 'dokan_selling', 'on' ) !== 'on' ) {
-                ?>
-                <style media="screen">
-                    .order_data_column .wc-order-status {
-                        display: none !important;
-                    }
-                </style>
-                <?php
-            }
-        }
-    }
-
-    /**
-     * Revoke vendor access of changing order status in the backend if permission is not given
-     *
-     * @since 2.8.0
-     * @since DOKAN_SINCE Moved this method from includes/functions.php
-     *
-     * @param array $columns
-     *
-     * @return array
-     */
-    public function remove_action_column( $columns ) {
-        if ( current_user_can( 'manage_woocommerce' ) ) {
-            return $columns;
-        }
-
-        if ( dokan_get_option( 'order_status_change', 'dokan_selling', 'on' ) !== 'on' ) {
-            unset( $columns['wc_actions'] );
-        }
-
-        return $columns;
-    }
-
-    /**
-     * Revoke vendor access of changing order status in the backend if permission is not given
-     *
-     * @since 2.8.0
-     * @since DOKAN_SINCE Moved this method form includes/functions.php file
-     *
-     * @param array $actions
-     *
-     * @return array;
-     */
-    public function remove_action_button( $actions ) {
-        if ( current_user_can( 'manage_woocommerce' ) ) {
-            return $actions;
-        }
-
-        if ( dokan_get_option( 'order_status_change', 'dokan_selling', 'on' ) !== 'on' ) {
-            unset( $actions['status'] );
-        }
-
-        return $actions;
     }
 }
