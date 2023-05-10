91523293fe61b11f7ffa4bf32c91054e3743d2fd
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-05-10 21:27:10 +0600

    update:pompay

---
+++ tests/e2e/a.js
@@ -0,0 +1,5 @@
+console.log(
+	'WordPress development site started at \n\
+WordPress test site started at \n\
+MySQL is listening on port 32770\n\
+MySQL for automated testing is listening on port 32769');
