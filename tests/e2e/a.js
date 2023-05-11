f66af8ef4b8c5c82ed3fd7cfae7bcda16964a263
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-05-11 16:43:50 +0600

    update:e2e

--- tests/e2e/a.js
+++
@@ -1,9 +0,0 @@
-console.log(
-	'WordPress development site started at WordPress test site started at MySQL is listening on port 32770 MySQL for automated testing is listening on port 32769');
-
-
-// console.log(
-// 	'WordPress development site started at \n\
-// 	WordPress test site started at \n\
-// 	MySQL is listening on port 32770\n\
-// 	MySQL for automated testing is listening on port 32769');
