93bdd17591841fe0bad23514e15b9a60c355060d
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-05-11 13:01:54 +0600

    update:e2e

--- tests/e2e/global-setup.ts
+++ tests/e2e/global-setup.ts
@@ -2,21 +2,23 @@ import { FullConfig, request } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
 	console.log('Global Setup running....');
-	// get site url structure
+	// // get site url structure
 	let serverUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:9999';
-	let query = '?';
-	const apiContext = await request.newContext({ ignoreHTTPSErrors: true });
-	const head = await apiContext.head(serverUrl);
-	const headers = head.headers();
-	const link = headers.link;
-	if (link.includes('rest_route')) {
-		serverUrl = serverUrl + '?rest_route=';
-		query = '&';
-	} else {
-		serverUrl = serverUrl + '/wp-json';
-	}
-	process.env.SERVER_URL = serverUrl;
-	process.env.QUERY = query;
+	// let query = '?';
+	// const apiContext = await request.newContext({ ignoreHTTPSErrors: true });
+	// const head = await apiContext.head(serverUrl);
+	// const headers = head.headers();
+	// const link = headers.link;
+	// if (link.includes('rest_route')) {
+	// 	serverUrl = serverUrl + '?rest_route=';
+	// 	query = '&';
+	// } else {
+	// 	serverUrl = serverUrl + '/wp-json';
+	// }
+	// process.env.SERVER_URL = serverUrl;
+	process.env.SERVER_URL = serverUrl + '/wp-json';
+	// process.env.QUERY = query;
+	process.env.QUERY = '&';
 
 	console.log('Global Setup Finished!');
 }
