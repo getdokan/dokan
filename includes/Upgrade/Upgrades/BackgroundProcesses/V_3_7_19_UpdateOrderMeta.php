27c5a7b27175191643602689a342c11c55748525
Shazahanul Islam Shohag <shohag121@gmail.com>
2023-05-22 09:16:22 +0600

    update: separate shipping tax from total tax (#1848)
    * update: added shipping tax fee recipient admin settings `shipping_tax_fee_recipient`
    
    * phpunit: fix the phpunit setup
    
    * phpunit: fix directory selection and exclude
    
    * update: shipping tax fee recipient admin settings support added
    
    * refactor: phpcs fix
    
    * fix: settings group
    
    * update: upgrader added to Update existing order meta
    
    * 📝 docs(Settings.php): update descriptions for shipping_fee_recipient, tax_fee_recipient, and shipping_tax_fee_recipient options in Settings class
    
    * 📝 docs(Settings.php): update description of shipping_fee_recipient field
    🐛 fix(Commission.php): fix variable name and add backward compatibility for shipping_tax_fee_recipient meta data
    
    * refactor: changed upgrader versions to 3.7.19
    
    ---------
    
    Co-authored-by: Nurul Umbhiya <zikubd@gmail.com>

---
+++ includes/Upgrade/Upgrades/BackgroundProcesses/V_3_7_19_UpdateOrderMeta.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WeDevs\Dokan\Upgrade\Upgrades\BackgroundProcesses;
+
+use WeDevs\Dokan\Abstracts\DokanBackgroundProcesses;
+
+/**
+ * Set additional order meta `shipping_tax_fee_recipient`.
+ *
+ * @since DOKAN_SINCE
+ */
+class V_3_7_19_UpdateOrderMeta extends DokanBackgroundProcesses {
+
+    /**
+     * Save order meta `shipping_tax_fee_recipient`.
+     *
+     * @param array $orders
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return bool
+     */
+    public function task( $orders ) {
+        if ( empty( $orders || ! is_array( $orders ) ) ) {
+            return false;
+        }
+
+        foreach ( $orders as $order ) {
+            /**
+             * @var \WC_Order $order Order.
+             */
+            if ( ! empty( $order->get_meta( 'has_sub_order' ) ) ) {
+				continue;
+            }
+
+            $saved_shipping_tax_recipient = $order->get_meta( 'shipping_tax_fee_recipient' );
+            $saved_tax_recipient = $order->get_meta( 'tax_fee_recipient' );
+
+            if ( empty( $saved_shipping_tax_recipient ) ) {
+                $order->add_meta_data( 'shipping_tax_fee_recipient', $saved_tax_recipient, true );
+            }
+        }
+
+        return false;
+    }
+}
