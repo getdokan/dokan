1b274d0fc6d257460abeae2dcdfaf73e18aa81f4
Aunshon <32583103+Aunshon@users.noreply.github.com>
2023-05-31 12:22:52 +0600

    🔥Beautify the command output messages.

--- bin/clean-build-dir.js
+++ bin/clean-build-dir.js
@@ -10,6 +10,6 @@ fs.remove( dir, ( error ) => {
   if ( error ) {
     console.log( error );
   } else {
-    console.log( chalk.greenBright(`🗑️Deleted the production directory - ${dir}.`) );
+    console.log( chalk.greenBright(`✅ Cleaned dokan-lite production directory - ${dir}.`) );
   }
 } );
