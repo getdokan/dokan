f5c79973a797739ca9ed8c5f8f0404bba8d313e9
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-06-04 04:14:28 +0600

    removed query from global setup

--- tests/pw/global-setup.ts
+++ tests/pw/global-setup.ts
@@ -1,23 +1,14 @@
-import { chromium, FullConfig, request } from '@playwright/test';
+import { FullConfig, request } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
 	console.log('Global Setup running....');
-
 	// get site url structure
 	let serverUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:9999';
-	let query = '?';
 	const apiContext = await request.newContext({ ignoreHTTPSErrors: true });
 	const head = await apiContext.head(serverUrl);  //TODO: add retry fro these step
 	const headers = head.headers();
 	const link = headers.link;
-	if (link.includes('rest_route')) {
-		serverUrl = serverUrl + '?rest_route=';
-		query = '&';
-	} else {
-		serverUrl = serverUrl + '/wp-json';
-	}
-	process.env.SERVER_URL = serverUrl;
-	process.env.QUERY = query;
+	process.env.SERVER_URL =  link.includes('rest_route') ? serverUrl + '?rest_route=': serverUrl = serverUrl + '/wp-json';
 	console.log('ServerUrl:', process.env.SERVER_URL);
 	console.log('Global Setup Finished!');
 }
