76e60f31737699bbc33d16d679c22b71cec710cd
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-06-10 23:40:29 +0600

    fixed refund tests & update apiutils

--- tests/pw/tests/api/refunds.spec.ts
+++ tests/pw/tests/api/refunds.spec.ts
@@ -5,20 +5,20 @@ import { endPoints } from '../../utils/apiEndPoints';
 import { payloads } from '../../utils/payloads';
 
 let apiUtils: ApiUtils;
-let orderId: string;
+// let orderId: string;
 let refundId: string;
 let orderResponseBody: any;
 
 test.beforeAll(async ({ request }) => {
 	apiUtils = new ApiUtils(request);
-	[, orderResponseBody, orderId,] = await apiUtils.createOrderWithStatus(payloads.createProduct(), payloads.createOrder, 'wc-processing', payloads.vendorAuth);
+	[, orderResponseBody,] = await apiUtils.createOrderWithStatus(payloads.createProduct(), payloads.createOrder, 'wc-processing', payloads.vendorAuth);
 	[, refundId] = await dbUtils.createRefund(orderResponseBody);
+	//TODO: dokan get all refunds dont recognize refunded by woocommerce, find out why & try to use refund by api instead of db
 	// [, refundId] = await apiUtils.createRefund(orderId, payloads.createRefund); // via woocommerce
 });
 
 
 test.describe('refunds api test', () => {
-	//TODO: need to refund from dokan not via wc
 	test('get all refunds @pro', async () => {
 		const [response, responseBody] = await apiUtils.get(endPoints.getAllRefunds, { headers: payloads.vendorAuth });
 		expect(response.ok()).toBeTruthy();
@@ -35,7 +35,6 @@ test.describe('refunds api test', () => {
 		const [response, responseBody] = await apiUtils.put(endPoints.cancelRefund(refundId));
 		expect(response.ok()).toBeTruthy();
 		expect(responseBody).toBeTruthy();
-
 	});
 
 	test('delete a refund @pro', async () => {
@@ -54,10 +53,8 @@ test.describe('refunds api test', () => {
 	test('update batch refunds @pro', async () => {
 		await dbUtils.createRefund(orderResponseBody);
 		const allPendingRefundsIds = (await apiUtils.getAllRefunds('pending', payloads.vendorAuth)).map((a: { id: unknown }) => a.id);
-		console.log(allPendingRefundsIds);
 		const [response, responseBody] = await apiUtils.put(endPoints.updateBatchRefunds, { data: { cancelled: allPendingRefundsIds } });
 		expect(response.ok()).toBeTruthy();
 		expect(responseBody).toBeTruthy();
-		console.log(responseBody);
 	});
 });
