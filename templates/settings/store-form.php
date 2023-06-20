3ad62126960072ca1c7c915f7bfb8b8d67876288
Tanjir Al Mamun <34626059+tanjiralmamun@users.noreply.github.com>
2023-06-20 17:01:21 +0600

    made changes as suggested and adjust responsiveness

--- templates/settings/store-form.php
+++ templates/settings/store-form.php
@@ -298,7 +298,8 @@ $args     = apply_filters( 'dokan_store_time_arguments', $args, $all_times );
 
 <style>
     .dokan-settings-content .dokan-settings-area .dokan-banner {
-        width: <?php echo esc_attr( $banner_width ) . 'px'; ?>;
+        max-width: <?php echo esc_attr( $banner_width ) . 'px'; ?>;
+        max-height: <?php echo esc_attr( $banner_height ) . 'px'; ?>;
     }
 
     .dokan-settings-content .dokan-settings-area .dokan-banner .dokan-remove-banner-image {
