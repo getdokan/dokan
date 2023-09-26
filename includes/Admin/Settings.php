630aef8f3d883eedb56815628bb804373f87884e
Asad Nur <asaduzzamannur.dev@gmail.com>
2023-09-26 11:52:31 +0600

    update: Description for the admin settings store per page field

--- includes/Admin/Settings.php
+++ includes/Admin/Settings.php
@@ -446,10 +446,9 @@ class Settings {
                 'store_products_per_page'            => [
                     'name'    => 'store_products_per_page',
                     'label'   => __( 'Store Products Per Page', 'dokan-lite' ),
-                    'desc'    => __( 'Set how many products to display per page on the vendor store page. It will affect only if the vendor isn\'t set this value on their vendor setting page.', 'dokan-lite' ),
+                    'desc'    => __( 'Set how many products to display per page on the vendor store page.', 'dokan-lite' ),
                     'type'    => 'number',
                     'default' => '12',
-                    'tooltip' => __( 'It will affect the vendor only if they havent set a value on their settings page.', 'dokan-lite' ),
                 ],
                 'enabled_address_on_reg'             => [
                     'name'    => 'enabled_address_on_reg',
