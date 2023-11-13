0744ea660694f0a8a283d2b71e14cd2134e92dda
shashwata <shashwatahalder01@gmail.com>
2023-11-14 01:02:05 +0600

    test

--- tests/pw/tests/e2e/_environment.setup.spec.ts
+++ tests/pw/tests/e2e/_environment.setup.spec.ts
@@ -10,6 +10,9 @@ import { data } from '@utils/testData';
 import { helpers } from '@utils/helpers';
 
 const { CUSTOMER_ID, DOKAN_PRO, HPOS } = data.env;
+console.log(DOKAN_PRO);
+console.log(typeof DOKAN_PRO);
+
 
 setup.describe('setup site & woocommerce & user settings', () => {
     setup.use({ extraHTTPHeaders: { Authorization: payloads.adminAuth.Authorization } });
