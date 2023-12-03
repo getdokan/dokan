201d31514fdef87544e8855d9aad4fa48830c51d
shashwata <shashwatahalder01@gmail.com>
2023-12-04 01:25:46 +0600

    update versions

--- tests/pw/pages/selectors.ts
+++ tests/pw/pages/selectors.ts
@@ -6945,7 +6945,8 @@ export const selector = {
 
             // Proceed to Checkout
             // proceedToCheckout: '.checkout-button.button.wc-forward, .wp-block-woocommerce-proceed-to-checkout-block a', //todo: remove in future
-            proceedToCheckout: 'div.wc-block-cart__submit-container a.wc-block-cart__submit-button',
+            // proceedToCheckout: 'div.wc-block-cart__submit-container a.wc-block-cart__submit-button',
+            proceedToCheckout: '//span[contains (text(), "Proceed to Checkout")]/..',
 
             // Remove All Item
             productCrossIcon: '.product-remove a',
