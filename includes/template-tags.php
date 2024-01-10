cda0655f48ab70b920acf65d3199e89333a055dd
Shazahanul Islam Shohag <shohag121@gmail.com>
2024-01-10 12:45:08 +0600

    fix: Translation-Related Issues with Specific Menu Items in Vendor Dashboard

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -580,7 +580,7 @@ function dokan_dashboard_nav( $active_menu = '' ) {
          *
          * @param string $menu_key
          */
-        $filtered_key = apply_filters( 'dokan_dashboard_nav_menu_key', $key );
+        $filtered_key = rawurlencode_deep( apply_filters( 'dokan_dashboard_nav_menu_key', $key ) );
 
         $class     = $active_menu === $filtered_key || 0 === stripos( $active_menu, $filtered_key ) ? 'active ' . $key : $key;  // checking starts with the key
         $title     = isset( $item['title'] ) ? $item['title'] : __( 'No title', 'dokan-lite' );
@@ -602,7 +602,7 @@ function dokan_dashboard_nav( $active_menu = '' ) {
                  * @param string $submenu_key
                  * @param string $menu_key
                  */
-                $filtered_subkey = apply_filters( 'dokan_dashboard_nav_submenu_key', $sub_key, $key );
+                $filtered_subkey = rawurlencode_deep( apply_filters( 'dokan_dashboard_nav_submenu_key', $sub_key, $key ) );
 
                 $submenu_class = $active_submenu === $filtered_subkey || 0 === stripos( $active_submenu, $filtered_subkey ) ? "current $sub_key" : $sub_key;
 
