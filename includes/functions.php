d58cae356e7af15971c1a93ee97e7e635b848b03
Edward Fisher JR <42970431+fisher2470@users.noreply.github.com>
2024-03-13 01:00:46 -0400

    Add Threads to Store Socials (#2188)

--- includes/functions.php
+++ includes/functions.php
@@ -2380,6 +2380,10 @@ function dokan_get_social_profile_fields() {
             'icon'  => 'flickr',
             'title' => __( 'Flickr', 'dokan-lite' ),
         ],
+        'threads'   => [
+            'icon'  => 'fa-brands fa-threads',
+            'title' => __( 'Threads', 'dokan-lite' ),
+        ],
     ];
 
     return apply_filters( 'dokan_profile_social_fields', $fields );
