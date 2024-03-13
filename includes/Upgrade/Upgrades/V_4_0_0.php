728bce271ded49687cdc2bc31575dcb7041e3a8a
Aunshon <32583103+Aunshon@users.noreply.github.com>
2024-03-13 13:46:19 +0600

    Commission upgrader added.

--- includes/Upgrade/Upgrades/V_4_0_0.php
+++ includes/Upgrade/Upgrades/V_4_0_0.php
@@ -4,15 +4,18 @@ namespace WeDevs\Dokan\Upgrade\Upgrades;
 
 use WeDevs\Dokan\Abstracts\DokanUpgrader;
 use WeDevs\Dokan\Commission\Calculators\FixedCommissionCalculator;
-use WeDevs\Dokan\Upgrade\Upgrades\BackgroundProcesses\V_4_0_0_UpdateCategoryGlobalCommissions;
+use WeDevs\Dokan\Upgrade\Upgrades\BackgroundProcesses\V_4_0_0_UpdateCommissions;
 
 class V_4_0_0 extends DokanUpgrader {
 
+    /**
+     * Update global commission settings.
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return void
+     */
     public static function update_global_commission_type() {
-        /**
-         * TODO: commission-restructure gets all category commission and saves into global category based commisssion.
-         * TODO: commission-restructure update all venodr saved commission type to fixed.
-         */
         $options         = get_option( 'dokan_selling', [] );
         $commission_type = isset( $options['dokan_commission_type'] ) ? $options['dokan_commission_type'] : FixedCommissionCalculator::SOURCE;
 
@@ -22,10 +25,17 @@ class V_4_0_0 extends DokanUpgrader {
         }
     }
 
+    /**
+     * Update global category commisions.
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return void
+     */
     public static function update_global_category_commission_types() {
         $has_categories = true;
         $page_number    = 1;
-        $processor      = new V_4_0_0_UpdateCategoryGlobalCommissions();
+        $processor      = new V_4_0_0_UpdateCommissions();
 
         while ( $has_categories ) {
             $args = [
@@ -42,7 +52,84 @@ class V_4_0_0 extends DokanUpgrader {
             if ( empty( $terms ) ) {
                 $has_categories = false;
             } else {
-                $processor->push_to_queue( $terms );
+                $args = [
+                    'data' => $terms,
+                    'task' => 'global-commission',
+                ];
+                $processor->push_to_queue( $args );
+            }
+
+            ++$page_number;
+        }
+
+        $processor->dispatch_process();
+    }
+
+    /**
+     * Update vendor comission settings.
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return void
+     */
+    public function update_category_commission_of_vendors() {
+        $page_number = 1;
+        $has_vendors = true;
+        $processor   = new V_4_0_0_UpdateCommissions();
+
+        while ( $has_vendors ) {
+            $vendors = dokan()->vendor->all(
+                [
+                    'paged' => $page_number,
+                ]
+            );
+
+            if ( empty( $vendors ) ) {
+                $has_vendors = false;
+            } else {
+                $args = [
+                    'data' => $vendors,
+                    'task' => 'vendors-commission',
+                ];
+                $processor->push_to_queue( $args );
+            }
+
+            ++$page_number;
+        }
+
+        $processor->dispatch_process();
+    }
+
+    /**
+     * Update product commission settings.
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return void
+     */
+    public function update_category_commission_of_products() {
+        $page_number  = 1;
+        $has_products = true;
+        $processor    = new V_4_0_0_UpdateCommissions();
+
+        while ( $has_products ) {
+            $products = dokan()->product->all(
+                [
+                    'posts_per_page' => 10,
+                    'paged'          => $page_number,
+                ]
+            );
+
+            $products = $products->posts;
+
+            if ( empty( $products ) ) {
+                $has_products = false;
+            } else {
+                $args = [
+                    'data' => $products,
+                    'task' => 'products-commission',
+                ];
+                $processor->push_to_queue( $args );
             }
 
             ++$page_number;
