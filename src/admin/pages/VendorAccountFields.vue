257fc2c0a4c7e8ba2bd6af2a33779faecadf0e60
Aunshon <32583103+Aunshon@users.noreply.github.com>
2024-04-02 13:14:45 +0600

    Update checking

--- src/admin/pages/VendorAccountFields.vue
+++ src/admin/pages/VendorAccountFields.vue
@@ -105,7 +105,7 @@
                         <div class="checkbox-left notify-vendor">
                             <switches
                                 @input="sendEmail"
-                                :enabled="true === vendorInfo.notify_vendor ? vendorInfo.notify_vendor : false"
+                                :enabled="true === vendorInfo.notify_vendor"
                                 value="notify_vendor"
                             ></switches>
                             <span class="desc">{{ __( 'Send the vendor an email about their account.', 'dokan-lite' ) }}</span>
