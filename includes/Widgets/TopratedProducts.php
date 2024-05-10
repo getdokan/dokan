ffb974930c734af009b0509830dedb27b40706ef
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-05-10 15:59:26 +0600

    style: ignore escaping for dynamic content

--- includes/Widgets/TopratedProducts.php
+++ includes/Widgets/TopratedProducts.php
@@ -34,9 +34,10 @@ class TopratedProducts extends WP_Widget {
 
         $r = dokan_get_top_rated_products( $no_of_product );
 
-        echo $args['before_widget']; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+        echo $args['before_widget']; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped,WordPress.Security.EscapeOutput.OutputNotEscaped
+
         if ( ! empty( $title ) ) {
-            echo $args['before_title'] . $title . $args['after_title']; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+            echo $args['before_title'] . $title . $args['after_title']; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped,WordPress.Security.EscapeOutput.OutputNotEscaped
         }
 
         dokan_get_template_part(
@@ -46,7 +47,7 @@ class TopratedProducts extends WP_Widget {
             )
         );
 
-        echo $args['after_widget']; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+        echo $args['after_widget']; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped,WordPress.Security.EscapeOutput.OutputNotEscaped
 
         wp_reset_postdata();
     }
@@ -103,5 +104,4 @@ class TopratedProducts extends WP_Widget {
         $instance['show_rating']   = ( ! empty( $new_instance['show_rating'] ) ) ? sanitize_text_field( $new_instance['show_rating'] ) : '';
         return $instance;
     }
-
-} // class Dokan best selling product widget
+}
