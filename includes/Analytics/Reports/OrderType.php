b42cfc9cf288d5eb6ec13ad0ae3f3439574b2d40
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2024-07-22 16:38:22 +0600

    Add method to get order type

--- includes/Analytics/Reports/OrderType.php
+++ includes/Analytics/Reports/OrderType.php
@@ -5,13 +5,14 @@ namespace WeDevs\Dokan\Analytics\Reports;
 use WC_Order_Refund;
 
 class OrderType {
-	public const ORDER_TYPE_WC_ORDER = 0;
-	public const ORDER_TYPE_DOKAN_SUBORDER = 1;
-	public const ORDER_TYPE_WC_ORDER_REFUND = 2;
-	public const ORDER_TYPE_DOKAN_SUBORDER_REFUND = 3;
+	public const WC_ORDER = 0;
+	public const DOKAN_SINGLE_ORDER = 1;
+	public const DOKAN_SUBORDER = 2;
+	public const WC_ORDER_REFUND = 3;
+	public const DOKAN_SUBORDER_REFUND = 4;
 
 
-	public function is_suborder_related( \WC_Abstract_Order $order ) {
+	public function is_dokan_suborder_related( \WC_Abstract_Order $order ) {
 		if ( ! $order->get_parent_id() ) {
 			return false;
 		}
@@ -22,27 +23,52 @@ class OrderType {
 
 		$parent_order = wc_get_order( $order->get_parent_id() );
 
-		return $this->is_suborder_related( $parent_order );
+		return $this->is_dokan_suborder_related( $parent_order );
 	}
 
 	public function get_type( \WC_Abstract_Order $order ) {
-		$is_suborder_related = self::is_suborder_related( $order );
+		$is_suborder_related = self::is_dokan_suborder_related( $order );
 
-		// Refund of Dokan suborder.
-		if ( $is_suborder_related && $order instanceof WC_Order_Refund ) {
-			return self::ORDER_TYPE_DOKAN_SUBORDER_REFUND;
-		}
-
-		// Refund of WC order.
-		if ( ! $is_suborder_related && $order instanceof WC_Order_Refund ) {
-			return self::ORDER_TYPE_WC_ORDER_REFUND;
-		}
-
-		// Dokan Suborder
 		if ( $is_suborder_related ) {
-			return self::ORDER_TYPE_DOKAN_SUBORDER;
+            // Refund of Dokan suborder.
+            if ( $order instanceof WC_Order_Refund ) {
+                return self::DOKAN_SUBORDER_REFUND;
+            }
+
+		    // Dokan Suborder
+			return self::DOKAN_SUBORDER;
 		}
 
-		return self::ORDER_TYPE_WC_ORDER;
+		if ( ! $is_suborder_related ) {
+		    // Refund of WC order.
+            if ( $order instanceof WC_Order_Refund ) {
+				return self::WC_ORDER_REFUND;
+			}
+
+            $suborder_ids = dokan_get_suborder_ids_by( $order->get_id() );
+
+            // Dokan Single Vendor Order
+            if ( $suborder_ids === null || ( is_array( $suborder_ids ) && count( $suborder_ids ) === 0 ) ) {
+                return self::DOKAN_SINGLE_ORDER;
+            }
+		}
+
+		return self::WC_ORDER;
 	}
+
+    public function get_types_for_admin(): array {
+        return [
+            self::WC_ORDER,
+            self::DOKAN_SINGLE_ORDER,
+            self::WC_ORDER_REFUND,
+        ];
+    }
+
+    public function get_types_for_seller(): array {
+        return [
+            self::DOKAN_SINGLE_ORDER,
+            self::DOKAN_SUBORDER,
+            self::DOKAN_SUBORDER_REFUND,
+        ];
+    }
 }
---
+++ tests/php/src/Analytics/OrderTypeTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace WeDevs\Dokan\Test\Analytics;
+
+use Exception;
+use WeDevs\Dokan\Analytics\Reports\OrderType;
+use WeDevs\Dokan\Test\DokanTestCase;
+
+class OrderTypeTest extends DokanTestCase {
+    public function test_wc_order_is_dokan_suborder_related_method() {
+        $parent_id = $this->create_multi_vendor_order();
+
+        $order_type = new OrderType();
+
+        $parent_order = wc_get_order( $parent_id );
+
+        $this->assertFalse( $order_type->is_dokan_suborder_related( $parent_order ) );
+
+        $sub_order_ids = dokan_get_suborder_ids_by( $parent_id );
+
+        foreach ( $sub_order_ids as $sub_id ) {
+            $sub_order = wc_get_order( $sub_id );
+
+            $this->assertTrue( $order_type->is_dokan_suborder_related( $sub_order ) );
+        }
+    }
+
+    public function test_order_type_method_for_multi_vendor() {
+        $parent_id = $this->create_multi_vendor_order();
+
+        $order_type = new OrderType();
+
+        $parent_order = wc_get_order( $parent_id );
+
+        $this->assertEquals( OrderType::WC_ORDER, $order_type->get_type( $parent_order ) );
+
+        $sub_order_ids = dokan_get_suborder_ids_by( $parent_id );
+
+        foreach ( $sub_order_ids as $sub_id ) {
+            $sub_order = wc_get_order( $sub_id );
+
+            $this->assertEquals( OrderType::DOKAN_SUBORDER, $order_type->get_type( $sub_order ) );
+        }
+    }
+
+    public function test_order_type_method_for_single_vendor() {
+        $this->seller_id2 = $this->seller_id1;
+
+        $parent_id = $this->create_multi_vendor_order();
+
+        $order_type = new OrderType();
+
+        $parent_order = wc_get_order( $parent_id );
+
+        $this->assertEquals( OrderType::DOKAN_SINGLE_ORDER, $order_type->get_type( $parent_order ) );
+
+        $sub_order_ids = dokan_get_suborder_ids_by( $parent_id );
+
+		$this->assertNull( $sub_order_ids );
+    }
+
+    public function test_order_type_for_dokan_sub_refund() {
+        $parent_id = $this->create_multi_vendor_order();
+
+        $sub_order_ids = dokan_get_suborder_ids_by( $parent_id );
+
+        $sub_id = $sub_order_ids[0];
+
+        $sub_order = wc_get_order( $sub_id );
+
+        $sub_order_items = $sub_order->get_items();
+
+        foreach ( $sub_order_items as $key => $item ) {
+            $item_id = $item->get_id();
+
+            $qty = $item->get_quantity() - 1;
+            $amount = $item->get_total();
+
+            $line_items[ $item_id ] = array(
+                'qty'          => $qty,
+                'refund_total' => $amount,
+                'refund_tax'   => array(),
+            );
+
+            break;
+        }
+
+        // Create the refund object.
+        $refund = wc_create_refund(
+            array(
+                'reason'         => 'Testing Refund',
+                'order_id'       => $sub_id,
+                'line_items'     => $line_items,
+            )
+        );
+
+		if ( is_wp_error( $refund ) ) {
+			throw new Exception( $refund->get_error_message() );
+		}
+
+        $order_type = new OrderType();
+
+        $this->assertEquals( OrderType::DOKAN_SUBORDER_REFUND, $order_type->get_type( $refund ) );
+    }
+
+    public function test_order_type_for_wc_refund() {
+        $parent_id = $this->create_multi_vendor_order();
+        $parent_order = wc_get_order( $parent_id );
+        $order_items = $parent_order->get_items();
+
+        foreach ( $order_items as $key => $item ) {
+            $item_id = $item->get_id();
+
+            $qty = $item->get_quantity() - 1;
+            $amount = $item->get_total();
+
+            $line_items[ $item_id ] = array(
+                'qty'          => $qty,
+                'refund_total' => $amount,
+                'refund_tax'   => array(),
+            );
+
+            break;
+        }
+
+        // Create the refund object.
+        $refund = wc_create_refund(
+            array(
+                'reason'         => 'Testing Refund',
+                'order_id'       => $parent_id,
+                'line_items'     => $line_items,
+            )
+        );
+
+		if ( is_wp_error( $refund ) ) {
+			throw new Exception( $refund->get_error_message() );
+		}
+
+        $order_type = new OrderType();
+
+        $this->assertEquals( OrderType::WC_ORDER_REFUND, $order_type->get_type( $refund ) );
+    }
+}
