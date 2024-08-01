6ae99927c30911d3b91df9d0be3e289836c27ddf
Md. Asif Hossain Nadim <90011088+MdAsifHossainNadim@users.noreply.github.com>
2024-08-01 12:07:46 +0600

    fix: Admin color picker settings/component not working as expected. (#2316)

--- src/admin/components/Fields.vue
+++ src/admin/components/Fields.vue
@@ -280,10 +280,9 @@
                         <color-picker
                             :itemKey="fieldData.name"
                             :customData="singleColorPicker"
-                            @custom-change="e => setCustomColor( e, fieldData.name )"
+                            v-model="fieldValue[fieldData.name]"
+                            @custom-change="e => setCustomColor( e, fieldData.name, fieldData.default )"
                             @toggleColorPicker="toggleColorPicker"
-                            :value="fieldValue[fieldData.name]"
-                            @input="event => inputValueHandler( fieldData.name, event.target.value, fieldValue[fieldData.name] )"
                         ></color-picker>
                     </div>
                 </fieldset>
@@ -723,6 +722,7 @@
                   fieldData.is_lite ?? false
                 );
             },
+
             inputValueHandler( name, newValue, oldValue ) {
               this.fieldValue[ name ] = this.validateInputData( name, newValue, oldValue, this.fieldData );
             },
@@ -925,12 +925,15 @@
                 }
             },
 
-            setCustomColor( value, key ) {
+            setCustomColor( value, key, defaultValue ) {
                 if ( ! key ) {
                     return;
                 }
 
-                this.fieldData[ key ] = value;
+                // Regular expression to validate hex color code
+                const hexPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+
+                this.fieldData[ key ] = hexPattern.test( value ) ? value : defaultValue;
             },
         },
     };
