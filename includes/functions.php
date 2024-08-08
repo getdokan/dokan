789d0b160b2c5affaa8ff1467b5ac4c6111dc89e
Asad Nur <asaduzzamannur.dev@gmail.com>
2024-08-08 09:25:24 +0600

    fix: Translation cache not flashing-after-updating-plugin-language-directory-files

--- includes/functions.php
+++ includes/functions.php
@@ -2950,8 +2950,10 @@ function dokan_get_translations_for_plugin_domain( $domain, $language_dir = null
  * @return array
  */
 function dokan_get_jed_locale_data( $domain, $language_dir = null ) {
+    $plugin_language_dir = WP_LANG_DIR . '/plugins/';
+
     // get transient key
-    $transient_key = sprintf( 'dokan_i18n-%s-%d-%s', $domain, filectime( $language_dir ), get_user_locale() );
+    $transient_key = sprintf( 'dokan_i18n-%s-%d-%d-%s', $domain, filectime( $language_dir ), filectime( $plugin_language_dir ), get_user_locale() );
 
     // check if data exists on cache or not
     $locale = Cache::get_transient( $transient_key );
