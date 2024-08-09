ea04207ab5498f809041777861e48951be7f7c4d
shashwata Halder <shashwatahalder01@gmail.com>
2024-08-09 13:32:41 +0600

    Update: Add support for authentication plugin 'caching_sha2_password' (automation suite) (#2331)
    * Replace mysqlconnector with mysql2 for mysql caching_sha2_password support and refactor dbutils
    
    * Add plugin setup  and refactor auth setup
    
    * Update localsite setup methods with cli commands
    
    * Update serialize parameter for all setDokanSettings method

--- tests/pw/utils/helpers.ts
+++ tests/pw/utils/helpers.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
 import { Browser, BrowserContextOptions, Page } from '@playwright/test';
-import open from 'open';
 
 export const helpers = {
     // replace '_' to space & capitalize first letter of string
@@ -42,12 +41,6 @@ export const helpers = {
 
     // check if object is empty
     isObjEmpty: (obj: object) => Object.keys(obj).length === 0,
-
-    // opens the url in the default browser
-    openUrl: (url: string) => open(url),
-
-    // opens test report in the default browser
-    openReport: () => open('playwright-report/html-report/index.html'),
 
     // string between two tags
     stringBetweenTags: (str: string): string => {
@@ -376,9 +369,14 @@ export const helpers = {
     // execute command
     async exeCommand(command: string, directoryPath = process.cwd()) {
         process.chdir(directoryPath);
-
-        const output = execSync(command, { encoding: 'utf-8' });
-        console.log(output);
+        try {
+            const output = execSync(command, { encoding: 'utf-8' });
+            console.log(output);
+            return output;
+        } catch (error: any) {
+            console.log(error);
+            return error;
+        }
     },
 
     // create a new page
