67c98e0b8461e08239f170f8fca1ac9d2c9a9e21
Aunshon <32583103+Aunshon@users.noreply.github.com>
2024-09-06 15:07:33 +0600

    Fix param type

--- includes/Product/Manager.php
+++ includes/Product/Manager.php
@@ -722,18 +722,18 @@ class Manager {
      *
      * @param string                $term
      * @param boolean|integer|array $user_ids
-     * @param integer               $exclude
-     * @param integer               $included_id
+     * @param array                 $exclude
+     * @param array                 $included_id
      * @param integer               $limit
      *
      * @return WC_Product[]
      */
-    public function get_linked_products( $term = '', $user_ids = false, $exclude = null, $included_id = null, $limit = 0 ) {
+    public function get_linked_products( $term = '', $user_ids = [], $exclude = [], $included_id = [], $limit = 0 ) {
         $term     = ! empty( $term ) ? sanitize_text_field( wp_unslash( $term ) ) : '';
         $user_ids = ! empty( $user_ids ) ? array_filter( array_map( 'absint', (array) wp_unslash( $user_ids ) ) ) : false;
 
         if ( empty( $term ) ) {
-            wp_die();
+            return [];
         }
 
         $ids = dokan_search_seller_products( $term, $user_ids, '', true );
