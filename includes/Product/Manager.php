ac2f50b10d2c4733e05cda3c333e8072d620ac5c
Aunshon <32583103+Aunshon@users.noreply.github.com>
2024-09-06 16:26:01 +0600

    Move linked product method to dokan pro

--- includes/Product/Manager.php
+++ includes/Product/Manager.php
@@ -714,42 +714,4 @@ class Manager {
 
         return $products;
     }
-
-    /**
-     * Returns linked products.
-     *
-     * @since DOKAN_SINCE
-     *
-     * @param string                $term
-     * @param boolean|integer|array $user_ids
-     * @param array                 $exclude
-     * @param array                 $included_id
-     * @param integer               $limit
-     *
-     * @return WC_Product[]
-     */
-    public function get_linked_products( $term = '', $user_ids = [], $exclude = [], $included_id = [], $limit = 0 ) {
-        $term     = ! empty( $term ) ? sanitize_text_field( wp_unslash( $term ) ) : '';
-        $user_ids = ! empty( $user_ids ) ? array_filter( array_map( 'absint', (array) wp_unslash( $user_ids ) ) ) : false;
-
-        if ( empty( $term ) ) {
-            return [];
-        }
-
-        $ids = dokan_search_seller_products( $term, $user_ids, '', true );
-
-        if ( ! empty( $exclude ) ) {
-            $ids = array_diff( $ids, (array) sanitize_text_field( wp_unslash( $exclude ) ) );
-        }
-
-        if ( ! empty( $included_id ) ) {
-            $ids = array_intersect( $ids, (array) sanitize_text_field( wp_unslash( $included_id ) ) );
-        }
-
-        if ( ! empty( $limit ) ) {
-            $ids = array_slice( $ids, 0, absint( $limit ) );
-        }
-
-        return array_filter( array_map( 'wc_get_product', $ids ), 'dokan_products_array_filter_editable' );
-    }
 }
