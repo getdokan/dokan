5c20890ea3e53daaf0e4e5bf8beb78c1debe8f14
Shazahanul Islam Shohag <shohag121@gmail.com>
2024-09-23 12:19:06 +0600

    chore: Release Version 3.12.2

--- dokan.php
+++ dokan.php
@@ -3,13 +3,13 @@
  * Plugin Name: Dokan
  * Plugin URI: https://dokan.co/wordpress/
  * Description: An e-commerce marketplace plugin for WordPress. Powered by WooCommerce and weDevs.
- * Version: 3.12.1
+ * Version: 3.12.2
  * Author: weDevs
  * Author URI: https://dokan.co/
  * Text Domain: dokan-lite
  * Requires Plugins: woocommerce
  * WC requires at least: 8.0.0
- * WC tested up to: 9.1.4
+ * WC tested up to: 9.3.2
  * Domain Path: /languages/
  * License: GPL2
  */
@@ -66,7 +66,7 @@ final class WeDevs_Dokan {
      *
      * @var string
      */
-    public $version = '3.12.1';
+    public $version = '3.12.2';
 
     /**
      * Instance of self
