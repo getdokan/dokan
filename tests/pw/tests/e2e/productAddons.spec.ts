248c6ab80d596c74f16c19f2660c4792f71061ae
shashwata <shashwatahalder01@gmail.com>
2024-09-27 15:35:53 +0600

    Add: add product addon tests

--- tests/pw/tests/e2e/productAddons.spec.ts
+++ tests/pw/tests/e2e/productAddons.spec.ts
@@ -4,6 +4,8 @@ import { ApiUtils } from '@utils/apiUtils';
 import { data } from '@utils/testData';
 import { payloads } from '@utils/payloads';
 import { dbUtils } from '@utils/dbUtils';
+import { serialize } from 'php-serialize';
+import { responseBody } from '@utils/interfaces';
 
 const { VENDOR_ID } = process.env;
 
@@ -16,11 +18,11 @@ test.describe('Product addon functionality test', () => {
     let apiUtils: ApiUtils;
 
     // create product addon
-    async function createVendorProductAddon(): Promise<[string, string, string, string]> {
+    async function createVendorProductAddon(): Promise<[responseBody, string, string, string, string]> {
         const [, categoryId, categoryName] = await apiUtils.createCategory(payloads.createCategoryRandom(), payloads.adminAuth);
-        const [, addonId, addonName, addonFieldTitle] = await apiUtils.createProductAddon({ ...payloads.createProductAddons(), restrict_to_categories: [categoryId] }, payloads.adminAuth);
+        const [responseBody, addonId, addonName, addonFieldTitle] = await apiUtils.createProductAddon({ ...payloads.createGlobalProductAddons(), restrict_to_categories: [categoryId] }, payloads.adminAuth);
         await dbUtils.updateCell(addonId, VENDOR_ID);
-        return [addonId, addonName, addonFieldTitle, categoryName];
+        return [responseBody, addonId, addonName, addonFieldTitle, categoryName];
     }
 
     test.beforeAll(async ({ browser }) => {
@@ -29,7 +31,7 @@ test.describe('Product addon functionality test', () => {
         vendor = new ProductAddonsPage(vPage);
 
         apiUtils = new ApiUtils(await request.newContext());
-        [, addonName, addonFieldTitle, categoryName] = await createVendorProductAddon();
+        [, , addonName, addonFieldTitle, categoryName] = await createVendorProductAddon();
     });
 
     test.afterAll(async () => {
@@ -39,20 +41,39 @@ test.describe('Product addon functionality test', () => {
     });
 
     // vendor
+
     test('vendor can view product addons menu page', { tag: ['@pro', '@exploratory', '@vendor'] }, async () => {
         await vendor.vendorProductAddonsSettingsRenderProperly();
     });
+
+    // global addon
 
     test('vendor can add global product addon', { tag: ['@pro', '@vendor'] }, async () => {
         await vendor.addAddon({ ...data.vendor.addon(), category: categoryName });
     });
 
-    test('vendor can edit product addon', { tag: ['@pro', '@vendor'] }, async () => {
+    test('vendor can edit global product addon', { tag: ['@pro', '@vendor'] }, async () => {
         await vendor.editAddon({ ...data.vendor.addon(), name: addonName, title: addonFieldTitle });
     });
 
-    test('vendor can delete product addon', { tag: ['@pro', '@vendor'] }, async () => {
-        const [, addonName] = await createVendorProductAddon();
-        await vendor.deleteAddon({ ...data.vendor.addon(), name: addonName });
+    test('vendor can import global product addon field', { tag: ['@pro', '@vendor'] }, async () => {
+        const [, addonId, , addonFieldTitle] = await createVendorProductAddon();
+        await vendor.importAddonField(addonId, serialize([payloads.createGlobalProductAddons().fields[0]]), addonFieldTitle);
     });
+
+    test('vendor can export global product addon field', { tag: ['@pro', '@vendor'] }, async () => {
+        const [responseBody, addonId] = await createVendorProductAddon();
+        await vendor.exportAddonField(addonId, responseBody.fields[0]);
+    });
+
+    test('vendor can remove product addon field', { tag: ['@pro', '@vendor'] }, async () => {
+        const [, addonId, , addonFieldTitle] = await createVendorProductAddon();
+        await vendor.removeAddonField(addonId, addonFieldTitle);
+    });
+
+    test('vendor can remove global product addon', { tag: ['@pro', '@vendor'] }, async () => {
+        const [, , addonName] = await createVendorProductAddon();
+        await vendor.removeAddon({ ...data.vendor.addon(), name: addonName });
+    });
+
 });
