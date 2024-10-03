5a818a62783c444dc70ec07f1084f900a43f18e8
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2024-10-03 12:59:59 +0600

    Update comment for ordery_type  table column

--- includes/Install/Installer.php
+++ includes/Install/Installer.php
@@ -547,7 +547,7 @@ class Installer {
         $sql = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}dokan_order_stats` (
                 `order_id` bigint UNSIGNED NOT NULL,
                 `seller_id` bigint UNSIGNED NOT NULL DEFAULT '0',
-                `order_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = WC Order, 1 = Dokan Single Vendor Order, 2 = Dokan Suborder, 3 = Refund of Dokan Parent Order, 4 = Refund of Dokan Suborder, 5 =  4 = Refund of Dokan Single Order',
+                `order_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = Dokan Parent Order, 1 = Dokan Single Vendor Order, 2 = Dokan Suborder, 3 = Refund of Dokan Parent Order, 4 = Refund of Dokan Suborder, 5 =  Refund of Dokan Single Order',
                 `seller_earning` double NOT NULL DEFAULT '0',
                 `seller_gateway_fee` double NOT NULL DEFAULT '0',
                 `seller_shipping_fee` double NOT NULL DEFAULT '0',
