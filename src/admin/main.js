959b52d7fadd6af06b73f0a3f1137864a59c5ba5
osman sufy <osmansufy20@gmail.com>
2024-10-17 12:09:41 +0600

    [remove] setLocaleData dokan.i18n['dokan-lite'] from /admin/main.js

--- src/admin/main.js
+++ src/admin/main.js
@@ -1,19 +1,18 @@
-import App from './App.vue'
-import router from './router'
-import menuFix from './utils/admin-menu-fix'
+import App from './App.vue';
+import router from './router';
+import menuFix from './utils/admin-menu-fix';
 
 /* eslint-disable no-new */
-let Vue = dokan_get_lib('Vue');
+let Vue = dokan_get_lib( 'Vue' );
 
-new Vue({
+new Vue( {
     el: '#dokan-vue-admin',
     router,
-    render: h => h(App),
+    render: ( h ) => h( App ),
 
     created() {
-        this.setLocaleData( dokan.i18n['dokan-lite'] );
         if ( dokan.dokan_pro_i18n ) {
-            this.setLocaleData( dokan.dokan_pro_i18n['dokan'] );
+            this.setLocaleData( dokan.dokan_pro_i18n[ 'dokan' ] );
         }
     },
 
@@ -21,15 +20,17 @@ new Vue({
         listTableTexts() {
             return {
                 loading: this.__( 'Loading', 'dokan-lite' ),
-                select_bulk_action: this.__( 'Select bulk action', 'dokan-lite' ),
+                select_bulk_action: this.__(
+                    'Select bulk action',
+                    'dokan-lite'
+                ),
                 bulk_actions: this.__( 'Bulk Actions', 'dokan-lite' ),
                 items: this.__( 'items', 'dokan-lite' ),
                 apply: this.__( 'Apply', 'dokan-lite' ),
-            }
-        }
+            };
+        },
     },
-});
-
+} );
 
 // fix the admin menu for the slug "vue-app"
-menuFix('dokan');
+menuFix( 'dokan' );
