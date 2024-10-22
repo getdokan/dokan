0b1ce0249c1458e20a596025e6b62fbcd017fbc2
Shazahanul Islam Shohag <shohag121@gmail.com>
2024-10-22 16:24:04 +0600

    update: `useNavigate` send as fillProps for `dokan-header-actions`

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,6 +1,8 @@
 import {Slot} from "@wordpress/components";
+import {useNavigate} from "react-router-dom";
 
 const Header = ( { title = '' } ) => {
+    const navigate = useNavigate();
 
     return (
         <div className="flex items-center sm:flex-wrap md:flex-nowrap">
@@ -9,7 +11,7 @@ const Header = ( { title = '' } ) => {
                 { title && (<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">{title}</h1>)}
             </div>
             <div className="dokan-header-actions flex flex-1 gap-2.5 md:justify-end">
-                <Slot name="dokan-header-actions"/>
+                <Slot name="dokan-header-actions" fillProps={ { navigate } }/>
             </div>
             <Slot name="dokan-after-header"/>
         </div>
