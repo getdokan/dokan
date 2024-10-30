4b461fc94bfe8fde1ccd450757709656582eaf93
MdAsifHossainNadim <nadim@wedevs.com>
2024-10-30 10:03:54 +0600

    revert: remove QueryFilter data.

--- includes/Analytics/Reports/Orders/Stats/QueryFilter.php
+++ includes/Analytics/Reports/Orders/Stats/QueryFilter.php
@@ -3,7 +3,6 @@
 namespace WeDevs\Dokan\Analytics\Reports\Orders\Stats;
 
 use WeDevs\Dokan\Analytics\Reports\Orders\QueryFilter as OrdersQueryFilter;
-use WeDevs\Dokan\Analytics\Reports\OrderType;
 
 /**
  * Class QueryFilter
@@ -57,30 +56,11 @@ class QueryFilter extends OrdersQueryFilter {
             return $column;
         }
 
-        global $wpdb;
-
         $table_name = $this->get_dokan_table();
         $types      = $this->get_order_types_for_sql_excluding_refunds();
 
-        // Calculate refunds to resolve Dokan refund distribution issue.
         $order_count = "SUM( CASE WHEN {$table_name}.order_type IN($types) THEN 1 ELSE 0 END )";
-        $refunds     = "ABS( SUM( CASE WHEN {$wc_table_name}.net_total < 0 THEN {$wc_table_name}.net_total ELSE 0 END ) )";
 
-        // Calculate coupons to resolve Dokan coupon distribution issue.
-        $parent_order_types_str = implode( ',', ( new OrderType() )->get_admin_order_types_excluding_refunds() );
-        $wc_coupon_table        = "{$wpdb->prefix}wc_order_coupon_lookup";
-        $coupon                 = "( Select SUM(discount_amount) FROM {$wc_coupon_table} JOIN {$table_name} ON {$table_name}.order_id = {$wc_coupon_table}.order_id WHERE {$table_name}.order_type IN($parent_order_types_str) )";
-
-        $gross_sales =
-            "( SUM({$wc_table_name}.total_sales)" .
-            " + COALESCE($coupon, 0)" . // SUM() all nulls gives null.
-            " - SUM({$wc_table_name}.tax_total)" .
-            " - SUM({$wc_table_name}.shipping_total)" .
-            " + {$refunds}" .
-            ' ) as gross_sales';
-
-        $column['gross_sales']          = $gross_sales;
-        $column['coupons']              = "{$coupon} AS coupons";
         $column['orders_count']         = "{$order_count} as orders_count";
         $column['avg_items_per_order']  = "SUM( {$wc_table_name}.num_items_sold ) / {$order_count} AS avg_items_per_order";
         $column['avg_order_value']      = "SUM( {$wc_table_name}.net_total ) / {$order_count} AS avg_order_value";
