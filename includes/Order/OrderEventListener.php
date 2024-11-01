9e93bfdc7aef21bea50992da81b060376042120d
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-11-01 10:48:01 +0600

    update: proper logging the trash and untrash orders

--- includes/Order/OrderEventListener.php
+++ includes/Order/OrderEventListener.php
@@ -13,9 +13,14 @@ class OrderEventListener {
     }
 
     /**
-     * Perform actions after an order is trashed
+     * Perform actions after an order is trashed.
      *
-     * @param int $order_id ID of the trashed order
+     * This method is triggered when an order is moved to the trash. It updates the order status
+     * in the Dokan tables and logs the action.
+     *
+     * @param int $order_id ID of the trashed order.
+     *
+     * @return void
      */
     public function after_order_trash( int $order_id ) {
         global $wpdb;
@@ -26,61 +31,19 @@ class OrderEventListener {
             return;
         }
 
-        $status = $order->get_status( 'edit' );
-        if ( strpos( $status, 'wc-' ) === false ) {
-            $status = 'wc-' . $status;
-        }
-
-        // Update Dokan tables
-        $wpdb->update(
-            $wpdb->prefix . 'dokan_orders',
-            array( 'order_status' => $status ),
-            array( 'order_id' => $order_id ),
-            array( '%s' ),
-            array( '%d' )
-        );
-
-        $wpdb->update(
-            $wpdb->prefix . 'dokan_vendor_balance',
-            array( 'status' => $status ),
-            array(
-				'trn_id' => $order_id,
-				'trn_type' => 'dokan_orders',
-            ),
-            array( '%s' ),
-            array( '%d', '%s' )
-        );
-
-		$user_id = doka_get_current_user_id();
-		if ( $user_id ) {
-			$user = get_user_by( 'id', $user_id );
-			$user_name = sprintf( '%s %s', $user->first_name, $user->last_name );
-			$user_role = implode( ', ', $user->roles );
-			$user_ip = WC_Geolocation::get_ip_address();
-
-			dokan_log(
-                sprintf(
-                    'Order %d has been trashed in Dokan order and vendor balance tables by %s (%s) from IP: %s.',
-                    $order_id,
-                    $user_name,
-                    $user_role,
-                    $user_ip
-                )
-			);
-		} else {
-			dokan_log(
-                sprintf(
-                    'Order %d has been trashed in Dokan order and vendor balance tables by System (system).',
-                    $order_id
-                )
-			);
-		}
+        $this->update_order_status( $order, $wpdb, $order_id );
+        $this->log_order_action( 'trashed', $order_id );
     }
 
     /**
-     * Perform actions after an order is untrashed (restored)
+     * Perform actions after an order is untrashed (restored).
      *
-     * @param int $order_id ID of the restored order
+     * This method is triggered when an order is restored from the trash. It updates the order status
+     * in the Dokan tables and logs the action.
+     *
+     * @param int $order_id ID of the restored order.
+     *
+     * @return void
      */
     public function after_order_untrash( int $order_id ) {
         global $wpdb;
@@ -91,6 +54,25 @@ class OrderEventListener {
             return;
         }
 
+        $this->update_order_status( $order, $wpdb, $order_id );
+        $this->log_order_action( 'restored', $order_id );
+    }
+
+    /**
+     * Update the order status in Dokan tables.
+     *
+     * This method updates the order status in the `dokan_orders` and `dokan_vendor_balance` tables
+     * based on the current status of the WooCommerce order.
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param WC_Order $order The WooCommerce order object.
+     * @param \wpdb    $wpdb  The WordPress database object.
+     * @param int      $order_id The ID of the order.
+     *
+     * @return void
+     */
+    protected function update_order_status( WC_Order $order, \wpdb $wpdb, int $order_id ): void {
         $previous_status = $order->get_status( 'edit' );
         if ( strpos( $previous_status, 'wc-' ) === false ) {
             $previous_status = 'wc-' . $previous_status;
@@ -109,13 +91,28 @@ class OrderEventListener {
             $wpdb->prefix . 'dokan_vendor_balance',
             array( 'status' => $previous_status ),
             array(
-				'trn_id' => $order_id,
-				'trn_type' => 'dokan_orders',
+                'trn_id'   => $order_id,
+                'trn_type' => 'dokan_orders',
             ),
             array( '%s' ),
             array( '%d', '%s' )
         );
+    }
 
+    /**
+     * Log the order action.
+     *
+     * This method logs the action performed on the order (e.g., trashed, restored) along with
+     * the user details who performed the action.
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param string $action The action performed on the order (e.g., 'trashed', 'restored').
+     * @param int $order_id The ID of the order.
+     *
+     * @return void
+     */
+    private function log_order_action( string $action, int $order_id ) {
         $user_id = dokan_get_current_user_id();
         if ( $user_id ) {
             $user = get_user_by( 'id', $user_id );
@@ -125,8 +122,9 @@ class OrderEventListener {
 
             dokan_log(
                 sprintf(
-                    'Order %d has been restored in Dokan order and vendor balance tables by %s (%s) from IP: %s.',
+                    'Order %d has been %s in Dokan order and vendor balance tables by %s (%s) from IP: %s.',
                     $order_id,
+                    $action,
                     $user_name,
                     $user_role,
                     $user_ip
@@ -135,8 +133,9 @@ class OrderEventListener {
         } else {
             dokan_log(
                 sprintf(
-                    'Order %d has been restored in Dokan order and vendor balance tables by System (system).',
-                    $order_id
+                    'Order %d has been %s in Dokan order and vendor balance tables by System (system).',
+                    $order_id,
+                    $action
                 )
             );
         }
