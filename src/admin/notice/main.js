e5553dad713b1a324a4d167f5a1133d1fe0fb9d5
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-11-25 14:08:47 +0600

    fix: Clicking on upgrader button doesn't work if notice is set to global scope

--- src/admin/notice/main.js
+++ src/admin/notice/main.js
@@ -1,8 +1,12 @@
-import App from './App.vue';
-const { jQuery: $ } = window;
 import Vue from 'vue';
+import App from './App.vue';
+import Mixin from '../../utils/Mixin';
+
+const { jQuery: $ } = window;
 
 if ( $( '#dokan-admin-notices' ).length ) {
+    Vue.mixin( Mixin );
+
     new Vue( {
         el: '#dokan-admin-notices',
         render: ( h ) => h( App ),
