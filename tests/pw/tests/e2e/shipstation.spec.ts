0861679d03c6f771bb88cf90e61b2665b64e0ee5
shashwata <shashwatahalder01@gmail.com>
2024-11-27 09:40:44 +0600

    change filename temporary

--- tests/pw/tests/e2e/shipstation.spec.ts
+++ tests/pw/tests/e2e/shipstation.spec.ts
@@ -1,5 +1,5 @@
 import { test, Page } from '@playwright/test';
-import { ShipStationPage } from '@pages/shipStationPage';
+import { ShipStationPage } from '@pages/shipPage';
 import { data } from '@utils/testData';
 
 test.describe('ShipStation test', () => {
