c04f5cfaf6bc4284f4fa5ffc54631ff99ff681a2
shashwata <shashwatahalder01@gmail.com>
2024-11-29 08:45:49 +0600

    Fix skipped store tests

--- tests/pw/tests/e2e/singleStore.spec.ts
+++ tests/pw/tests/e2e/singleStore.spec.ts
@@ -22,8 +22,7 @@ test.describe('Single store functionality test', () => {
         await customer.singleStoreRenderProperly(data.predefined.vendorStores.vendor1);
     });
 
-    test.skip('customer can view store open-close time on single store', { tag: ['@lite', '@customer'] }, async () => {
-        // todo: pre: need store open close time
+    test('customer can view store open-close time on single store', { tag: ['@lite', '@customer'] }, async () => {
         await customer.storeOpenCloseTime(data.predefined.vendorStores.vendor1);
     });
 
