c747a69983f37ba2db98bae7dd98aace4adb87e7
shashwata <shashwatahalder01@gmail.com>
2024-12-01 00:17:03 +0600

    Add colors tests

--- tests/pw/tests/e2e/colors.spec.ts
+++ tests/pw/tests/e2e/colors.spec.ts
@@ -1,22 +1,32 @@
-import { test, Page } from '@playwright/test';
+import { test, Page, request } from '@playwright/test';
 import { ColorsPage } from '@pages/colorsPage';
+import { ApiUtils } from '@utils/apiUtils';
 import { dbUtils } from '@utils/dbUtils';
 import { data } from '@utils/testData';
 import { dbData } from '@utils/dbData';
+import { payloads } from '@utils/payloads';
 
 test.describe('Color scheme customizer test', () => {
     let admin: ColorsPage;
     let aPage: Page;
+    let apiUtils: ApiUtils;
 
     test.beforeAll(async ({ browser }) => {
         const adminContext = await browser.newContext(data.auth.adminAuth);
         aPage = await adminContext.newPage();
         admin = new ColorsPage(aPage);
+
+        apiUtils = new ApiUtils(await request.newContext());
     });
 
     test.afterAll(async () => {
         await dbUtils.setOptionValue(dbData.dokan.optionName.colors, dbData.dokan.colorsSettings);
+        await apiUtils.activateModules(payloads.moduleIds.colorSchemeCustomize, payloads.adminAuth);
         await aPage.close();
+    });
+
+    test('admin can enable color scheme customizer module', { tag: ['@pro', '@admin'] }, async () => {
+        await admin.enableColorSchemeCustomizerModule();
     });
 
     test('admin can switch predefined color palette', { tag: ['@pro', '@admin'] }, async () => {
@@ -30,5 +40,10 @@ test.describe('Color scheme customizer test', () => {
 
     test('admin can update custom color palette', { tag: ['@pro', '@admin'] }, async () => {
         await admin.addColorPalette('custom', data.dokanSettings.colors.paletteValues.custom2, 'custom');
+    });
+
+    test('admin can disable color scheme customizer module', { tag: ['@pro', '@admin'] }, async () => {
+        await apiUtils.deactivateModules(payloads.moduleIds.colorSchemeCustomize, payloads.adminAuth);
+        await admin.disableColorSchemeCustomizerModule();
     });
 });
