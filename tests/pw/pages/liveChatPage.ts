b5b78d4ef960525965e43ca146b8bc895066319d
shashwata <shashwatahalder01@gmail.com>
2024-12-02 00:02:32 +0600

    Add live chat tests

--- tests/pw/pages/liveChatPage.ts
+++ tests/pw/pages/liveChatPage.ts
@@ -23,6 +23,20 @@ export class LiveChatPage extends BasePage {
         await this.goIfNotThere(data.subUrls.frontend.productDetails(helpers.slugify(productName)), 'domcontentloaded', force);
     }
 
+    // admin
+
+    // enable live chat module
+    async enableLiveChatModule() {
+        await this.goto(data.subUrls.backend.dokan.settings);
+        await this.toBeVisible(selector.admin.dokan.settings.menus.liveChat);
+    }
+
+    // disable live chat module
+    async disableLiveChatModule() {
+        await this.goto(data.subUrls.backend.dokan.settings, { waitUntil: 'domcontentloaded' }, true);
+        await this.notToBeVisible(selector.admin.dokan.settings.menus.liveChat);
+    }
+
     // vendor
 
     // vendor inbox render properly
