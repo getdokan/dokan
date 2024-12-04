cd6f58b48c848f0207585a0b31ccc94a57d601d7
osman sufy <osmansufy20@gmail.com>
2024-12-04 18:41:16 +0600

    in try catch for update_vendor_balance

--- includes/Withdraw/Hooks.php
+++ includes/Withdraw/Hooks.php
@@ -88,46 +88,51 @@ class Hooks {
      * @return void
      */
     public function update_vendor_balance( $withdraw ) {
-        global $wpdb;
+        try{
+            global $wpdb;
 
-        if ( NumberUtil::round( dokan_get_seller_balance( $withdraw->get_user_id(), false ), 2 ) < NumberUtil::round( $withdraw->get_amount(), 2 ) ) {
-            return;
-        }
+            if ( NumberUtil::round( dokan_get_seller_balance( $withdraw->get_user_id(), false ), 2 ) < NumberUtil::round( $withdraw->get_amount(), 2 ) ) {
+                error_log( 'Withdraw request amount is greater than vendor balance'. $withdraw->get_amount() );
+                return;
+            }
 
-        $balance_result = $wpdb->get_row(
-            $wpdb->prepare(
-                "select * from {$wpdb->dokan_vendor_balance} where trn_id = %d and trn_type = %s",
-                $withdraw->get_id(),
-                'dokan_withdraw'
-            )
-        );
-
-        if ( empty( $balance_result ) ) {
-            $wpdb->insert(
-                $wpdb->dokan_vendor_balance,
-                [
-                    'vendor_id'    => $withdraw->get_user_id(),
-                    'trn_id'       => $withdraw->get_id(),
-                    'trn_type'     => 'dokan_withdraw',
-                    'perticulars'  => 'Approve withdraw request',
-                    'debit'        => 0,
-                    'credit'       => $withdraw->get_amount(),
-                    'status'       => 'approved',
-                    'trn_date'     => $withdraw->get_date(),
-                    'balance_date' => dokan_current_datetime()->format( 'Y-m-d H:i:s' ),
-                ],
-                [
-                    '%d',
-                    '%d',
-                    '%s',
-                    '%s',
-                    '%f',
-                    '%f',
-                    '%s',
-                    '%s',
-                    '%s',
-                ]
+            $balance_result = $wpdb->get_row(
+                $wpdb->prepare(
+                    "select * from {$wpdb->dokan_vendor_balance} where trn_id = %d and trn_type = %s",
+                    $withdraw->get_id(),
+                    'dokan_withdraw'
+                )
             );
+
+            if ( empty( $balance_result ) ) {
+                $wpdb->insert(
+                    $wpdb->dokan_vendor_balance,
+                    [
+                        'vendor_id'    => $withdraw->get_user_id(),
+                        'trn_id'       => $withdraw->get_id(),
+                        'trn_type'     => 'dokan_withdraw',
+                        'perticulars'  => 'Approve withdraw request',
+                        'debit'        => 0,
+                        'credit'       => $withdraw->get_amount(),
+                        'status'       => 'approved',
+                        'trn_date'     => $withdraw->get_date(),
+                        'balance_date' => dokan_current_datetime()->format( 'Y-m-d H:i:s' ),
+                    ],
+                    [
+                        '%d',
+                        '%d',
+                        '%s',
+                        '%s',
+                        '%f',
+                        '%f',
+                        '%s',
+                        '%s',
+                        '%s',
+                    ]
+                );
+            }
+        } catch ( \Exception $e ) {
+        dokan_log( 'Withdraw Balance Update Error: ' . $e->getMessage() );
         }
     }
 
