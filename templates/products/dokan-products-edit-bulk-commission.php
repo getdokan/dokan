608d1269bcf2432fde64b763ff5fcc7d72617028
Aunshon <aunshonhasan@gmail.com>
2024-12-06 17:12:51 +0600

    Enhancement: product commission bulk edit (#2464)
    * Remove $commission_type variable it was not used
    
    * Save fixed as default commission type
    
    * Save bulk product commission.
    
    * Update bulk edit ui
    Skip reverse withdrawal and advertisement product id
    
    * Update bulk edit ui
    Skip reverse withdrawal and advertisement product id
    
    * Revert alignment

---
+++ templates/products/dokan-products-edit-bulk-commission.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Template Name: Dokan commission setting bulk product edit
+ *
+ * @since DOKAN_SINCE
+ *
+ * @package Dokan
+ */
+
+use WeDevs\Dokan\Commission\Formula\Fixed;
+?>
+
+<div class="inline-edit-col" style="float: left">
+    <h4><?php esc_html_e( 'Commission settings', 'dokan-lite' ); ?></h4>
+    <div class="inline-edit-group">
+        <label class="alignleft">
+            <span class="title"><?php esc_html_e( 'Commission', 'dokan-lite' ); ?></span>
+            <span class="input-text-wrap">
+					<select class="dokan_override_bulk_product_commission change_to" name="dokan_override_bulk_product_commission">
+						<option value=""><?php esc_html_e( '— No change —', 'dokan-lite' ); ?></option>
+                        <option value="1"><?php esc_html_e( 'Change to:', 'dokan-lite' ); ?></option>
+                    </select>
+				</span>
+        </label>
+        <div class="change-input inline-edit-group dokan-admin-bulk-product-commission-data-box" style="">
+            <div>
+                <label for="admin_commission">
+                    <?php esc_html_e( 'Fixed', 'dokan-lite' ); ?>
+                </label>
+
+                <span class="input-text-wrap" style="display: flex">
+                        <input type="hidden" name="_per_product_admin_commission_type" value="<?php echo esc_attr( Fixed::SOURCE ); ?>">
+                        <input class="input-text wc_input_price" min="0" max="100" type="text" name="_per_product_admin_commission" value=""/>
+                        <span style="display: flex; align-items: center">%&nbsp;&nbsp;+</span>
+                        <input type="text" name="_per_product_admin_additional_fee" class="input-text wc_input_price" value="">
+                    </span>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
