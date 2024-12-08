908190b4f70bb3289a88e41e2557112e4422acc5
shashwata <shashwatahalder01@gmail.com>
2024-12-08 14:57:12 +0600

    Update product advertising tests

--- tests/pw/tests/e2e/productAdvertising.spec.ts
+++ tests/pw/tests/e2e/productAdvertising.spec.ts
@@ -30,6 +30,10 @@ test.describe('Product Advertising test (admin)', () => {
 
     //admin
 
+    test('admin can enable product advertising module', { tag: ['@pro', '@admin'] }, async () => {
+        await admin.enableProductAdvertisingModule();
+    });
+
     test('admin can view product advertising menu page', { tag: ['@pro', '@exploratory', '@admin'] }, async () => {
         await admin.adminProductAdvertisingRenderProperly();
     });
@@ -67,11 +71,16 @@ test.describe('Product Advertising test (admin)', () => {
 });
 
 test.describe('Product Advertising test (vendor)', () => {
+    let admin: ProductAdvertisingPage;
     let vendor: VendorPage;
-    let vPage: Page;
+    let aPage: Page, vPage: Page;
     let apiUtils: ApiUtils;
 
     test.beforeAll(async ({ browser }) => {
+        const adminContext = await browser.newContext(data.auth.adminAuth);
+        aPage = await adminContext.newPage();
+        admin = new ProductAdvertisingPage(aPage);
+
         const vendorContext = await browser.newContext(data.auth.vendorAuth);
         vPage = await vendorContext.newPage();
         vendor = new VendorPage(vPage);
@@ -80,6 +89,7 @@ test.describe('Product Advertising test (vendor)', () => {
     });
 
     test.afterAll(async () => {
+        await apiUtils.activateModules(payloads.moduleIds.productAdvertising, payloads.adminAuth);
         await vPage.close();
         await apiUtils.dispose();
     });
@@ -108,5 +118,10 @@ test.describe('Product Advertising test (vendor)', () => {
         const orderId = await vendor.buyProductAdvertising(productName, 'auction', AuctionsPage);
         await apiUtils.updateOrderStatus(orderId, 'wc-completed', payloads.adminAuth);
         await vendor.assertProductAdvertisementIsBought(productName, 'auction', AuctionsPage);
+    });
+
+    test('admin can disable product advertising module', { tag: ['@pro', '@admin'] }, async () => {
+        await apiUtils.deactivateModules(payloads.moduleIds.productAdvertising, payloads.adminAuth);
+        await admin.disableProductAdvertisingModule();
     });
 });
