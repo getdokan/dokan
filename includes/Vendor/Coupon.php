2dc425d4097789a65f22ec8a0a26b0c047e57d36
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-12-23 16:57:56 +0600

    refactor: using chat-gpt

--- includes/Vendor/Coupon.php
+++ includes/Vendor/Coupon.php
@@ -19,70 +19,70 @@ class Coupon {
         add_action( 'dokan_wc_coupon_applied', [ $this, 'save_item_coupon_discount' ], 10, 3 );
     }
 
-    /*
-     * Apply line item coupon discount
+    /**
+     * Intercept coupon application to apply line item coupon discount.
      *
-     * @param int $apply_quantity
-     * @param object $item
-     * @param WC_Coupon $coupon
-     * @param WC_Discounts $discounts
+     * @param int $apply_quantity Number of items the coupon applies to.
+     * @param object $item The cart or order item object.
+     * @param WC_Coupon $coupon The coupon being applied.
+     * @param WC_Discounts $discounts Discount object.
      *
      * @return int
-     * @throws \Exception
      */
-    public function intercept_wc_coupon( $apply_quantity, $item, $coupon, $discounts ): int {
+    public function intercept_wc_coupon( int $apply_quantity, $item, WC_Coupon $coupon, WC_Discounts $discounts ): int {
         remove_filter( 'woocommerce_coupon_get_apply_quantity', [ $this, 'intercept_wc_coupon' ], 15 );
-        $discounts = clone $discounts;
-        $discounts->apply_coupon( $coupon );
 
-        do_action( 'dokan_wc_coupon_applied', $coupon, $discounts, $item, $apply_quantity, $this );
+        $discounts_clone = clone $discounts;
+        $discounts_clone->apply_coupon( $coupon );
+
+        do_action( 'dokan_wc_coupon_applied', $coupon, $discounts_clone, $item, $apply_quantity, $this );
 
         add_filter( 'woocommerce_coupon_get_apply_quantity', [ $this, 'intercept_wc_coupon' ], 15, 4 );
 
         return $apply_quantity;
     }
 
-    /*
-    * Apply line item coupon discount
-    *
-    * @param WC_Coupon $coupon
-    * @param WC_Discounts $discounts
-    * @param object $item
-    *
-    * @return void
-    */
-    public function save_item_coupon_discount( $coupon, $discounts, $item ) {
+    /**
+     * Save coupon discount data for an item.
+     *
+     * @param WC_Coupon $coupon The coupon being applied.
+     * @param WC_Discounts $discounts Discount object.
+     * @param object $item The cart or order item object.
+     *
+     * @return void
+     */
+    public function save_item_coupon_discount( WC_Coupon $coupon, WC_Discounts $discounts, $item ): void {
         $object_to_apply_coupon = $discounts->get_object();
 
         if ( $object_to_apply_coupon instanceof WC_Cart ) {
             $this->save_coupon_data_to_cart_item( $coupon, $discounts, $item );
-        }
-
-        if ( $object_to_apply_coupon instanceof WC_Order ) {
+        } elseif ( $object_to_apply_coupon instanceof WC_Order ) {
             $order = wc_get_order( $object_to_apply_coupon->get_id() );
             $this->save_coupon_data_to_order_item( $coupon, $discounts, $item, $order );
         }
     }
 
     /**
-    * @param WC_Coupon $coupon
-    * @param WC_Discounts $discounts
-    * @param object $item
-    *
-    * @return void
-    */
-    protected function save_coupon_data_to_cart_item( $coupon, $discounts, $item ) {
-        $coupon_codes = WC()->cart->get_applied_coupons();
-
+     * Save coupon data to a cart item.
+     *
+     * @param WC_Coupon $coupon The coupon being applied.
+     * @param WC_Discounts $discounts Discount object.
+     * @param object $item The cart item object.
+     *
+     * @return void
+     */
+    protected function save_coupon_data_to_cart_item( WC_Coupon $coupon, WC_Discounts $discounts, $item ): void {
+        $cart = WC()->cart;
+        $coupon_codes = $cart->get_applied_coupons();
         $item_key = $item->object['key'];
+        $item_object = $cart->cart_contents[ $item_key ];
 
-        $item_object = WC()->cart->cart_contents[ $item_key ];
-        $coupon_info = $item_object[ static::DOKAN_COUPON_META_KEY ] ?? [];
-
+        $coupon_info = $item_object[ self::DOKAN_COUPON_META_KEY ] ?? [];
         $coupon_info = array_filter(
-            $coupon_info, function ( $coupon_temp ) use ( $coupon_codes ) {
-				return in_array( $coupon_temp['coupon_code'], $coupon_codes, true );
-			}
+            $coupon_info,
+            function ( $coupon_temp ) use ( $coupon_codes ) {
+                return in_array( $coupon_temp['coupon_code'], $coupon_codes, true );
+            }
         );
 
         $item_wise_discounts = $discounts->get_discounts();
@@ -91,45 +91,46 @@ class Coupon {
             $discount_amount = $item_wise_discounts[ $coupon->get_code() ][ $item_key ];
 
             $coupon_info[ $coupon->get_code() ] = [
-                'discount' => $discount_amount,
+                'discount'    => $discount_amount,
                 'coupon_code' => $coupon->get_code(),
-                'per_item' => $discount_amount / $item_object['quantity'],
-                'product_id' => $item_object['product_id'],
+                'per_item'    => $discount_amount / $item_object['quantity'],
+                'product_id'  => $item_object['product_id'],
             ];
         }
 
-        $item_object[ static::DOKAN_COUPON_META_KEY ] = $coupon_info;
-        WC()->cart->cart_contents[ $item_object['key'] ] = $item_object;
+        $item_object[ self::DOKAN_COUPON_META_KEY ] = $coupon_info;
+        $cart->cart_contents[ $item_object['key'] ] = $item_object;
     }
 
     /**
-     * Save coupon data to order item
+     * Save coupon data to an order item.
      *
-     * @param WC_Coupon $coupon
-     * @param WC_Discounts $discounts
-     * @param object $item
-     * @param WC_Order $order
+     * @param WC_Coupon $coupon The coupon being applied.
+     * @param WC_Discounts $discounts Discount object.
+     * @param object $item The order item object.
+     * @param WC_Order $order The order object.
      *
      * @return void
      * @throws \Exception
      */
-    protected function save_coupon_data_to_order_item( $coupon, $discounts, $item, $order ) {
+    protected function save_coupon_data_to_order_item( WC_Coupon $coupon, WC_Discounts $discounts, $item, WC_Order $order ): void {
         $coupon_codes = $order->get_coupon_codes();
 
-        /** @var WC_Order_Item_Product $item */
+        /** @var WC_Order_Item_Product $order_item */
         $order_item = $item->object;
         $item_key = $order_item->get_id();
 
-        $coupon_info = $order_item->get_meta( static::DOKAN_COUPON_META_KEY );
+        $coupon_info = $order_item->get_meta( self::DOKAN_COUPON_META_KEY );
 
         if ( ! is_array( $coupon_info ) ) {
             $coupon_info = [];
         }
 
         $coupon_info = array_filter(
-            $coupon_info, function ( $coupon_temp ) use ( $coupon_codes ) {
-				return in_array( $coupon_temp['coupon_code'], $coupon_codes, true );
-			}
+            $coupon_info,
+            function ( $coupon_temp ) use ( $coupon_codes ) {
+                return in_array( $coupon_temp['coupon_code'], $coupon_codes, true );
+            }
         );
 
         $item_wise_discounts = $discounts->get_discounts();
@@ -138,65 +139,64 @@ class Coupon {
             $discount_amount = $item_wise_discounts[ $coupon->get_code() ][ $item_key ];
 
             $coupon_info[ $coupon->get_code() ] = [
-                'discount' => $discount_amount,
+                'discount'    => $discount_amount,
                 'coupon_code' => $coupon->get_code(),
-                'per_item' => $discount_amount / $order_item->get_quantity(),
-                'product_id' => $order_item->get_product_id(),
+                'per_item'    => $discount_amount / $order_item->get_quantity(),
+                'product_id'  => $order_item->get_product_id(),
             ];
         }
 
-        wc_update_order_item_meta( $order_item->get_id(), static::DOKAN_COUPON_META_KEY, $coupon_info );
+        wc_update_order_item_meta( $order_item->get_id(), self::DOKAN_COUPON_META_KEY, $coupon_info );
     }
 
     /**
-     * Add coupon info to order item
+     * Add coupon info to an order item during checkout.
      *
-     * @param $item
-     * @param $cart_item_key
-     * @param $values
-     *
-     * @return void
+     * @param WC_Order_Item_Product $item The order item object.
+     * @param string $cart_item_key The cart item key.
+     * @param array $values Cart item values.
      */
-    public function add_coupon_info_to_order_item( $item, $cart_item_key, $values ) {
-        if ( ! empty( $values[ static::DOKAN_COUPON_META_KEY ] ) ) {
+    public function add_coupon_info_to_order_item( $item, $cart_item_key, $values ): void {
+        if ( ! empty( $values[ self::DOKAN_COUPON_META_KEY ] ) ) {
             $total_discount = 0;
-            $limit_reached = '';
-            $coupon_info = $values[ static::DOKAN_COUPON_META_KEY ];
-            // Adjust coupon discount greater than product price
+            $limit_reached = false;
+            $coupon_info = $values[ self::DOKAN_COUPON_META_KEY ];
+
             foreach ( $coupon_info as $key => $coupon ) {
                 $total_discount += $coupon['discount'];
                 $product = wc_get_product( $coupon['product_id'] );
                 $total_product_price = $product->get_price() * $values['quantity'];
-                if ( $limit_reached === 'exit' ) {
+
+                if ( $limit_reached ) {
                     $coupon_info[ $key ]['discount'] = 0;
                 }
-                if ( $total_discount > $total_product_price && $limit_reached !== 'exit' ) {
+
+                if ( $total_discount > $total_product_price && $limit_reached === false ) {
                     $remain_discount = $total_discount - $total_product_price;
                     $coupon_info[ $key ]['discount'] = $coupon['discount'] - $remain_discount;
-                    $limit_reached = 'exit';
+                    $limit_reached = true;
                 }
             }
-            $item->add_meta_data( static::DOKAN_COUPON_META_KEY, $coupon_info, true );
+
+            $item->add_meta_data( self::DOKAN_COUPON_META_KEY, $coupon_info, true );
         }
     }
 
     /**
-     * Remove coupon info from cart item
+     * Remove coupon info from a cart item when a coupon is removed.
      *
-     * @param $coupon_code
-     *
-     * @return void
+     * @param string $coupon_code The coupon code being removed.
      */
-    public function remove_coupon_info_from_cart_item( $coupon_code ) {
+    public function remove_coupon_info_from_cart_item( string $coupon_code ): void {
         $cart = WC()->cart;
         $cart_contents = $cart->cart_contents;
 
         foreach ( $cart_contents as $cart_item_key => $cart_item ) {
-            $coupon_info = $cart_item[ static::DOKAN_COUPON_META_KEY ] ?? [];
+            $coupon_info = $cart_item[ self::DOKAN_COUPON_META_KEY ] ?? [];
+
             if ( isset( $coupon_info[ $coupon_code ] ) ) {
                 unset( $coupon_info[ $coupon_code ] );
-
-                $cart_contents[ $cart_item_key ][ static::DOKAN_COUPON_META_KEY ] = $coupon_info;
+                $cart_contents[ $cart_item_key ][ self::DOKAN_COUPON_META_KEY ] = $coupon_info;
             }
         }
 
