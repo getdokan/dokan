82d35606bd1adbaad74d34bad7511c8bd1aa5596
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-01-23 10:42:37 +0600

    refactor: revert the query

--- includes/Withdraw/Withdraws.php
+++ includes/Withdraw/Withdraws.php
@@ -93,7 +93,7 @@ class Withdraws {
         $join       = '';
         $where      = '';
         $groupby    = '';
-        $orderby    = 'ORDER BY date DESC';
+        $orderby    = '';
         $limits     = '';
         $query_args = [ 1, 1 ];
 
