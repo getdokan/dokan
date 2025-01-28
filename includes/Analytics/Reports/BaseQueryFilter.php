29a4554e433d811e661c129657aaff9c33541cb9
Md. Asif Hossain Nadim <90011088+MdAsifHossainNadim@users.noreply.github.com>
2025-01-28 11:56:29 +0600

    enhance: Add analytics cache modifier for seller analytics filter. (#2529)
    * enhance: Add seller analytic stats modifier for seller filter.
    
    * fix: phpcs issue for subquery refund method.
    
    * enhance: add filter for analytics query argument entities.
    
    * fix: class name stuff on CacheKeyModifier class constructor.

--- includes/Analytics/Reports/BaseQueryFilter.php
+++ includes/Analytics/Reports/BaseQueryFilter.php
@@ -69,7 +69,7 @@ abstract class BaseQueryFilter implements Hookable {
      * @return array The modified where clauses.
      */
 	protected function add_where_subquery_for_refund( array $clauses ): array {
-		if ( ! isset( $_GET['refunds'] ) ) {
+		if ( ! isset( $_GET['refunds'] ) ) { //phpcs:ignore
 			return $clauses;
 		}
 
@@ -176,6 +176,6 @@ abstract class BaseQueryFilter implements Hookable {
 			return dokan_get_current_user_id();
 		}
 
-		return (int) ( wp_unslash( $_GET['sellers']  ?? 0 ) ); // phpcs:ignore
+		return (int) ( wp_unslash( $_GET['sellers'] ?? 0 ) ); // phpcs:ignore
 	}
 }
