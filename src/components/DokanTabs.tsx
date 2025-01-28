2313d4fb693f8d3fff5b7990b32f63e667a37f5a
osman sufy <osmansufy20@gmail.com>
2025-01-28 17:10:44 +0600

    feat: Dokan reusable tabs components

---
+++ src/components/DokanTabs.tsx
@@ -0,0 +1,34 @@
+import { TabPanel } from '@wordpress/components';
+import { twMerge } from 'tailwind-merge';
+
+const MyTabPanel = ( {
+    onSelect,
+    tabs,
+}: {
+    onSelect: ( tabName: string ) => void;
+    tabs: {
+        name: string;
+        title: string;
+        className: string;
+        content: string | JSX.Element;
+    }[];
+} ) => (
+    <TabPanel
+        className="my-tab-panel"
+        activeClass="bg-dokan-btn-hover"
+        onSelect={ onSelect }
+        tabs={ tabs.map( ( tab ) => ( {
+            name: tab.name,
+            title: tab.title,
+            className: twMerge(
+                'bg-dokan-btn text-dokan-btn hover:bg-dokan-btn-hover hover:hover:text-dokan-btn-hover h-10',
+                tab.className
+            ),
+            content: tab.content,
+        } ) ) }
+    >
+        { ( tab ) => <div>{ tab.content }</div> }
+    </TabPanel>
+);
+
+export default MyTabPanel;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,8 +5,10 @@ export {
     DataForm,
     VIEW_LAYOUTS,
     filterSortAndPaginate,
-    isItemValid
+    isItemValid,
 } from '@wordpress/dataviews/wp';
 export { default as PriceHtml } from './PriceHtml';
 export { default as DateTimeHtml } from './DateTimeHtml';
 export { default as Filter } from './Filter';
+
+export { default as DokanTabs } from './DokanTabs';
