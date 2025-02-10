e147b90c9a1abb2b80f0a9c6769ecf5b7a72f3f8
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-02-10 14:20:27 +0600

    Register hooks through Hookable interface

--- dokan-class.php
+++ dokan-class.php
@@ -1,5 +1,6 @@
 <?php
 
+use WeDevs\Dokan\Contracts\Hookable;
 use WeDevs\Dokan\DependencyManagement\Container;
 
 /**
@@ -268,6 +269,11 @@ final class WeDevs_Dokan {
         add_action( 'in_plugin_update_message-dokan-lite/dokan.php', [ \WeDevs\Dokan\Install\Installer::class, 'in_plugin_update_message' ] );
 
         add_action( 'widgets_init', [ $this, 'register_widgets' ] );
+
+        $hooks = $this->get_container()->get( Hookable::class );
+        foreach ( $hooks as $hook ) {
+            $hook->register_hooks();
+        }
     }
 
     /**
