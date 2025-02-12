1ff2f3b6aa89b5414615b01f7e0787184853205f
Shazahanul Islam Shohag <shohag121@gmail.com>
2025-02-12 11:00:30 +0600

    refactor: implemented the new servicefor AdminDashboardServiceProvider.php

--- includes/DependencyManagement/Providers/AdminDashboardServiceProvider.php
+++ includes/DependencyManagement/Providers/AdminDashboardServiceProvider.php
@@ -10,25 +10,12 @@ class AdminDashboardServiceProvider extends BaseServiceProvider {
     /**
      * Tag for services added to the container.
      */
-    public const TAG = 'admin-dashboard-service';
+    protected $tags = [ 'admin-dashboard-service' ];
 
 	protected $services = [
-		self::TAG,
         Dashboard::class,
         Status::class,
 	];
-
-	/**
-     * {@inheritDoc}
-     *
-     * Check if the service provider can provide the given service alias.
-     *
-     * @param string $alias The service alias to check.
-     * @return bool True if the service provider can provide the service, false otherwise.
-     */
-	public function provides( string $alias ): bool {
-		return in_array( $alias, $this->services, true );
-	}
 
 	/**
      * Register the classes.
