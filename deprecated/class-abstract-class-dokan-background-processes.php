1893312c89f9fd7cd3591e6dabc2af546a3aa5ff
Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-03-03 12:29:02 +0600

    Remove the deprecated folder and Deprecated controllers (#2585)

--- deprecated/class-abstract-class-dokan-background-processes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use WeDevs\Dokan\Abstracts\DokanBackgroundProcesses;
-
-/**
- * Abstract Dokan_Background_Processes class
- * @deprecated 2.9.30 Use `WeDevs\Dokan\Abstracts\DokanBackgroundProcesses` instead of `WeDevs\Dokan\Abstracts\Abstract_Dokan_Background_Processes`
- */
-abstract class Abstract_Dokan_Background_Processes extends DokanBackgroundProcesses {}
