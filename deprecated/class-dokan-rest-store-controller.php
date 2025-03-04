07489cd398bd00ccd0e2369587d2f1f2869c8b66
Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-03-04 10:56:43 +0600

    Revert "Remove the deprecated folder and Deprecated controllers (#2585)"
    This reverts commit 1893312c89f9fd7cd3591e6dabc2af546a3aa5ff.

---
+++ deprecated/class-dokan-rest-store-controller.php
@@ -0,0 +1,12 @@
+<?php
+
+use WeDevs\Dokan\REST\StoreController;
+
+/**
+ * Dokan REST Store Controller
+ *
+ * @deprecated 2.9.30 Use `WeDevs\Dokan\REST\StoreController` instead of `Dokan_REST_Store_Controller`
+ */
+
+
+class Dokan_REST_Store_Controller extends StoreController {}
