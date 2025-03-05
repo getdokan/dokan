b0784d32efbffcd6221955e334fff936259a732e
Shazahanul Islam Shohag <shohag121@gmail.com>
2025-03-05 15:00:31 +0600

    chore: Remove Build resources

--- assets/js/dokan-admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c99fd182fea4b863f667');
--- assets/js/vendor-registration.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '027c605699f858233620');
