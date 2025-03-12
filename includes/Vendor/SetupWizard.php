8ab21fa12728fab0a3b72f27cba5efad3bdc5f05
Osman Goni Sufy <47870515+osmansufy@users.noreply.github.com>
2025-03-12 12:36:32 +0600

    fix: setup store country state validation  (#2550)
    * update: county state validation in store step
    
    * fix: phpcs
    
    * undo: space
    
    * update: check null field state & country

--- includes/Vendor/SetupWizard.php
+++ includes/Vendor/SetupWizard.php
@@ -487,9 +487,9 @@ class SetupWizard extends DokanSetupWizard {
         $dokan_settings['show_email']   = isset( $_POST['show_email'] ) ? 'yes' : 'no';
         $country = $dokan_settings['address']['country'] ?? '';
         $state = $dokan_settings['address']['state'] ?? '';
-        $country_has_states = isset( $states[ $country ] ) && count( $states[ $country ] ) > 0;
-        $state_is_empty = empty( $state );        // Validating fileds.
-        // Validating fileds.
+        $country_has_states = isset( $states[ $country ] );
+        $state_is_empty = empty( $state );        // Validating filed.
+        // Validating filed.
         $is_valid_form = true;
         if ( empty( $dokan_settings['address']['street_1'] ) ) {
             $is_valid_form = false;
@@ -506,11 +506,13 @@ class SetupWizard extends DokanSetupWizard {
         if ( empty( $dokan_settings['address']['country'] ) ) {
             $is_valid_form = false;
             $_POST['error_address[country]'] = 'error';
-        } elseif ( ( $country_has_states && $state_is_empty ) || ( ! $country_has_states && $state_is_empty ) ) {
+        } elseif ( ( $country_has_states && count( $states[ $country ] ) > 0 && $state_is_empty ) ) {
+            $is_valid_form = false;
+            $_POST['error_address[state]'] = 'error';
+        } elseif ( ! $country_has_states && $state_is_empty ) {
             $is_valid_form = false;
             $_POST['error_address[state]'] = 'error';
         }
-
         if ( ! $is_valid_form ) {
             return;
         }
