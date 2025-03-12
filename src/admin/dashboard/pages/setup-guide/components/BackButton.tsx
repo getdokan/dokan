c04e9279ca9bbd1a73b14b8e27520c48579d5566
MdAsifHossainNadim <nadim@wedevs.com>
2025-03-12 15:43:57 +0600

    enhance: introduce all headings for pages.

--- src/admin/dashboard/pages/setup-guide/components/BackButton.tsx
+++ src/admin/dashboard/pages/setup-guide/components/BackButton.tsx
@@ -1,15 +1,23 @@
 import { Button } from '@getdokan/dokan-ui';
-import BackIcon from '@dokan/admin/onboard/icons/BackIcon';
+import BackIcon from '../icons/BackIcon';
 import { __ } from '@wordpress/i18n';
 
-const BackButton = ( { onBack }: { onBack: () => void } ) => {
+const BackButton = (
+    {
+        onBack,
+        className = '',
+        children = __( 'Back', 'dokan-lite' ),
+    }: {
+        onBack: () => void
+    }
+) => {
     return (
         <Button
             onClick={ onBack }
-            className="flex items-center text-base md:text-lg text-gray-600 font-medium border-0 shadow-none p-0"
+            className={ `flex items-center text-base text-[#393939] font-medium border-0 shadow-none p-0 ${ className }` }
         >
             <BackIcon />
-            { __( 'Back', 'dokan-lite' ) }
+            { children }
         </Button>
     );
 };
