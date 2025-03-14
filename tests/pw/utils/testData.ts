62716cdddc566124ffe65f611ae5e98e22f4af66
shashwata <shashwatahalder01@gmail.com>
2025-03-14 14:22:48 +0600

    disable debuglog

--- tests/pw/utils/testData.ts
+++ tests/pw/utils/testData.ts
@@ -2841,10 +2841,10 @@ export const data = {
 
         // debugInfo
         debugInfo: {
-            WP_DEBUG: true,
-            SCRIPT_DEBUG: true,
-            WP_DEBUG_LOG: true,
-            WP_DEBUG_DISPLAY: true,
+            WP_DEBUG: false,
+            SCRIPT_DEBUG: false,
+            WP_DEBUG_LOG: false,
+            WP_DEBUG_DISPLAY: false,
         },
 
         // site info
