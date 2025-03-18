0f8460853d52fc3da5ae735c107adee1b11c16bf
MdAsifHossainNadim <nadim@wedevs.com>
2025-03-18 11:32:20 +0600

    fix: update select field ui.

--- src/admin/dashboard/pages/setup-guide/Elements/Fields/Select.tsx
+++ src/admin/dashboard/pages/setup-guide/Elements/Fields/Select.tsx
@@ -4,11 +4,6 @@ import { SimpleSelect } from '@getdokan/dokan-ui';
 
 const Select = ( { element, onValueChange }: SettingsProps ) => {
     const [ selectedOption, setSelectedOption ] = useState( [] );
-
-    if ( ! element.display ) {
-        return <></>;
-    }
-
     const onHandleChange = ( event ) => {
         const selectedValue = event?.target?.value;
 
@@ -18,20 +13,25 @@ const Select = ( { element, onValueChange }: SettingsProps ) => {
             value: selectedValue,
         } );
     };
+
+    if ( ! element.display ) {
+        return <></>;
+    }
+
     return (
         <div
-            className="flex justify-between p-4  flex-wrap "
+            className="flex justify-between p-4 flex-wrap "
             id={ element.hook_key + '_div' }
         >
-            <div className="flex flex-col mb-4 w-full sm:w-[70%]">
+            <div className="flex flex-col sm:w-[70%]">
                 <h2 className="text-sm leading-6 font-semibold text-gray-900">
                     { element?.title }
                 </h2>
-                <p className=" text-sm font-normal text-[#828282]">
+                <p className="text-sm font-normal text-[#828282]">
                     { element?.description }
                 </p>
             </div>
-            <div className="w-[11rem]">
+            <div className="flex w-[11rem]">
                 <SimpleSelect
                     name={ element?.id }
                     label={ '' }
