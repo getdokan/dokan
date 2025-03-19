85151ceddc8e28ab82ba1d26daeccfa32928e586
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-03-19 13:55:20 +0600

    added `usePermission` hooks

---
+++ src/hooks/usePermission.ts
@@ -0,0 +1,23 @@
+import { useSelect } from '@wordpress/data';
+import coreStore from '@dokan/stores/core';
+
+type CapType = string | string[];
+
+export const usePermission = ( cap: CapType = 'dokandar' ) => {
+    const hasPermission = useSelect(
+        ( select ) => {
+            if ( typeof cap === 'string' ) {
+                return select( coreStore ).hasCap( cap );
+            }
+            if ( cap?.length ) {
+                return cap.every( ( item ) =>
+                    select( coreStore ).hasCap( item )
+                );
+            }
+            return false;
+        },
+        [ cap ]
+    );
+
+    return { hasPermission };
+};
