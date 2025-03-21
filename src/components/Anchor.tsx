bf97e1981e10afc52bccca1a62f7fcdc4b3c455f
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2025-03-21 12:06:56 +0600

    feat: add new components and update asset files for improved functionality and styling

---
+++ src/components/Anchor.tsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type DokanAnchorProps = React.AnchorHTMLAttributes< HTMLAnchorElement > & {
+    children?: React.ReactNode;
+    className?: string;
+    href: string;
+};
+
+// A single Anchor component that handles all variants
+export const Anchor = ( {
+    children,
+    className = '',
+    href,
+    ...props
+}: DokanAnchorProps ) => {
+    return (
+        <a
+            href={ href }
+            className={ twMerge( 'dokan-link', className ) }
+            { ...props }
+        >
+            { children }
+        </a>
+    );
+};
+
+export default Anchor;
