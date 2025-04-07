522e5067676f6064aa9af63cfc4bff94d21b97ee
Asad Nur <asaduzzamannur.dev@gmail.com>
2025-04-07 14:15:34 +0600

    e2e: Vendor subscription on new dashboard

--- tests/pw/tests/e2e/vendorSubscriptionReact.spec.ts
+++ tests/pw/tests/e2e/vendorSubscriptionReact.spec.ts
@@ -1,11 +1,67 @@
 import { test, expect } from '@playwright/test';
 
-test.only('test', async ({ page }) => {
+test('vendor can visit new dashboard subscriptions page', async ({ page }) => {
     await page.goto('https://dokan-dev.test/my-account/');
-    await page.getByLabel('Username or email address *').click();
-    await page.getByLabel('Username or email address *').fill('vendor1');
-    await page.getByLabel('Username or email address *').press('Tab');
+    // await page.locator('#username').click();
+    await page.locator('#username').fill('vendor1');
+    // await page.locator('#password').click();
     await page.locator('#password').fill('vendor1');
-    await page.locator('#password').press('Enter');
+    await page.getByRole('button', { name: 'Log in' }).click();
     await page.getByRole('link', { name: ' Subscription' }).click();
+    await expect(page).toHaveURL('https://dokan-dev.test/dashboard/new/#/subscription?tab=packs');
+});
+
+test('vendor can see new dashboard subscriptions page contents', async ({ page }) => {
+    await page.goto('https://dokan-dev.test/my-account/');
+    await page.locator('#username').fill('vendor1');
+    await page.locator('#password').fill('vendor1');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: ' Subscription' }).click();
+    await expect(page.getByRole('heading', { name: 'Subscription', exact: true })).toBeVisible();
+});
+
+test('vendor can see new dashboard subscriptions page current subscription tab', async ({ page }) => {
+    await page.goto('https://dokan-dev.test/my-account/');
+    await page.locator('#username').fill('vendor1');
+    await page.locator('#password').fill('vendor1');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: ' Subscription' }).click();
+    await expect(page.locator('#tab-panel-0-packs')).toContainText('Subscription Packs');
+});
+
+test('vendor can see new dashboard subscriptions page current subscription section', async ({ page }) => {
+    await page.goto('https://dokan-dev.test/my-account/');
+    await page.locator('#username').fill('vendor1');
+    await page.locator('#password').fill('vendor1');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: ' Subscription' }).click();
+    await expect(page.getByLabel('Subscription Packs')).toContainText('Current Subscription');
+});
+
+test('vendor can see new dashboard subscriptions page subscription packages section', async ({ page }) => {
+    await page.goto('https://dokan-dev.test/my-account/');
+    await page.locator('#username').fill('vendor1');
+    await page.locator('#password').fill('vendor1');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: ' Subscription' }).click();
+    await expect(page.getByLabel('Subscription Packs')).toContainText('Subscription Packages');
+});
+
+test('vendor can see new dashboard subscriptions page subscription orders tab', async ({ page }) => {
+    await page.goto('https://dokan-dev.test/my-account/');
+    await page.locator('#username').fill('vendor1');
+    await page.locator('#password').fill('vendor1');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: ' Subscription' }).click();
+    await expect(page.locator('#tab-panel-0-orders')).toContainText('Subscription Orders');
+});
+
+test('vendor can see new dashboard subscriptions page subscription orders table', async ({ page }) => {
+    await page.goto('https://dokan-dev.test/my-account/');
+    await page.locator('#username').fill('vendor1');
+    await page.locator('#password').fill('vendor1');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await page.getByRole('link', { name: ' Subscription' }).click();
+    await page.getByRole('tab', { name: 'Subscription Orders' }).click();
+    await expect(page.locator('thead')).toContainText('Order');
 });
