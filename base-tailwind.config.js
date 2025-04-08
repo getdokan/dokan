fe9416d24f1ff05c4da1e1768ccbdf76fc53347d
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2025-04-08 16:31:25 +0600

    Fix: compatibility support color classes (#2636)
    * Update: add tertiary color options for dokan button styles in Tailwind config
    
    * Update: rename label styles to badge styles in Tailwind config
    
    * Update: fix spacing in dokan badge info class in Tailwind config
    
    * Update: rename label styles to badge styles in base-tailwind.scss
    
    * Update: adjust header title section layout for better alignment
    
    * Update: import sprintf for string formatting in DokanModal and adjust confirmation title layout
    
    * Update: remove unused color variables from base-tailwind.config.js

--- base-tailwind.config.js
+++ base-tailwind.config.js
@@ -187,11 +187,12 @@ const baseConfig = {
                 },
             },
             colors: {
-                primary: 'var(--dokan-button-background-color, #7047EB)',
-                secondary: 'var(--dokan-button-secondary-background-color, #ffffff)',
                 dokan: {
                     sidebar: 'var(--dokan-button-background-color, #322067)',
                     btn: 'var(--dokan-button-background-color, #7047EB)',
+                    primary: 'var(--dokan-button-background-color, #F05025)',
+                    secondary: 'var(--dokan-button-secondary-background-color, var(--dokan-button-text-color, #ffffff))',
+                    tertiary: 'var(--dokan-button-tertiary-background-color, #00000000)',
                 },
             },
         },
