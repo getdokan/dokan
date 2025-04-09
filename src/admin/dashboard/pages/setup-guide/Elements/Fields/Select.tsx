ee7df4b54648f4edbd2ea3b1161804d92806da4f
devianadim9 <devinadim@gmail.com>
2025-04-09 15:04:01 +0600

    update: remove comment out testcases.

--- src/admin/dashboard/pages/setup-guide/Elements/Fields/Select.tsx
+++ src/admin/dashboard/pages/setup-guide/Elements/Fields/Select.tsx
@@ -6,6 +6,10 @@ const Select = ( { element, onValueChange }: SettingsProps ) => {
     const initialValue = element.value ? element.value : element.default;
     const [ selectedOption, setSelectedOption ] = useState( initialValue );
 
+    if ( ! element.display ) {
+        return <></>;
+    }
+
     const onHandleChange = ( event ) => {
         const selectedValue = event?.target?.value;
 
@@ -17,10 +21,7 @@ const Select = ( { element, onValueChange }: SettingsProps ) => {
     };
 
     return (
-        <div
-            className="flex justify-between p-4"
-            id={ element.hook_key }
-        >
+        <div className="flex justify-between p-4" id={ element.hook_key }>
             <div className="flex flex-col sm:w-[70%]">
                 <h2 className="text-sm leading-6 font-semibold text-gray-900">
                     <RawHTML>{ element?.title }</RawHTML>
