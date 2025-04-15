292d73dfd2a7446d2bc3981636f5b3a0a74e7de0
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-04-15 18:46:34 +0600

    Added dokan legacy order commission calculation.

--- includes/Commission/OrderLineItemCommission.php
+++ includes/Commission/OrderLineItemCommission.php
@@ -191,13 +191,10 @@ class OrderLineItemCommission extends AbstractCommissionCalculator {
      * @return \WeDevs\Dokan\Commission\Model\Commission
      */
     public function additional_adjustments( Commission $commission_data ): Commission {
-        if ( empty( $this->dokan_coupon_infos ) ) {
-			// check if lineItem has coupon information.
-	        // If not, then we need to check if the order has coupon information.
-	        // If the order has coupon information, then we need to generate the coupon information and save it to the line item. as per the backward compatibility.
+        $this->dokan_coupon_infos = apply_filters( 'adjust_commission_with_backward_compatibility_coupon_for_line_item', $this->dokan_coupon_infos , $commission_data, $this->order, $this->item );
 
-			// If the coupon is not applied to the line item, then we need to calculate the commission data for backward compatibility.
-	        return apply_filters( 'adjust_commission_with_backward_compatibility_coupon_for_line_item', $commission_data, $this->order, $this->item, $this->vendor_id );
+        if ( empty( $this->dokan_coupon_infos ) ) {
+			return $commission_data;
         }
 
         $admin_net_commission = 0.0;
@@ -209,12 +206,18 @@ class OrderLineItemCommission extends AbstractCommissionCalculator {
             $percent               = $commission_params['percentage'] ?? 0;
             $admin_commission_rate = $flat + ( $percent / 100 );
 
-            /**
-             * Calculate admin commission and vendor earning based on the coupon type.
-             * @see https://github.com/getdokan/plugin-internal-tasks/issues/198#issuecomment-2608895467
-             */
-            $admin_net_commission += ( ( $commission_data->get_total_amount() - $dokan_coupon_info->get_vendor_discount() ) * $admin_commission_rate ) - $dokan_coupon_info->get_admin_discount();
-            $vendor_net_earning += floatval( $this->item->get_total() ) - $admin_net_commission;
+            if ( $dokan_coupon_info->is_subsidy_supported() || in_array( $dokan_coupon_info->get_coupon_commissions_type(), [ 'default', '' ], true ) ) {
+                /**
+                 * Calculate admin commission and vendor earning based on the coupon type.
+                 * @see https://github.com/getdokan/plugin-internal-tasks/issues/198#issuecomment-2608895467
+                 */
+                $admin_net_commission += ( ( $commission_data->get_total_amount() - $dokan_coupon_info->get_vendor_discount() ) * $admin_commission_rate ) - $dokan_coupon_info->get_admin_discount();
+                $vendor_net_earning += floatval( $this->item->get_total() ) - $admin_net_commission;
+            } else {
+                $current_admin_net_commission = ( $commission_data->get_total_amount() * $admin_commission_rate ) - $dokan_coupon_info->get_admin_discount();
+                $admin_net_commission += $current_admin_net_commission < 0 ? 0 : $current_admin_net_commission;
+                $vendor_net_earning += floatval( $this->item->get_subtotal() - $dokan_coupon_info->get_vendor_discount() - $dokan_coupon_info->get_admin_discount() ) - abs( $admin_net_commission );
+            }
 
             $commission_data->set_vendor_discount( $commission_data->get_vendor_discount() + $dokan_coupon_info->get_vendor_discount() );
             $commission_data->set_admin_discount( $commission_data->get_admin_discount() + $dokan_coupon_info->get_admin_discount() );
--- includes/Vendor/DokanOrderLineItemCouponInfo.php
+++ includes/Vendor/DokanOrderLineItemCouponInfo.php
@@ -17,7 +17,39 @@ class DokanOrderLineItemCouponInfo {
     private $coupon_commissions_type          = '';
     private $admin_shared_coupon_type         = '';
     private $admin_shared_coupon_amount       = '';
+    private bool $subsidy_supported                = true;
 
+    /**
+     * Check if subsidy is supported
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return bool
+     */
+    public function is_subsidy_supported(): bool {
+        return $this->subsidy_supported;
+    }
+
+    /**
+     * Set subsidy supported
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param bool $subsidy_supported
+     *
+     * @return void
+     */
+    public function set_subsidy_supported( bool $subsidy_supported ): void {
+        $this->subsidy_supported = $subsidy_supported;
+    }
+
+    /**
+     * Get coupon info
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return array
+     */
     public function get_coupon_info(): array {
         return [
             'discount'                         => $this->get_discount(),
@@ -28,9 +60,19 @@ class DokanOrderLineItemCouponInfo {
             'coupon_commissions_type'          => $this->get_coupon_commissions_type(),
             'admin_shared_coupon_type'         => $this->get_admin_shared_coupon_type(),
             'admin_shared_coupon_amount'       => $this->get_admin_shared_coupon_amount(),
+            'subsidy_supported'                => $this->is_subsidy_supported(),
         ];
     }
 
+    /**
+     * Set coupon info
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param $coupon_info
+     *
+     * @return $this
+     */
     public function set_coupon_info( $coupon_info ): self {
         $this->set_discount( $coupon_info['discount'] ?? 0 );
         $this->set_coupon_code( $coupon_info['coupon_code'] ?? '' );
@@ -44,76 +86,195 @@ class DokanOrderLineItemCouponInfo {
         return $this;
     }
 
+    /**
+     * Get discount amount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return float
+     */
     public function get_discount(): float {
         return $this->discount;
     }
 
+    /**
+     * Set discount amount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param float $discount
+     *
+     * @return $this
+     */
     public function set_discount( float $discount ): self {
         $this->discount = $discount;
 
         return $this;
     }
 
+    /**
+     * Get coupon code
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return string
+     */
     public function get_coupon_code(): string {
         return $this->coupon_code;
     }
 
+    /**
+     * Set coupon code
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param string $coupon_code
+     *
+     * @return $this
+     */
     public function set_coupon_code( string $coupon_code ): self {
         $this->coupon_code = $coupon_code;
 
         return $this;
     }
 
+    /**
+     * Get per qty amount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return float
+     */
     public function get_per_qty_amount(): float {
         return floatval( $this->per_qty_amount );
     }
 
+    /**
+     * Set per qty amount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param float $per_qty_amount
+     *
+     * @return $this
+     */
     public function set_per_qty_amount( int $per_qty_amount ): self {
         $this->per_qty_amount = $per_qty_amount;
 
         return $this;
     }
 
+    /**
+     * Get quantity
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return int
+     */
     public function get_quantity(): int {
         return $this->quantity;
     }
 
+    /**
+     * Set quantity
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param int $quantity
+     *
+     * @return $this
+     */
     public function set_quantity( int $quantity ): self {
         $this->quantity = $quantity;
 
         return $this;
     }
 
+    /**
+     * Get admin coupons enabled for vendor
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return string
+     */
     public function get_admin_coupons_enabled_for_vendor(): string {
         return $this->admin_coupons_enabled_for_vendor;
     }
 
+    /**
+     * Set admin coupons enabled for vendor
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param string $admin_coupons_enabled_for_vendor
+     *
+     * @return $this
+     */
     public function set_admin_coupons_enabled_for_vendor( string $admin_coupons_enabled_for_vendor ): self {
         $this->admin_coupons_enabled_for_vendor = $admin_coupons_enabled_for_vendor;
 
         return $this;
     }
 
+    /**
+     * Get coupon commissions type
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return string
+     */
     public function get_coupon_commissions_type(): string {
         return $this->coupon_commissions_type;
     }
 
+    /**
+     * Set coupon commissions type
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param string $coupon_commissions_type
+     *
+     * @return $this
+     */
     public function set_coupon_commissions_type( string $coupon_commissions_type ): self {
         $this->coupon_commissions_type = $coupon_commissions_type;
 
         return $this;
     }
 
+    /**
+     * Get admin shared coupon type
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return string
+     */
     public function get_admin_shared_coupon_type(): string {
         return $this->admin_shared_coupon_type;
     }
 
+    /**
+     * Set admin shared coupon type
+     *
+     * @since DOKAN_SINCE
+     *
+     * @param string $admin_shared_coupon_type
+     *
+     * @return $this
+     */
     public function set_admin_shared_coupon_type( string $admin_shared_coupon_type ): self {
         $this->admin_shared_coupon_type = $admin_shared_coupon_type;
 
         return $this;
     }
 
+    /**
+     * Get admin shared coupon amount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return string
+     */
     public function get_admin_shared_coupon_amount(): string {
         return $this->admin_shared_coupon_amount;
     }
@@ -124,6 +285,13 @@ class DokanOrderLineItemCouponInfo {
         return $this;
     }
 
+    /**
+     * Get vendor discount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return float
+     */
     public function get_vendor_discount() {
         /**
          * if $this->coupon_commissions_type is `from_vendor` or `default` or empty, then vendor will get the full discount
@@ -133,6 +301,13 @@ class DokanOrderLineItemCouponInfo {
         return apply_filters( 'dokan_get_vendor_portion_amount_of_coupon_discount', $discount, $this );
     }
 
+    /**
+     * Get admin discount
+     *
+     * @since DOKAN_SINCE
+     *
+     * @return float
+     */
     public function get_admin_discount() {
         $vendor_portion = $this->get_vendor_discount();
 
