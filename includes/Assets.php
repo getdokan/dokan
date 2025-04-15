2a0b629ace9acddc32d3347f852a54e6f0a55719
Md Mahbub Rabbani <mahbub.rucse@gmail.com>
2025-04-15 15:37:32 +0600

    fix: WC admin script registration

--- includes/Assets.php
+++ includes/Assets.php
@@ -24,6 +24,7 @@ class Assets {
         } else {
             add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_front_scripts' ] );
             add_action( 'wp_enqueue_scripts', [ $this, 'load_dokan_global_scripts' ], 5 );
+            add_action( 'init', [ $this, 'register_wc_admin_scripts' ] );
         }
     }
 
@@ -618,11 +619,30 @@ class Assets {
             ];
         }
 
+        return $scripts;
+    }
+
+    /**
+     * Registers WooCommerce Admin scripts for the React-based Dokan Vendor dashboard.
+     *
+     * This function ensures that the necessary WooCommerce Admin assets are registered
+     * for use in the Dokan Vendor dashboard. It temporarily suppresses "doing it wrong"
+     * warnings during the registration process.
+     *
+     * @return void
+     */
+    public function register_wc_admin_scripts() {
         // Register WooCommerce Admin Assets for the React-base Dokan Vendor ler dashboard.
-        $wc_instance = WCAdminAssets::get_instance();
+        if ( ! function_exists( 'get_current_screen' ) ) {
+            require_once ABSPATH . '/wp-admin/includes/screen.php';
+        }
+
+        add_filter( 'doing_it_wrong_trigger_error', '__return_false' );
+
+		$wc_instance = WCAdminAssets::get_instance();
         $wc_instance->register_scripts();
 
-        return $scripts;
+        add_filter( 'doing_it_wrong_trigger_error', '__return_true' );
     }
 
     /**
