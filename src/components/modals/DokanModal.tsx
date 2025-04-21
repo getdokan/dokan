d7cc80b779588775d4c0db5e543ce8727bac53c9
devianadim9 <devinadim@gmail.com>
2025-04-21 11:05:58 +0600

    fix: update dokan modal button styles.

--- src/components/modals/DokanModal.tsx
+++ src/components/modals/DokanModal.tsx
@@ -2,9 +2,10 @@ import { __, sprintf } from '@wordpress/i18n';
 import { Slot } from '@wordpress/components';
 import { kebabCase } from '@dokan/utilities';
 import { debounce } from '@wordpress/compose';
-import { Modal, Button } from '@getdokan/dokan-ui';
+import { Modal } from '@getdokan/dokan-ui';
 import { useCallback, useState } from '@wordpress/element';
-import DialogIcon from "./DialogIcon";
+import DialogIcon from './DialogIcon';
+import { DokanButton } from '../Button';
 
 interface DokanModalProps {
     isOpen: boolean;
@@ -24,7 +25,7 @@ interface DokanModalProps {
     loading?: boolean;
 }
 
-const DokanModal = ({
+const DokanModal = ( {
     isOpen,
     onClose,
     className,
@@ -40,9 +41,11 @@ const DokanModal = ({
     dialogFooter,
     dialogContent,
     loading = false,
-}: DokanModalProps) => {
+}: DokanModalProps ) => {
     if ( ! namespace ) {
-        throw new Error( 'Namespace is required for the Confirmation Modal component' );
+        throw new Error(
+            'Namespace is required for the Confirmation Modal component'
+        );
     }
 
     const dialogNamespace = kebabCase( namespace );
@@ -69,85 +72,103 @@ const DokanModal = ({
             onClose={ onClose }
             className={ `dokan-layout bg-transparent shadow-none flex justify-center w-fit` }
         >
-            <div className={ `relative text-left bg-white max-w-xl rounded transition-all transform shadow-xl self-center z-0 ${ className }` }>
+            <div
+                className={ `relative text-left bg-white max-w-xl rounded transition-all transform shadow-xl self-center z-0 ${ className }` }
+            >
                 <Modal.Title className={ `border-b` }>
                     <Slot
                         name={ `dokan-before-dialog-header-${ dialogNamespace }` }
-                        fillProps={{ namespace }}
+                        fillProps={ { namespace } }
                     />
 
                     { dialogHeader || (
-                        <div className='text-gray-900'>
-                            { dialogTitle || __( 'Confirmation Dialog', 'dokan-lite' ) }
+                        <div className="text-gray-900">
+                            { dialogTitle ||
+                                __( 'Confirmation Dialog', 'dokan-lite' ) }
                         </div>
-                    )}
+                    ) }
 
                     <Slot
                         name={ `dokan-after-dialog-header-${ dialogNamespace }` }
-                        fillProps={{ namespace }}
+                        fillProps={ { namespace } }
                     />
                 </Modal.Title>
                 <Modal.Content>
                     <Slot
                         name={ `dokan-before-${ dialogNamespace }-dialog-content` }
-                        fillProps={{ namespace }}
+                        fillProps={ { namespace } }
                     />
 
                     { dialogContent || (
                         <div className="sm:flex sm:items-start min-h-32">
-                            {dialogIcon || (
+                            { dialogIcon || (
                                 <div
-                                    className={`flex items-center justify-center flex-shrink-0 w-14 h-14 bg-red-50 border border-red-50 rounded-full`}>
+                                    className={ `flex items-center justify-center flex-shrink-0 w-14 h-14 bg-red-50 border border-red-50 rounded-full` }
+                                >
                                     <DialogIcon />
                                 </div>
-                            )}
+                            ) }
                             <div className="mt-3 sm:ml-4 sm:mt-0 sm:text-left">
-                                <h3 className="text-base font-semibold text-gray-900"
-                                    dangerouslySetInnerHTML={
-                                        {__html: confirmationTitle || __('Delete Confirmation', 'dokan-lite')}
-                                    }
+                                <h3
+                                    className="text-base font-semibold text-gray-900"
+                                    dangerouslySetInnerHTML={ {
+                                        __html:
+                                            confirmationTitle ||
+                                            __(
+                                                'Delete Confirmation',
+                                                'dokan-lite'
+                                            ),
+                                    } }
                                 ></h3>
-                                <div className="mt-2"
-                                     dangerouslySetInnerHTML={
-                                         {
-                                             __html: confirmationDescription || sprintf(
-                                                 /* translators: %1$s: opening strong tag, %2$s: closing strong tag */
-                                                 __('Are you sure you want to proceed with this %1$sdeletion%2$s?', 'dokan-lite'),
-                                                 '<strong>',
-                                                 '</strong>'
-                                             )
-                                         }
-                                     }
+                                <div
+                                    className="mt-2"
+                                    dangerouslySetInnerHTML={ {
+                                        __html:
+                                            confirmationDescription ||
+                                            sprintf(
+                                                /* translators: %1$s: opening strong tag, %2$s: closing strong tag */
+                                                __(
+                                                    'Are you sure you want to proceed with this %1$sdeletion%2$s?',
+                                                    'dokan-lite'
+                                                ),
+                                                '<strong>',
+                                                '</strong>'
+                                            ),
+                                    } }
                                 ></div>
                             </div>
                         </div>
-                    )}
+                    ) }
 
                     <Slot
                         name={ `dokan-after-${ dialogNamespace }-dialog-content` }
-                        fillProps={{ namespace }}
+                        fillProps={ { namespace } }
                     />
                 </Modal.Content>
                 <Modal.Footer className="border-t">
                     { dialogFooter || (
-                        <div className={ `flex items-center justify-end gap-3` }>
-                            <Button
+                        <div
+                            className={ `flex items-center justify-end gap-3` }
+                        >
+                            <DokanButton
                                 onClick={ onClose }
+                                variant={ `secondary` }
                                 disabled={ isSubmitting || loading }
-                                className={ `inline-flex justify-center bg-white hover:bg-gray-200 text-gray-700 font-semibold text-sm py-2 px-4 ring-1 ring-inset ring-gray-300 rounded-md focus:outline-none focus-visible:ring-1` }
                             >
-                                { cancelButtonText || __( 'Cancel', 'dokan-lite' ) }
-                            </Button>
-                            <Button
+                                { cancelButtonText ||
+                                    __( 'Cancel', 'dokan-lite' ) }
+                            </DokanButton>
+                            <DokanButton
+                                variant={ `danger` }
                                 onClick={ handleConfirm }
                                 loading={ isSubmitting || loading }
                                 disabled={ isSubmitting || loading }
-                                className={ `inline-flex justify-center bg-red-500 hover:bg-red-600 text-white font-semibold text-sm py-2 px-4 ring-1 ring-inset ring-red-500 rounded-md` }
                             >
-                                { confirmButtonText || __( 'Yes, Delete', 'dokan-lite' ) }
-                            </Button>
+                                { confirmButtonText ||
+                                    __( 'Yes, Delete', 'dokan-lite' ) }
+                            </DokanButton>
                         </div>
-                    )}
+                    ) }
                 </Modal.Footer>
             </div>
         </Modal>
