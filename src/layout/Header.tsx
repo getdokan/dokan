107d3a4639c5687c2ff0d44320b0977afc95b80f
KAMRUZZAMAN <kzamanbn@gmail.com>
2025-04-21 21:03:00 +0600

    fix: header layout section grid

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -68,7 +68,7 @@ const Header = ( { route } ) => {
                 <Slot name="dokan-before-header" />
             </div>
             <div className="dokan-header-title-section @container/header-title-section flex justify-between items-center flex-wrap">
-                <div className="dokan-header-title flex flex-wrap">
+                <div className="dokan-header-title w-1/2 flex flex-wrap">
                     { title && (
                         <>
                             <h3 className="text-2xl font-semibold text-gray-800  md:text-4xl lg:text-3xl">
@@ -81,11 +81,10 @@ const Header = ( { route } ) => {
                         </>
                     ) }
                 </div>
-                <div className="dokan-header-actions grid grid-cols-2 flex-wrap gap-2.5 md:justify-end">
+                <div className="dokan-header-actions w-1/2 flex flex-wrap gap-2.5 md:justify-end">
                     { backUrl && (
                         <DokanButton
                             variant="secondary"
-                            className={ `col-span-2 flex items-center` }
                             onClick={ () =>
                                 navigate( parseBackUrl( backUrl ) )
                             }
