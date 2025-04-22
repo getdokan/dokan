7d5e6b4198b210042663968be7a93d7744ec22d9
Aunshon <aunshonhasan@gmail.com>
2025-04-22 12:59:48 +0600

    Maintain Dokan UI Standard for v4.0 Withdraw (#2671)

--- src/components/DateTimeHtml.tsx
+++ src/components/DateTimeHtml.tsx
@@ -1,6 +1,7 @@
 import { RawHTML } from '@wordpress/element';
 import '../definitions/window-types';
 import { dateI18n, getSettings } from '@wordpress/date';
+import { __, sprintf } from "@wordpress/i18n";
 
 function DateTimeHtml( {
     date,
@@ -14,10 +15,19 @@ function DateTimeHtml( {
     }
     return (
         <RawHTML>
-            { dateI18n(
-                getSettings().formats.datetime,
-                date,
-                getSettings().timezone.string
+            { sprintf(
+                // translators: %1$s: withdraw date, %2$s: withdraw time
+                __( '%1$s at %2$s', 'dokan' ),
+                dateI18n(
+                    getSettings().formats.date,
+                    date,
+                    getSettings().timezone.string
+                ),
+                dateI18n(
+                    getSettings().formats.time,
+                    date,
+                    getSettings().timezone.string
+                )
             ) }
         </RawHTML>
     );
