444a0da3f2a91ba11ee2caa977b74ff2628ab389
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2025-05-02 11:58:07 +0600

    feat: add InternalError component and integrate with ErrorBoundary (#2699)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -20,4 +20,5 @@ export { default as DokanLink } from './Link';
 export { default as MediaUploader } from './Upload';
 export { default as NotFound } from './../layout/404';
 export { default as Forbidden } from './../layout/403';
+export { default as InternalError } from './../layout/500';
 export { default as VisitStore } from './VisitStore';
