2ecd154bbf340dbd7e70db36347b63d1c856c4a1
Aunshon <aunshonhasan@gmail.com>
2025-07-03 16:36:42 +0600

    Admin dashboard general tab, withdraw tab, verification tab, subscription tab. (#2772)
    * enhance: Verification tab & withdraw tab done.
    
    * enhance: General tab done.
    
    * Moded verification codes to pro
    
    * enhance: Remove unused tabs from vendor dashboard
    
    * enhance: Add dokan-react-components dependency and localize frontend script in admin dashboard
    
    * Moved eu fields to pro.
    
    * enhance: Refactor GeneralTab to use SlotFillProvider and PluginArea
    
    * enhance: Add NoInformation component export to index
    
    * enhance: Rename NoInformation component file for improved structure
    
    * Moved subscription tab component to pro
    
    * enhance: Improve dependency handling and add fallback for country/state retrieval
    
    * enhance: Reorder tab positions in TabSections for improved navigation
    
    * enhance: Update GeneralTab and NoInformation components for improved default value handling and localization

--- src/admin/dashboard/pages/vendors-single/InformationTabs/SubscriptionTab.tsx
+++
@@ -1,9 +0,0 @@
-import { Vendor } from '@dokan/definitions/dokan-vendors';
-
-interface SubscriptionTabProps {
-    vendor: Vendor;
-}
-
-const SubscriptionTab = ( { vendor }: SubscriptionTabProps ) => {};
-
-export default SubscriptionTab;
--- src/admin/dashboard/pages/vendors-single/InformationTabs/VerificationTab.tsx
+++
@@ -1,9 +0,0 @@
-import { Vendor } from '@dokan/definitions/dokan-vendors';
-
-interface VerificationTabProps {
-    vendor: Vendor;
-}
-
-const VerificationTab = ( { vendor }: VerificationTabProps ) => {};
-
-export default VerificationTab;
