f979e7e9db1d7fafed61b8dc7d2d9338c3c5f572
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-07-03 17:31:16 +0600

    enhance: Implement custom 404 component for admin dashboard

--- src/layout/404.tsx
+++ src/layout/404.tsx
@@ -2,6 +2,7 @@ import NotFoundIcon from '@dokan/layout/Icons/NotFoundIcon';
 import { __ } from '@wordpress/i18n';
 import { DokanButton } from '../components/Button';
 import { Slot } from '@wordpress/components';
+import { twMerge } from "tailwind-merge";
 
 const NotFound = ( {
     children,
@@ -11,11 +12,13 @@ const NotFound = ( {
         'The page you were looking for appears to have been moved, deleted or does not exist',
         'dokan-lite'
     ),
+    className = '',
 }: {
-    children: JSX.Element;
+    children?: JSX.Element;
     navigateButton?: JSX.Element;
     title?: string;
     message?: string;
+    className?: string;
 } ) => {
     // @ts-ignore
     const dashBoardUrl = window.dokan?.urls?.dashboardUrl ?? '#';
@@ -24,7 +27,12 @@ const NotFound = ( {
             { children ? (
                 children
             ) : (
-                <div className="bg-[url('/assets/images/error-page-bg.png')] bg-no-repeat bg-center w-full bg-cover h-[21rem] ">
+                <div
+                    className={ twMerge(
+                        "bg-[url('/assets/images/error-page-bg.png')] bg-no-repeat bg-center w-full bg-cover h-[21rem]",
+                        className
+                    ) }
+                >
                     { /*slot for 404*/ }
                     <Slot name="before-dokan-not-found" />
                     <div className="flex flex-col items-center justify-center h-full gap-4">
@@ -42,7 +50,7 @@ const NotFound = ( {
                                     link={ true }
                                     href={ dashBoardUrl }
                                     variant={ 'primary' }
-                                    className="w-[10.5rem] mt-2 py-2"
+                                    className="w-[10.5rem] mt-2 py-2 text-center"
                                 >
                                     { __( 'Back to Dashboard', 'dokan-lite' ) }
                                 </DokanButton>
