d0b08a05c50c14d66700bc93d8633f042db3a736
Shazahanul Islam Shohag <shohag121@gmail.com>
2025-07-16 11:36:56 +0600

    feat: refactor Menu component to use Disclosure for nested items and improve state management

--- src/admin/dashboard/pages/settings/Elements/Menu.tsx
+++ src/admin/dashboard/pages/settings/Elements/Menu.tsx
@@ -1,4 +1,10 @@
-import { SettingsElement } from '../StepSettings';
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+} from '@headlessui/react';
+import { ChevronRightIcon } from 'lucide-react';
+import { SettingsElement } from '../../../../../stores/adminSettings/types';
 
 function classNames( ...classes ) {
     return classes.filter( Boolean ).join( ' ' );
@@ -6,7 +12,6 @@ function classNames( ...classes ) {
 
 const Menu = ( {
     pages,
-    parentPages,
     loading,
     activePage,
     onMenuClick,
@@ -20,33 +25,98 @@ const Menu = ( {
     return (
         <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
             <nav className="bg-white rounded-md">
-                { ! loading &&
-                    pages.map( ( item: SettingsElement ) => {
-                        if ( ! item.display ) {
-                            return <></>;
-                        }
-                        return (
-                            <a
-                                key={ item.id }
-                                href={ item.id }
-                                onClick={ ( e ) => {
-                                    e.preventDefault();
-                                    onMenuClick( item.id );
-                                } }
-                                className={ classNames(
-                                    item.id === activePage
-                                        ? 'bg-gray-50 text-orange-600 hover:bg-white'
-                                        : 'text-gray-900 hover:text-gray-900 hover:bg-gray-50',
-                                    'group rounded-md px-6 py-3 flex items-center text-sm font-medium'
-                                ) }
-                                aria-current={
-                                    item.id === activePage ? 'page' : undefined
-                                }
-                            >
-                                <span className="truncate">{ item.title }</span>
-                            </a>
-                        );
-                    } ) }
+                <ul className="-mx-2 space-y-1">
+                    { ! loading &&
+                        pages.map( ( item ) => {
+                            if ( ! item.display ) {
+                                return <></>;
+                            }
+                            return (
+                                <li key={ item.id }>
+                                    { ! item.children ? (
+                                        <a
+                                            href={ item.id }
+                                            className={ classNames(
+                                                item.id === activePage ||
+                                                    item.children.some(
+                                                        ( child ) =>
+                                                            child.id ===
+                                                            activePage
+                                                    )
+                                                    ? 'bg-gray-50'
+                                                    : 'hover:bg-gray-50',
+                                                'block rounded-md py-2 pl-10 pr-2 text-sm/6 font-semibold text-gray-700'
+                                            ) }
+                                        >
+                                            { item.title }
+                                        </a>
+                                    ) : (
+                                        <Disclosure as="div">
+                                            <DisclosureButton
+                                                className={ classNames(
+                                                    item.id === activePage ||
+                                                        item?.children?.some(
+                                                            ( child ) =>
+                                                                child.id ===
+                                                                activePage
+                                                        )
+                                                        ? 'bg-gray-50'
+                                                        : 'hover:bg-gray-50',
+                                                    'group flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm/6 font-semibold text-gray-700'
+                                                ) }
+                                            >
+                                                <ChevronRightIcon
+                                                    aria-hidden="true"
+                                                    className="size-5 shrink-0 text-gray-400 group-data-[open]:rotate-90 group-data-[open]:text-gray-500"
+                                                />
+                                                { item.title }
+                                            </DisclosureButton>
+                                            <DisclosurePanel
+                                                as="ul"
+                                                className="mt-1 px-2"
+                                            >
+                                                { item?.children?.map(
+                                                    ( subItem ) => (
+                                                        <li
+                                                            key={
+                                                                subItem.title
+                                                            }
+                                                        >
+                                                            <DisclosureButton
+                                                                as="a"
+                                                                href={
+                                                                    subItem.id
+                                                                }
+                                                                onClick={ (
+                                                                    e
+                                                                ) => {
+                                                                    e.preventDefault();
+                                                                    onMenuClick(
+                                                                        subItem.id
+                                                                    );
+                                                                } }
+                                                                className={ classNames(
+                                                                    subItem.id ===
+                                                                        activePage
+                                                                        ? 'bg-gray-50'
+                                                                        : 'hover:bg-gray-50',
+                                                                    'block rounded-md py-2 pl-9 pr-2 text-sm/6 text-gray-700'
+                                                                ) }
+                                                            >
+                                                                {
+                                                                    subItem.title
+                                                                }
+                                                            </DisclosureButton>
+                                                        </li>
+                                                    )
+                                                ) }
+                                            </DisclosurePanel>
+                                        </Disclosure>
+                                    ) }
+                                </li>
+                            );
+                        } ) }
+                </ul>
             </nav>
         </aside>
     );
