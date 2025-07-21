f78d16cce06ff1d731fddddd25cc345176cdea37
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-07-21 16:21:08 +0600

    enhance: Add customizable class names for NoInformation component elements

--- src/components/NoInformation.tsx
+++ src/components/NoInformation.tsx
@@ -7,30 +7,65 @@ interface NoInformationProps {
     title?: string;
     description?: string;
     className?: string;
+    contentClassName?: string;
+    textContentClassName?: string;
+    iconClassName?: string;
+    titleClassName?: string;
+    descriptionClassName?: string;
 }
 
 function NoInformation( props: NoInformationProps ) {
     return (
         <Card
             className={ twMerge(
-                'flex flex-col items-center justify-center min-h-[500px] w-full bg-white shadow',
+                'flex flex-col items-center justify-center space-y-4 min-h-[500px] w-full bg-white shadow',
                 props?.className ?? ''
             ) }
         >
-            <div className="flex flex-col items-center space-y-4">
-                <div className="w-16 h-16 bg-[#EFEAFF] rounded-full flex items-center justify-center text-[#7047EB]">
+            <div
+                className={ twMerge(
+                    'flex flex-col items-center space-y-4',
+                    props?.contentClassName ?? ''
+                ) }
+            >
+                <div
+                    className={ twMerge(
+                        'w-16 h-16 bg-[#EFEAFF] rounded-full flex items-center justify-center text-[#7047EB]',
+                        props?.iconClassName ?? ''
+                    ) }
+                >
                     { props.icon ?? <File /> }
                 </div>
 
-                <h3 className="text-lg font-semibold text-gray-900 mt-6">
-                    { props.title ??
-                        __( 'No Information Available', 'dokan-lite' ) }
-                </h3>
+                <div
+                    className={ twMerge(
+                        '!m-0 flex flex-col justify-center',
+                        props?.textContentClassName ?? ''
+                    ) }
+                >
+                    <h3
+                        className={ twMerge(
+                            'text-lg font-semibold text-gray-900 mt-6',
+                            props?.titleClassName ?? ''
+                        ) }
+                    >
+                        { props.title ??
+                            __( 'No Information Available', 'dokan-lite' ) }
+                    </h3>
 
-                <span className="text-gray-500 text-center max-w-xs !m-0">
-                    { props.description ??
-                        __( 'Information needs to be updated', 'dokan-lite' ) }
-                </span>
+                    <span
+                        className={ twMerge(
+                            'text-gray-500 text-center max-w-xs !m-0',
+                            props?.descriptionClassName ?? ''
+                        ) }
+                    >
+                        { props.description ??
+                            __(
+                                'Information needs to be updated',
+                                'dokan-lite'
+                            ) }
+                    </span>
+                </div>
             </div>
         </Card>
     );
