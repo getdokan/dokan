16000d787d7fab7ccb80f8d397e76fa007b948de
Osman Goni Sufy <47870515+osmansufy@users.noreply.github.com>
2025-07-25 09:15:06 +0600

    feat: update PHPUnit config to use an empty default DB password

--- tests/php/phpunit-wp-config.php
+++ tests/php/phpunit-wp-config.php
@@ -38,7 +38,7 @@ define( 'WP_DEBUG', true );
 
 define( 'DB_NAME', getenv( 'WP_DB_NAME' ) ?: 'wp_phpunit_tests' );
 define( 'DB_USER', getenv( 'WP_DB_USER' ) ?: 'root' );
-define( 'DB_PASSWORD', getenv( 'WP_DB_PASS' ) ?: 'osman' );
+define( 'DB_PASSWORD', getenv( 'WP_DB_PASS' ) ?: '' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
