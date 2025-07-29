fee194b935851ca3498cffafb3a883f3a0b67317
Osman Goni Sufy <47870515+osmansufy@users.noreply.github.com>
2025-07-29 14:36:53 +0600

    feat: update CommissionHeader and Section components for improved styling and conditional rendering

--- src/admin/dashboard/pages/settings/Elements/Fields/Commission/CommissionHeader.tsx
+++ src/admin/dashboard/pages/settings/Elements/Fields/Commission/CommissionHeader.tsx
@@ -4,21 +4,20 @@ const CommissionHeader: React.FC = () => {
     return (
         <div className=" h-16 flex items-center px-5">
             <div className="flex items-center">
-                <span className="font-semibold text-[14px] text-[#575757]">
+                <span className=" text-xs uppercase text-[#575757]">
                     { __( 'Category', 'dokan-lite' ) }
                 </span>
             </div>
             <div className="flex items-center ml-auto gap-3">
                 <div className="flex items-center">
                     <div className="w-[142px]">
-                        <span className="font-semibold text-[14px] text-[#575757]">
-                            { __( 'Percentage (%)', 'dokan-lite' ) }
+                        <span className=" uppercase text-xs text-[#828282]">
+                            { __( 'Percentage', 'dokan-lite' ) }
                         </span>
                     </div>
-                    <div className="mx-2 w-4"></div>
                     <div className="w-[120px]">
-                        <span className="font-semibold text-[14px] text-[#575757]">
-                            { __( 'Fixed Amount', 'dokan-lite' ) }
+                        <span className=" text-xs uppercase text-[#828282]">
+                            { __( 'Flat', 'dokan-lite' ) }
                         </span>
                     </div>
                 </div>
