5e752c18d14ada979d6c3f3ab4766a08e05d859a
Kamruzzaman <kzamanbn@gmail.com>
2025-08-21 16:27:28 +0600

    Update category selector and skip category filter test
    Changed the category selector in selectors.ts to use '#dokan-geo-product-categories-root'. Commented out the category filter test in shop.spec.ts with a note to re-enable when the feature is implemented in WooCommerce.

--- tests/pw/pages/selectors.ts
+++ tests/pw/pages/selectors.ts
@@ -7217,7 +7217,7 @@ export const selector = {
                 filterDiv: 'form.dokan-geolocation-location-filters',
                 searchProduct: 'input.dokan-form-control[placeholder="Search Products"]',
                 location: '.location-address input',
-                selectCategory: '#product_cat',
+                selectCategory: '#dokan-geo-product-categories-root',
                 radiusSlider: '.dokan-range-slider',
                 search: '.dokan-btn',
             },
