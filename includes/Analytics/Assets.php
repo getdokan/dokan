fb4ba4b928a3164d801b423e505a2991774a88a1
Kamruzzaman <kzamanbn@gmail.com>
2025-08-28 12:33:09 +0600

    Move script translations call for analytics chunks

--- includes/Analytics/Assets.php
+++ includes/Analytics/Assets.php
@@ -106,6 +106,7 @@ class Assets implements Hookable {
                 $version,
                 true
             );
+            wp_set_script_translations( $chunk_handle, 'dokan-lite' );
         }
 
 		wp_register_script(
@@ -150,10 +151,6 @@ class Assets implements Hookable {
         wp_enqueue_script( 'vendor_analytics_script' );
         wp_enqueue_style( 'vendor_analytics_style' );
         wp_set_script_translations( 'vendor_analytics_script', 'dokan-lite' );
-
-        foreach ( $this->get_analytics_chunks() as $chunk_handle => $chunk ) {
-            wp_set_script_translations( $chunk_handle, 'dokan-lite' );
-        }
 
 		wp_add_inline_script(
             'vendor_analytics_script', 'var vendorAnalyticsDokanConfig = ' . wp_json_encode(
