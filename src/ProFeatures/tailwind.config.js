e3b3d555256d86ac310bd193ac415fc9caa7c26b
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-09-17 10:56:53 +0600

    fix: update Tailwind CSS configuration and add scrollbar hide plugin

--- src/ProFeatures/tailwind.config.js
+++ src/ProFeatures/tailwind.config.js
@@ -3,21 +3,14 @@ import baseConfig from './../../base-tailwind.config';
 /** @type {import('tailwindcss').Config} */
 const updatedConfig = {
     ...baseConfig,
-    content: ['./src/ProFeatures/**/*.{jsx,ts,tsx}'],
+    content: [ './src/ProFeatures/**/*.{jsx,ts,tsx}' ],
     theme: {
         ...baseConfig.theme,
-        extend: {
-            ...baseConfig.theme?.extend,
-            fontFamily: {
-                'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
-                'inter': ['Inter', 'sans-serif'],
-            },
-        },
     },
     plugins: [
-        ...(baseConfig.plugins || []),
-        require('tailwind-scrollbar-hide')
-    ]
+        ...( baseConfig.plugins || [] ),
+        require( 'tailwind-scrollbar-hide' ),
+    ],
 };
 
 export default updatedConfig;
