eb5aa70efa70ee99292d57fb315359db95e9b15f
Md Asif Hossain Nadim <devianadim@gmail.com>
2025-09-30 15:49:46 +0600

    feat: format sales and commissions display in admin dashboard and enhance popover styling

--- src/admin/dashboard/pages/dashboard/sections/AllTimeStatsSection/index.tsx
+++ src/admin/dashboard/pages/dashboard/sections/AllTimeStatsSection/index.tsx
@@ -6,6 +6,7 @@ import { useDashboardApiData } from '../../hooks/useDashboardApiData';
 import { fetchAllTimeStats } from '../../utils/api';
 import { AllTimeStatsData } from '../../types';
 import AllTimeStatsSkeleton from './Skeleton';
+import { formatPrice } from '../../../../../../utilities';
 
 const AllTimeStatsSection = () => {
     const { data, loading, error } =
@@ -29,14 +30,22 @@ const AllTimeStatsSection = () => {
                 <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     { data &&
                         Object.entries( data ).map( ( [ key, item ] ) => {
+                            let count = item.count;
+                            if (
+                                key === 'total_sales' ||
+                                key === 'total_commissions'
+                            ) {
+                                count = formatPrice( item.count );
+                            }
+
                             return (
                                 <Card
                                     key={ key }
                                     icon={
                                         <DynamicIcon iconName={ item.icon } />
                                     }
+                                    content={ count }
                                     text={ item.title }
-                                    content={ item.count }
                                     tooltip={ item.tooltip }
                                 />
                             );
