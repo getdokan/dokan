12560cb1bba5d1e18aad67dbec78fb7def66d168
Shazahanul Islam Shohag <shohag121@gmail.com>
2025-10-21 11:46:02 +0600

    fix: add 'withdraw' to panel switch supported keys filter
    - Extended `supportedKeys` filter to include 'withdraw' for better panel switch handling.

--- src/admin/panel-switcher/PanelSwitch.tsx
+++ src/admin/panel-switcher/PanelSwitch.tsx
@@ -15,7 +15,7 @@ const PanelSwitch = () => {
     const supportedKeys = wp.hooks.applyFilters(
         // Define an array with a filter hook for supported URL keys.
         'dokan_admin_panel_switch_supported_keys',
-        [ 'dashboard' ]
+        [ 'dashboard', 'withdraw' ]
     );
 
     // Get the current URL hash path segments.
