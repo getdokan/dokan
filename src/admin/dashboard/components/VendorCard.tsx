dfdceabb5053fcd7525a232978ef101251d83d58
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-10-23 12:43:02 +0600

    fix: refactor vendor table with `VendorCard`, update styles and labels

---
+++ src/admin/dashboard/components/VendorCard.tsx
@@ -0,0 +1,50 @@
+import { DokanLink } from '@src/components';
+import { twMerge } from 'tailwind-merge';
+
+function VendorCard( { name, avatar, isLoading, loadingClass, onClick, email = '' } ) {
+    return (
+        <div className="flex items-center gap-3">
+            { avatar ? (
+                <div
+                    className={ twMerge(
+                        'w-[44px] h-[44px] rounded object-cover',
+                        isLoading ? `${ loadingClass }` : ''
+                    ) }
+                >
+                    <img
+                        src={ avatar }
+                        alt={ name || 'Store avatar' }
+                        className={ twMerge(
+                            'w-[44px] h-[44px] rounded-[5px] object-cover border-[1px] border-[#E9E9E9] border-solid',
+                            isLoading ? 'opacity-0' : 'opacity-100'
+                        ) }
+                    />
+                </div>
+            ) : (
+                <div
+                    className="w-[44px] h-[44px] rounded bg-gray-100"
+                    aria-hidden="true"
+                ></div>
+            ) }
+            <span className="flex flex-col gap-[6px] h-[44px]">
+                <DokanLink
+                    as="div"
+                    onClick={ onClick }
+                    className="cursor-pointer"
+                >
+                    <span
+                        className={ twMerge(
+                            'text-[14px] font-[600] text-[#7047EB]',
+                            isLoading ? loadingClass : ''
+                        ) }
+                    >
+                        { name }
+                    </span>
+                </DokanLink>
+                <span className="text-[14px] font-[400] text-[#A5A5AA]">{ email }</span>
+            </span>
+        </div>
+    );
+}
+
+export default VendorCard;
