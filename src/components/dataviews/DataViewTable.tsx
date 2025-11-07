fe7b12bbdc662b33ee827f5bb85caa7a6510ae40
Md. Asif Hossain Nadim <90011088+MdAsifHossainNadim@users.noreply.github.com>
2025-11-07 11:24:43 +0600

    enhance: Update `Dataviews` package to `(10.2.0)` for `Dokan`. (#2958)
    * enhance: Update `Dataviews` package to `(10.2.0)` for `Dokan`.
    
    * refactor: Clean up console logs and update `defaultLayouts` structure across dashboard components
    
    * enhance: Add `DataViewsPicker` export and improve hover styling for buttons in `dataviews` component
    
    * refactor: Update `defaultLayouts` to optional and apply filters for improved flexibility in `DataViewTable` components
    
    * revert: package lock json changes from this pr.
    
    * refactor: Replace `dangerouslySetInnerHTML` with `RawHTML` for improved safety in `Header` component
    
    * revert: Replace `dangerouslySetInnerHTML` with `RawHTML` for improved safety in `Header` component
    
    * revert: Replace `dangerouslySetInnerHTML` with `RawHTML` for improved safety in `Header` component
    
    * enhance: Adjust button padding and simplify styles in `dataviews` component

--- src/components/dataviews/DataViewTable.tsx
+++ src/components/dataviews/DataViewTable.tsx
@@ -1,10 +1,15 @@
 import { DataViews } from '@wordpress/dataviews/wp';
-import { Slot } from "@wordpress/components";
+import { Slot } from '@wordpress/components';
 import { ViewportDimensions } from '@dokan/hooks/ViewportDimensions';
-import type { Action, Field, SupportedLayouts, View } from "@wordpress/dataviews/src/types";
-import { kebabCase, snakeCase } from "@dokan/utilities";
-import { useEffect } from "@wordpress/element";
-import { useWindowDimensions } from "@dokan/hooks";
+import type {
+    Action,
+    Field,
+    SupportedLayouts,
+    View,
+} from '@wordpress/dataviews/src/types';
+import { kebabCase, snakeCase } from '@dokan/utilities';
+import { useEffect } from '@wordpress/element';
+import { useWindowDimensions } from '@dokan/hooks';
 import './style.scss';
 
 type ItemWithId = { id: string };
@@ -25,7 +30,7 @@ type DataViewsProps< Item > = {
         totalPages: number;
     };
     ViewportDimensions?: typeof ViewportDimensions;
-    defaultLayouts: SupportedLayouts;
+    defaultLayouts?: SupportedLayouts;
     selection?: string[];
     onChangeSelection?: ( items: string[] ) => void;
     onClickItem?: ( item: Item ) => void;
@@ -35,14 +40,25 @@ type DataViewsProps< Item > = {
     ? { getItemId?: ( item: Item ) => string }
     : { getItemId: ( item: Item ) => string } );
 
-const applyFiltersToTableElements = (namespace: string, elementName: string, element, props: DataViewsProps) => {
-    const snakeCaseNamespace = snakeCase(namespace);
-    return wp.hooks.applyFilters( `dokan_${snakeCaseNamespace}_dataviews_${elementName}`, element, { ...props } );
+const applyFiltersToTableElements = (
+    namespace: string,
+    elementName: string,
+    element,
+    props: DataViewsProps
+) => {
+    const snakeCaseNamespace = snakeCase( namespace );
+    return wp.hooks.applyFilters(
+        `dokan_${ snakeCaseNamespace }_dataviews_${ elementName }`,
+        element,
+        { ...props }
+    );
 };
 
 const DataViewTable = ( props: DataViewsProps< Item > ) => {
     if ( ! props.namespace ) {
-        throw new Error( 'Namespace is required for the DataViewTable component' );
+        throw new Error(
+            'Namespace is required for the DataViewTable component'
+        );
     }
 
     const { width: windowWidth } = useWindowDimensions();
@@ -56,12 +72,35 @@ const DataViewTable = ( props: DataViewsProps< Item > ) => {
         data,
     } = props;
 
+    const defaultLayouts =
+        props?.defaultLayouts ||
+        ( {
+            table: { density: 'comfortable' },
+            list: {},
+        } as SupportedLayouts );
+
     const filteredProps = {
         ...props,
         data: applyFiltersToTableElements( namespace, 'data', data, props ),
         view: applyFiltersToTableElements( namespace, 'view', view, props ),
-        fields: applyFiltersToTableElements( namespace, 'fields', fields, props ),
-        actions: applyFiltersToTableElements( namespace, 'actions', actions, props ),
+        fields: applyFiltersToTableElements(
+            namespace,
+            'fields',
+            fields,
+            props
+        ),
+        actions: applyFiltersToTableElements(
+            namespace,
+            'actions',
+            actions,
+            props
+        ),
+        defaultLayouts: applyFiltersToTableElements(
+            namespace,
+            'layouts',
+            defaultLayouts,
+            props
+        ),
     };
 
     const tableNameSpace = kebabCase( namespace );
