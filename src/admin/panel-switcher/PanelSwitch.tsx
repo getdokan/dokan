0e94a217cb199b77437c038f72292725da7a8798
Shazahanul Islam Shohag <shohag121@gmail.com>
2025-11-24 14:13:13 +0600

    feat: extend panel switcher with support for subscriptions and abuse reports (#2965)

--- src/admin/panel-switcher/PanelSwitch.tsx
+++ src/admin/panel-switcher/PanelSwitch.tsx
@@ -14,7 +14,15 @@ const PanelSwitch = () => {
     const supportedKeys = wp.hooks.applyFilters(
         // Define an array with a filter hook for supported URL keys.
         'dokan_admin_panel_switch_supported_keys',
-        [ 'dashboard', 'withdraw', 'vendors', 'store-reviews', 'verifications' ]
+        [
+            'dashboard',
+            'withdraw',
+            'vendors',
+            'store-reviews',
+            'subscriptions',
+            'abuse-reports',
+            'verifications',
+        ]
     );
 
     // Get the current URL hash path segments.
