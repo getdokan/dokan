6920123acacb706fe4b67389fdabe269e027ee85
Md Asif Hossain Nadim <devianadim@gmail.com>
2025-11-26 16:54:27 +0600

    fix: Remove redundant error logging from FullWidthVendorLayout

--- includes/Shortcodes/FullWidthVendorLayout.php
+++ includes/Shortcodes/FullWidthVendorLayout.php
@@ -173,8 +173,6 @@ class FullWidthVendorLayout implements Hookable {
                 );
             }
 
-            error_log( print_r( dokan_get_dashboard_nav(), 1 ) );
-
             wp_add_inline_script(
                 $this->script_key,
                 'var vendorDashboardLayoutConfig = ' . wp_json_encode(
