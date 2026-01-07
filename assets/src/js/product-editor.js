4c372f9be9d0ccc440011127c9ff2af457db2cc3
Md Asif Hossain Nadim <devianadim@gmail.com>
2026-01-07 12:58:19 +0600

    enhance: Implement token separators for vendor product tags creation.

--- assets/src/js/product-editor.js
+++ assets/src/js/product-editor.js
@@ -443,6 +443,7 @@
             } );
             $( '.product_tag_search' ).select2( {
                 allowClear: false,
+                tokenSeparators: [ ',', ' ' ],
                 tags:
                     dokan.product_vendors_can_create_tags &&
                     'on' === dokan.product_vendors_can_create_tags,
