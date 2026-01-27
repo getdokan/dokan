68ab2e1c8e4f4bb34b5d9514174db1b6d848ff6e
midu-01 <midumojumder8@gmail.com>
2026-01-27 17:03:55 +0600

    refactor: move admin footer rating template to global template file(update)

--- src/admin/dashboard/pages/dashboard/sections/SalesChartSection/index.tsx
+++ src/admin/dashboard/pages/dashboard/sections/SalesChartSection/index.tsx
@@ -187,7 +187,6 @@ const D3Chart = ( { data }: { data: SalesChartDataPoint[] } ) => {
             .append( 'div' )
             .attr( 'class', 'tooltip' )
             .style( 'position', 'absolute' )
-            // .style( 'padding', '10px' )
             .style( 'background', 'rgba(0, 0, 0, 0.8)' )
             .style( 'color', 'white' )
             .style( 'border-radius', '5px' )
