3bc227f5158347e68c4a4e69361d75f0ce0f5a32
Md Asif Hossain Nadim <devianadim@gmail.com>
2026-01-29 14:49:27 +0600

    enhance: improve validation handling by updating needSaving logic and adding validationError support in settings fields

--- src/admin/dashboard/pages/settings/Elements/Fields/DokanEmail.tsx
+++ src/admin/dashboard/pages/settings/Elements/Fields/DokanEmail.tsx
@@ -30,6 +30,7 @@ export default function DokanEmail( { element } ) {
                         tooltip={ element.helper_text }
                         imageUrl={ element?.image_url }
                         wrapperClassNames={ 'w-full' }
+                        validationError={ element?.validationError }
                     />
                 </div>
             ) }
