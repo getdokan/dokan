6258fe1b9518d3068ad3c455fa7be636cbc231fc
Md Asif Hossain Nadim <devianadim@gmail.com>
2026-02-25 16:19:57 +0600

    enhance: Add admin earning calculations based on order type checks in commission and analytics.

--- includes/Commission/Model/Commission.php
+++ includes/Commission/Model/Commission.php
@@ -5,7 +5,20 @@ namespace WeDevs\Dokan\Commission\Model;
 use WeDevs\Dokan\Commission\Contracts\CommissionInterface;
 
 class Commission implements CommissionInterface {
+
+    /**
+     * Vendor Earning without subsidy.
+     *
+     * @var float
+     */
     protected float $admin_net_commission;
+
+    /**
+     * Vendor Earning without subsidy.
+     *
+     * @var float
+     */
+    protected float $admin_net_earning;
 
     /**
      * Vendor Earning without subsidy.
@@ -53,6 +66,28 @@ class Commission implements CommissionInterface {
      */
     public function get_admin_net_commission(): float {
         return $this->admin_net_commission ?? 0;
+    }
+
+    /**
+     * Set the admin's net earning.
+     *
+     * @param float $admin_net_earning The net earning amount for the admin.
+     *
+     * @return self
+     */
+    public function set_admin_net_earning( float $admin_net_earning ): self {
+        $this->admin_net_earning = $admin_net_earning;
+
+        return $this;
+    }
+
+    /**
+     * Get the admin's net earning.
+     *
+     * @return float The net earning amount for the admin.
+     */
+    public function get_admin_net_earning(): float {
+        return $this->admin_net_earning ?? 0;
     }
 
     /**
