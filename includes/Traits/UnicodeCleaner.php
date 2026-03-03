4fa06eeedd61debf7df70e620066102d975316e0
Midu Mojumder <midumojumder@Midus-MacBook-Pro.local>
2026-03-03 23:56:37 +0600

    Fix T&C validation by handling NBSP and Unicode whitespace with handling PHPCS

--- includes/Traits/UnicodeCleaner.php
+++ includes/Traits/UnicodeCleaner.php
@@ -1,28 +1,40 @@
 <?php
+/**
+ * Unicode cleaning helper trait.
+ *
+ * @since DOKAN_SINCE
+ *
+ * @package Dokan
+ */
 
 namespace WeDevs\Dokan\Traits;
 
 trait UnicodeCleaner {
-  /**
-   * @param string $text
-   *
-   * @return string
-   */
-  private function unicodeReplace( $text ) {
-    $text = html_entity_decode( $text, ENT_QUOTES | ENT_HTML5, 'UTF-8' );
-    
-    // Remove UTF-8 BOM (EF BB BF)
-    $text = preg_replace( '/^\xEF\xBB\xBF/', '', $text ) ?? '';
 
-    // Remove UTF-16 BOM (FE FF or FF FE)
-    $text = preg_replace( '/^\xFE\xFF|\xFF\xFE/', '', $text ) ?? '';
+	/**
+	 * Clean unicode related invisible characters from text.
+	 *
+	 * @param string $text Text to clean.
+	 *
+	 * @return string
+	 */
+	private function unicodeReplace( string $text ): string {
 
-    // Replace non-breaking spaces (U+00A0) with normal space
-    $text = str_replace( "\xC2\xA0", ' ', $text );
+		// Decode HTML entities (e.g., &nbsp;).
+		$text = html_entity_decode( $text, ENT_QUOTES | ENT_HTML5, 'UTF-8' );
 
-    // Remove other potential control characters except \n and \t
-    $text = preg_replace( '/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/u', '', $text ) ?? '';
+		// Remove UTF-8 BOM.
+		$text = preg_replace( '/^\xEF\xBB\xBF/', '', $text );
 
-    return $text;
-  }
+		// Remove UTF-16 BOM.
+		$text = preg_replace( '/^(?:\xFE\xFF|\xFF\xFE)/', '', $text );
+
+		// Replace non-breaking space with normal space.
+		$text = str_replace( "\xC2\xA0", ' ', $text );
+
+		// Remove control characters except \n and \t.
+		$text = preg_replace( '/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/u', '', $text );
+
+		return $text;
+	}
 }
