37720543e5ca338f2b88a670f1cfb826296db93f
Kamruzzaman <kzamanbn@gmail.com>
2026-04-23 15:54:35 +0600

    fix: remove unnecessary handling for unregistered @wordpress/ui script

--- webpack-dependency-mapping.js
+++ webpack-dependency-mapping.js
@@ -121,12 +121,6 @@ const wooRequestToHandle = ( request ) => {
  *
  */
 const requestToExternal = ( request ) => {
-    // @wordpress/ui is not registered as a WP script handle yet.
-    // Bundle it instead of externalizing to the missing `wp.ui` global.
-    if ( request === '@wordpress/ui' ) {
-        return false;
-    }
-
     const dokanStores = request.match( /^@dokan\/stores\/(.+)$/ );
     const wc = request.match( /^@woocommerce\/(.+)$/ );
     const dokan = request.match( /^@dokan\/([^/]+)$/ );
