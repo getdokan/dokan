750b8b231919a05bf65bc98a18a83b19e05ce1b8
Kamruzzaman <kzamanbn@gmail.com>
2026-06-03 14:32:01 +0600

    fix: correct navigation URL format and update function parameter in ReactUrlGenerationTest

--- tests/php/src/ReactUrlGenerationTest.php
+++ tests/php/src/ReactUrlGenerationTest.php
@@ -20,7 +20,7 @@ class ReactUrlGenerationTest extends DokanTestCase {
 		return [
 			[
 				'slug' => 'withdraw',
-				'new_url' => '/dashboard/new/#withdraw',
+				'new_url' => '/dashboard/new/#withdraw/',
 			],
 
 		];
@@ -31,7 +31,7 @@ class ReactUrlGenerationTest extends DokanTestCase {
 	 * @dataProvider data_provider
      */
     public function test_dokan_get_navigation_url( $slug, $expected ): void {
-		$url = dokan_get_navigation_url( $slug, null );
+		$url = dokan_get_navigation_url( $slug, true );
 		$this->assertEquals( home_url() . $expected, $url );
     }
 }
